445206e2b9e4601be46d4714e15d5fcf263473cf
Maciej Zarzycki <rapfamilly@gmail.com>
2021-04-12 09:40:47 +0200

    dodanie spella a'la energy field do czarow rogue
    (cherry picked from commit e05b363002f17954d16c4d2f65956f07ffdd73a4)

--- Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Rogue/RogueInitializer.cs
+++ Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Rogue/RogueInitializer.cs
@@ -7,11 +7,12 @@ namespace Server.ACC.CSS.Systems.Rogue
     {
 		public static void Configure()
 		{
-			Register( typeof( RogueFalseCoinSpell ),    "Falszywa moneta",   "Łotrzyk sztuczką ręki podaje fałszywe złoto.",          "Sulfurous Ash; Nightshade",                null, 20481, 5100, School.Rogue );
-			Register( typeof( RogueCharmSpell ),        "Zaklecie",        "Łotrzyk hipnotyzuje cel swoimi złymi oczami.",                 "Black Pearl; Nightshade; Spider's Silk",   null, 21282, 5100, School.Rogue );
-			Register( typeof( RogueSlyFoxSpell ),       "Przebiegła forma",      "Łotrzyk zmienia kształt w ukradkowego lisa.",                  "Petrafied Wood; Nox Crystal; Nightshade",  null, 20491, 5100, School.Rogue );
-			Register( typeof( RogueShadowSpell ),       "Cien",       "Postac wymyka się z cienia.",                                  "Spider's Silk; Daemon Blood; Black Pearl", null, 21003, 5100, School.Rogue );
-			Register( typeof( RogueIntimidationSpell ), "Zastraszenie", "The Rogue begins to look angry and mean at the loss of his skills.", null,                                       null, 20485, 5100, School.Rogue );
+			Register( typeof( RogueFalseCoinSpell ),    "Falszywa moneta",   "Łotrzyk sztuczką ręki podaje fałszywe złoto.",          "",                null, 20481, 5100, School.Rogue );
+			Register( typeof( RogueShieldOfEarthSpell ),    "Klody pod nogi",   "Sciana sieci zatrzymuje wroga.",          "",                null, 2254,  5120, School.Rogue );
+			Register( typeof( RogueCharmSpell ),        "Zaklecie",        "Łotrzyk hipnotyzuje cel swoimi złymi oczami.",                 "",   null, 21282, 5100, School.Rogue );
+			Register( typeof( RogueSlyFoxSpell ),       "Przebiegła forma",      "Łotrzyk zmienia kształt w ukradkowego lisa.",                  "",  null, 20491, 5100, School.Rogue );
+			Register( typeof( RogueShadowSpell ),       "Cien",       "Postac wymyka się z cienia.",                                  "", null, 21003, 5100, School.Rogue );
+			Register( typeof( RogueIntimidationSpell ), "Zastraszenie", "Porazajacy piorun wystrzeliwany jest w kierunku wroga.", "Proszek translokacji", null, 20485, 5100, School.Rogue );
 		}
 	}
 }
