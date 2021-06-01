d1a88d5081cd7299f1f88a3806771d866fcbcd78
Maciej Zarzycki <rapfamilly@gmail.com>
2021-06-01 09:44:03 +0200

    fixy w rogue spell
    (cherry picked from commit 26aff8c36b7b10c5911c748feaef139840f93237)

--- Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Rogue/RogueInitializer.cs
+++ Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Rogue/RogueInitializer.cs
@@ -1,18 +1,22 @@
-using System;
-using Server;
-
 namespace Server.ACC.CSS.Systems.Rogue
 {
-    public class RogueList : BaseInitializer
-    {
+	public class RogueList : BaseInitializer
+	{
 		public static void Configure()
 		{
-			Register( typeof( RogueFalseCoinSpell ),    "Falszywa moneta",   "Łotrzyk sztuczką ręki podaje fałszywe złoto.",          "",                null, 20481, 5100, School.Rogue );
-			Register( typeof( RogueShieldOfEarthSpell ),    "Klody pod nogi",   "Sciana sieci zatrzymuje wroga.",          "",                null, 2254,  5120, School.Rogue );
-			Register( typeof( RogueCharmSpell ),        "Zaklecie",        "Łotrzyk hipnotyzuje cel swoimi złymi oczami.",                 "",   null, 21282, 5100, School.Rogue );
-			Register( typeof( RogueSlyFoxSpell ),       "Przebiegła forma",      "Łotrzyk zmienia kształt w ukradkowego lisa.",                  "",  null, 20491, 5100, School.Rogue );
-			Register( typeof( RogueShadowSpell ),       "Cien",       "Postac wymyka się z cienia.",                                  "", null, 21003, 5100, School.Rogue );
-			Register( typeof( RogueIntimidationSpell ), "Zastraszenie", "Porazajacy piorun wystrzeliwany jest w kierunku wroga.", "Proszek translokacji", null, 20485, 5100, School.Rogue );
+			Register(typeof(RogueFalseCoinSpell), "Falszywa moneta",
+				"Łotrzyk szybkim ruchem ręki podaje fałszywe złoto.", "", null, 20481, 5100, School.Rogue);
+			Register(typeof(RogueShieldOfEarthSpell), "Klody pod nogi",
+				"Łotrzyk rzuca kłody pod nogi celu, blokując go.", "", null, 2254, 5120, School.Rogue);
+			Register(typeof(RogueCharmSpell), "Zaklecie", "Łotrzyk hipnotyzuje cel swoimi złymi oczami.", "", null,
+				21282, 5100, School.Rogue);
+			Register(typeof(RogueSlyFoxSpell), "Przebiegła forma",
+				"Łotrzyk zmienia kształt w panterę, która zwiększa jego siłę, zręczność i inteligencję.", "", null,
+				20491, 5100, School.Rogue);
+			Register(typeof(RogueShadowSpell), "Cien", "Postać znika w cieniu.", "", null, 21003, 5100, School.Rogue);
+			Register(typeof(RogueIntimidationSpell), "Zastraszenie",
+				"Porazajacy piorun wystrzeliwany jest w kierunku wroga.", "Proszek translokacji", null, 20485, 5100,
+				School.Rogue);
 		}
 	}
 }
