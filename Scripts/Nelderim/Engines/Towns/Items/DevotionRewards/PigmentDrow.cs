b064c015454495fb652bf93904ac4483351f8485
Maciej Zarzycki <rapfamilly@gmail.com>
2021-06-15 13:44:25 +0200

    pigmenty miastowe wioski drowow i garlan
    (cherry picked from commit f1b4e2fd6c0cf9d55ddeb3023d24c54723ea7f9e)

---
+++ Scripts/Nelderim/Engines/Towns/Items/DevotionRewards/PigmentDrow.cs
@@ -0,0 +1,37 @@
+
+namespace Server.Items
+{
+    public class PigmentDrow : BasePigment
+    {
+        [Constructable]
+        public PigmentDrow()
+            : this(5)
+        {
+        
+        }
+        [Constructable]
+        public PigmentDrow(int uses)
+            : base(PigmentTarget.Cloth, uses, 2909)
+        {
+            Name = "Pigment barw Wioski Drowow";
+        }
+
+        public PigmentDrow(Serial serial) : base(serial)
+        {
+        }
+        
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write(0);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+    }
+}
