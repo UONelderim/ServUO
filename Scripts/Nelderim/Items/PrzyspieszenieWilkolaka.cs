7eb33d0ca9f08b9daae594c7c4daf981e46a6da4
Maciej Zarzycki <rapfamilly@gmail.com>
2021-07-01 21:04:43 +0200

    przyspieszenie wilkolaka
    (cherry picked from commit 7f87538a5f50d29a2a9cb9ca25f02bbb9f02e4c6)

---
+++ Scripts/Nelderim/Items/PrzyspieszenieWilkolaka.cs
@@ -0,0 +1,62 @@
+using System;
+using Server;
+using Server.Misc;
+using Server.Items;
+using Server.Targeting;
+using Server.Commands;
+
+namespace Server.Items 
+{
+    public class PrzyspieszenieWilkolaka : SilverRing
+	{ 
+		
+		[Constructable] 
+		public PrzyspieszenieWilkolaka()  
+		{ 
+			
+			Weight = 1.0; 
+			Attributes.RegenMana = -5;
+            Attributes.RegenHits = -8;
+			Hue = 1153; 
+			Name = "Przyspieszenie Wilkolaka"; 
+			LootType = LootType.Blessed;
+			Movable = false;
+            SkillBonuses.SetValues(0, SkillName.Fencing, 30.0);
+			
+			
+			
+		}
+
+		public override void OnDoubleClick( Mobile m ) 
+		{
+			string prefix = Server.Commands.CommandSystem.Prefix;
+
+			if ( m.AccessLevel >= AccessLevel.Player)
+			{
+				CommandSystem.Handle( m, String.Format( "{0}speedboost", prefix ) );	
+			}
+
+			else
+			{
+				m.SendMessage( "You are unable to use that!" );
+				this.Delete();
+			}
+		} 
+
+		public PrzyspieszenieWilkolaka( Serial serial ) : base( serial ) 
+		{ 
+		} 
+       
+		public override void Serialize( GenericWriter writer ) 
+		{ 
+			base.Serialize( writer ); 
+			writer.Write( (int) 1 ); // version 
+		}
+
+		public override void Deserialize( GenericReader reader ) 
+		{ 
+			base.Deserialize( reader ); 
+			int version = reader.ReadInt(); 
+		} 
+	}
+}
