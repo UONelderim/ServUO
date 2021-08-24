1f2b014b614427a192cc4dea12dba7d4e1211b4d
Maciej Zarzycki <rapfamilly@gmail.com>
2021-08-24 11:52:34 +0200

    - Czar z Okultyzmu - Hedonizm - nie daje buffa [a teraz juz bedzie] - Czar z Magii Natury - bless nie dziala [a teraz tez juz bedzie]
    (cherry picked from commit c2a376525987131bacbfcc0f59c8e86a26872770)

--- Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Druid/Spells/HollowReedSpell.cs
+++ Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Druid/Spells/HollowReedSpell.cs
@@ -1,57 +1,58 @@
-using System;
-using Server.Targeting;
-using Server.Network;
 using Server.Spells;
+using Server.Targeting;
 
 namespace Server.ACC.CSS.Systems.Druid
 {
 	public class DruidHollowReedSpell : DruidSpell
 	{
 		private static SpellInfo m_Info = new SpellInfo(
-		                                                "Siła Natury", "En Crur Aeta Sec En Ess ",
-                                                        //SpellCircle.Second,
-		                                                203,
-		                                                9061,
-		                                                false,
-		                                                Reagent.Bloodmoss,
-		                                                Reagent.MandrakeRoot,
-		                                                Reagent.Nightshade
-		                                               );
+			"Siła Natury", "En Crur Aeta Sec En Ess ",
+			//SpellCircle.Second,
+			203,
+			9061,
+			false,
+			Reagent.Bloodmoss,
+			Reagent.MandrakeRoot,
+			Reagent.Nightshade
+		);
 
-        public override SpellCircle Circle
-        {
-            get { return SpellCircle.Second; }
-        }
-
-		public override double CastDelay{ get{ return 1.0; } }
-		public override double RequiredSkill{ get{ return 30.0; } }
-		public override int RequiredMana{ get{ return 30; } }
-
-		public DruidHollowReedSpell( Mobile caster, Item scroll ) : base( caster, scroll, m_Info )
+		public override SpellCircle Circle
 		{
-			                    if (this.Scroll != null)
-                        Scroll.Consume();
+			get { return SpellCircle.Second; }
+		}
+
+		public override double CastDelay { get { return 1.0; } }
+		public override double RequiredSkill { get { return 30.0; } }
+		public override int RequiredMana { get { return 30; } }
+
+		public DruidHollowReedSpell(Mobile caster, Item scroll) : base(caster, scroll, m_Info)
+		{
+			if (this.Scroll != null)
+				Scroll.Consume();
 		}
 
 		public override void OnCast()
 		{
-			Caster.Target = new InternalTarget( this );
+			Caster.Target = new InternalTarget(this);
 		}
 
-		public void Target( Mobile m )
+		public void Target(Mobile m)
 		{
-			if ( !Caster.CanSee( m ) )
+			if (!Caster.CanSee(m))
 			{
-				Caster.SendLocalizedMessage( 500237 ); // Target can not be seen.
+				Caster.SendLocalizedMessage(500237); // Target can not be seen.
 			}
-			else if ( CheckBSequence( m ) )
+			else if (CheckBSequence(m))
 			{
-				SpellHelper.Turn( Caster, m );
-				SpellHelper.AddStatBonus( Caster, m, true, StatType.Str );
-				SpellHelper.AddStatBonus( Caster, m, false, StatType.Dex );
+				SpellHelper.Turn(Caster, m);
+				SpellHelper.AddStatBonus(Caster, m, StatType.Str);
+				SpellHelper.DisableSkillCheck = true;
+				SpellHelper.AddStatBonus(Caster, m, StatType.Dex);
+				SpellHelper.AddStatBonus(Caster, m, StatType.Int);
+				SpellHelper.DisableSkillCheck = false;
 
-				m.PlaySound( 0x15 );
-				m.FixedParticles( 0x373A, 10, 15, 5018, EffectLayer.Waist );
+				m.PlaySound(0x15);
+				m.FixedParticles(0x373A, 10, 15, 5018, EffectLayer.Waist);
 			}
 
 			FinishSequence();
@@ -61,20 +62,20 @@ namespace Server.ACC.CSS.Systems.Druid
 		{
 			private DruidHollowReedSpell m_Owner;
 
-			public InternalTarget( DruidHollowReedSpell owner ) : base( 12, false, TargetFlags.Beneficial )
+			public InternalTarget(DruidHollowReedSpell owner) : base(12, false, TargetFlags.Beneficial)
 			{
 				m_Owner = owner;
 			}
 
-			protected override void OnTarget( Mobile from, object o )
+			protected override void OnTarget(Mobile from, object o)
 			{
-				if ( o is Mobile )
+				if (o is Mobile)
 				{
-					m_Owner.Target( (Mobile)o );
+					m_Owner.Target((Mobile)o);
 				}
 			}
 
-			protected override void OnTargetFinish( Mobile from )
+			protected override void OnTargetFinish(Mobile from)
 			{
 				m_Owner.FinishSequence();
 			}
