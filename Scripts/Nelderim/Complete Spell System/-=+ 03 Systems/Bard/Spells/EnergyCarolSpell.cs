547a273e68637f00ac7142dd2ac5e979bb8c59ba
Maciej Zarzycki <rapfamilly@gmail.com>
2021-09-21 09:30:05 +0200

    - nerf Elghina - wydluzenie trwania piesni barda dajacych resisty
    (cherry picked from commit 9e94b8753907fcffc3847c1a2c1c7b9019ce2f22)

--- Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Bard/Spells/EnergyCarolSpell.cs
+++ Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Bard/Spells/EnergyCarolSpell.cs
@@ -1,66 +1,64 @@
 using System;
 using System.Collections;
-using Server;
 using Server.Mobiles;
-using Server.Network;
-using Server.Items;
 using Server.Spells;
 
 namespace Server.ACC.CSS.Systems.Bard
 {
 	public class BardEnergyCarolSpell : BardSpell
 	{
-
 		private static SpellInfo m_Info = new SpellInfo(
-		                                                "Pobudzająca Pieśń", "Energious",
-		                                                //SpellCircle.First,
-		                                                212,9041
-		                                               );
+			"Pobudzająca Pieśń", "Energious",
+			//SpellCircle.First,
+			212, 9041
+		);
 
-        public override SpellCircle Circle
-        {
-            get { return SpellCircle.First; }
-        }
-		
-		public override double CastDelay{ get{ return 3; } }
-		public override double RequiredSkill{ get{ return 30.0; } }
-		public override int RequiredMana{ get{ return 12; } }
-		public BardEnergyCarolSpell( Mobile caster, Item scroll ) : base( caster, scroll, m_Info )
+		public override SpellCircle Circle
 		{
-			                    if (this.Scroll != null)
-                        Scroll.Consume();
+			get { return SpellCircle.First; }
+		}
+
+		public override double CastDelay { get { return 3; } }
+		public override double RequiredSkill { get { return 30.0; } }
+		public override int RequiredMana { get { return 12; } }
+
+		public BardEnergyCarolSpell(Mobile caster, Item scroll) : base(caster, scroll, m_Info)
+		{
+			if (this.Scroll != null)
+				Scroll.Consume();
 		}
 
 		public override void OnCast()
 		{
-			if( CheckSequence() )
+			if (CheckSequence())
 			{
 				ArrayList targets = new ArrayList();
 
-				foreach ( Mobile m in Caster.GetMobilesInRange( 3 ) )
+				foreach (Mobile m in Caster.GetMobilesInRange(3))
 				{
-					if ( Caster.CanBeBeneficial( m, false, true ) && !(m is Golem) )
-						targets.Add( m );
+					if (Caster.CanBeBeneficial(m, false, true) && !(m is Golem))
+						targets.Add(m);
 				}
-				for ( int i = 0; i < targets.Count; ++i )
+
+				for (int i = 0; i < targets.Count; ++i)
 				{
 					Mobile m = (Mobile)targets[i];
-					if ( m.BeginAction( typeof( BardEnergyCarolSpell ) ) )
+					if (m.BeginAction(typeof(BardEnergyCarolSpell)))
 					{
-						TimeSpan duration = TimeSpan.FromSeconds( Caster.Skills[SkillName.Provocation].Value * 0.3 );
-						int amount = (int)( Caster.Skills[SkillName.Musicianship].Value * .125 );
+						TimeSpan duration = TimeSpan.FromSeconds(Caster.Skills[CastSkill].Value * 5.0);
+						int amount = (int)(Caster.Skills[SkillName.Musicianship].Value * .125);
 
-						m.SendMessage( "Twoja odporność na energię wzrasta." );
-						ResistanceMod mod1 = new ResistanceMod( ResistanceType.Energy, + amount );
+						m.SendMessage("Twoja odporność na energię wzrasta.");
+						ResistanceMod mod1 = new ResistanceMod(ResistanceType.Energy, +amount);
 
-						m.FixedParticles( 0x373A, 10, 15, 5012, 0x14, 3, EffectLayer.Waist );
-						m.AddResistanceMod( mod1 );
+						m.FixedParticles(0x373A, 10, 15, 5012, 0x14, 3, EffectLayer.Waist);
+						m.AddResistanceMod(mod1);
 
-						new ExpireTimer( m, mod1, duration ).Start();
+						new ExpireTimer(m, mod1, duration).Start();
 					}
 				}
 			}
-			
+
 			FinishSequence();
 		}
 
@@ -69,7 +67,7 @@ namespace Server.ACC.CSS.Systems.Bard
 			private Mobile m_Mobile;
 			private ResistanceMod m_Mods;
 
-			public ExpireTimer( Mobile m, ResistanceMod mod, TimeSpan delay ) : base( delay )
+			public ExpireTimer(Mobile m, ResistanceMod mod, TimeSpan delay) : base(delay)
 			{
 				m_Mobile = m;
 				m_Mods = mod;
@@ -77,16 +75,16 @@ namespace Server.ACC.CSS.Systems.Bard
 
 			public void DoExpire()
 			{
-				m_Mobile.RemoveResistanceMod( m_Mods );
-				m_Mobile.EndAction( typeof( BardEnergyCarolSpell ) );
+				m_Mobile.RemoveResistanceMod(m_Mods);
+				m_Mobile.EndAction(typeof(BardEnergyCarolSpell));
 				Stop();
 			}
- 
+
 			protected override void OnTick()
 			{
-				if ( m_Mobile != null )
+				if (m_Mobile != null)
 				{
-					m_Mobile.SendMessage( "Efekt pieśni wygasa." );
+					m_Mobile.SendMessage("Efekt pieśni wygasa.");
 					DoExpire();
 				}
 			}
--- Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Bard/Spells/IceCarolSpell.cs
+++ Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Bard/Spells/IceCarolSpell.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections;
-using Server;
 using Server.Mobiles;
-using Server.Network;
-using Server.Items;
 using Server.Spells;
 
 namespace Server.ACC.CSS.Systems.Bard
@@ -11,67 +8,67 @@ namespace Server.ACC.CSS.Systems.Bard
 	public class BardIceCarolSpell : BardSpell
 	{
 		private static SpellInfo m_Info = new SpellInfo(
-		                                                "Pieśń Lodu", "Insulatus",
-		                                                //SpellCircle.First,
-		                                                212,9041
-		                                               );
+			"Pieśń Lodu", "Insulatus",
+			//SpellCircle.First,
+			212, 9041
+		);
 
-        public override SpellCircle Circle
-        {
-            get { return SpellCircle.First; }
-        }
-
-		public override double CastDelay{ get{ return 3; } }
-		public override double RequiredSkill{ get{ return 30.0; } }
-		public override int RequiredMana{ get{ return 12; } }
-
-		public BardIceCarolSpell( Mobile caster, Item scroll) : base( caster, scroll, m_Info )
+		public override SpellCircle Circle
 		{
-			                    if (this.Scroll != null)
-                        Scroll.Consume();
+			get { return SpellCircle.First; }
+		}
+
+		public override double CastDelay { get { return 3; } }
+		public override double RequiredSkill { get { return 30.0; } }
+		public override int RequiredMana { get { return 12; } }
+
+		public BardIceCarolSpell(Mobile caster, Item scroll) : base(caster, scroll, m_Info)
+		{
+			if (this.Scroll != null)
+				Scroll.Consume();
 		}
 
 		public override void OnCast()
 		{
-			if( CheckSequence() )
+			if (CheckSequence())
 			{
 				ArrayList targets = new ArrayList();
 
-				foreach ( Mobile m in Caster.GetMobilesInRange( 3 ) )
+				foreach (Mobile m in Caster.GetMobilesInRange(3))
 				{
-					if ( Caster.CanBeBeneficial( m, false, true ) && !(m is Golem) )
-						targets.Add( m );
+					if (Caster.CanBeBeneficial(m, false, true) && !(m is Golem))
+						targets.Add(m);
 				}
 
-				for ( int i = 0; i < targets.Count; ++i )
+				for (int i = 0; i < targets.Count; ++i)
 				{
 					Mobile m = (Mobile)targets[i];
-					if ( m.BeginAction( typeof( BardIceCarolSpell ) ) )
+					if (m.BeginAction(typeof(BardIceCarolSpell)))
 					{
+						TimeSpan duration = TimeSpan.FromSeconds(Caster.Skills[CastSkill].Value * 5.0);
+						int amount = (int)(Caster.Skills[SkillName.Musicianship].Value * .125);
 
-					TimeSpan duration = TimeSpan.FromSeconds( Caster.Skills[SkillName.Provocation].Value * 0.3 );
-					int amount = (int)( Caster.Skills[SkillName.Musicianship].Value * .125 );
+						m.SendMessage("Twoja odporność na zimno wzrasta.");
+						ResistanceMod mod1 = new ResistanceMod(ResistanceType.Cold, +amount);
 
-					m.SendMessage( "Twoja odporność na zimno wzrasta." );
-					ResistanceMod mod1 = new ResistanceMod( ResistanceType.Cold, + amount );
+						m.AddResistanceMod(mod1);
 
-					m.AddResistanceMod( mod1 );
+						m.FixedParticles(0x373A, 10, 15, 5012, 0x480, 3, EffectLayer.Waist);
 
-					m.FixedParticles( 0x373A, 10, 15, 5012, 0x480, 3, EffectLayer.Waist );
-
-					new ExpireTimer( m, mod1, duration ).Start();
+						new ExpireTimer(m, mod1, duration).Start();
+					}
 				}
-			}
 
-			FinishSequence();
+				FinishSequence();
+			}
 		}
-		}
+
 		private class ExpireTimer : Timer
 		{
 			private Mobile m_Mobile;
 			private ResistanceMod m_Mods;
 
-			public ExpireTimer( Mobile m, ResistanceMod mod, TimeSpan delay ) : base( delay )
+			public ExpireTimer(Mobile m, ResistanceMod mod, TimeSpan delay) : base(delay)
 			{
 				m_Mobile = m;
 				m_Mods = mod;
@@ -79,17 +76,17 @@ namespace Server.ACC.CSS.Systems.Bard
 
 			public void DoExpire()
 			{
-				m_Mobile.RemoveResistanceMod( m_Mods );
-				m_Mobile.EndAction( typeof( BardIceCarolSpell ) );
+				m_Mobile.RemoveResistanceMod(m_Mods);
+				m_Mobile.EndAction(typeof(BardIceCarolSpell));
 
 				Stop();
 			}
 
 			protected override void OnTick()
 			{
-				if ( m_Mobile != null )
+				if (m_Mobile != null)
 				{
-					m_Mobile.SendMessage( "Efekt pieśni wygasa." );
+					m_Mobile.SendMessage("Efekt pieśni wygasa.");
 					DoExpire();
 				}
 			}
