6f44e68306d81b2cbc9b188246b03869254f277f
Maciej Zarzycki <rapfamilly@gmail.com>
2021-11-16 10:51:15 +0100

    fix na townvariables

--- Scripts/Nelderim/Engines/Towns/TownVariables.cs
+++ Scripts/Nelderim/Engines/Towns/TownVariables.cs
@@ -145,7 +145,7 @@ namespace Nelderim.Towns
             typeof( SpidersSilk ), typeof( SulfurousAsh ) , typeof( Nightshade ) ,
             typeof( MandrakeRoot ) , typeof( Ginseng ) , typeof( Garlic ) , typeof( Bloodmoss ),
             typeof( BlackPearl ), typeof( BatWing ), typeof( GraveDust ), typeof( DaemonBlood ),
-            typeof( NoxCrystal ), typeof( PigIron ), typeof( SpringWater ), typeof( DestroyingAngel ), typeof( PetrafiedWood ), typeof( TaintedMushroom );, typeof( HornOfTheDreadhorn )
+            typeof( NoxCrystal ), typeof( PigIron ), typeof( SpringWater ), typeof( DestroyingAngel ), typeof( PetrafiedWood ), typeof( TaintedMushroom ), typeof( HornOfTheDreadhorn )
         };
         #endregion
 
