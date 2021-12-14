8a3a22b188cb0915dacf7c857efdb74d580262cd
Kaczy <kaczyf93@gmail.com>
2021-12-14 22:55:16 +0100

    Fixy na mikolaja
    (cherry picked from commit 62a6d7bfbb0de5822da6e25063a06cbd3f2c20ab)

--- Scripts/Nelderim/Mobiles/Monsters/Swiateczne/MikolajBoss.cs
+++ Scripts/Nelderim/Mobiles/Monsters/Swiateczne/MikolajBoss.cs
@@ -7,9 +7,9 @@ namespace Server.Mobiles.Swiateczne
 {
 	public class MikolajBoss : BaseCreature
 	{
+		private readonly int m_MaxAbilityInterval = 3; //seconds
+		private readonly int m_MinAbilityInterval = 1; //seconds
 		private DateTime m_NextAbilityTime;
-		private int m_MinAbilityInterval = 1; //seconds
-		private int m_MaxAbilityInterval = 3; //seconds
 
 		[Constructable]
 		public MikolajBoss() : base(AIType.AI_Boss, FightMode.Closest, 12, 1, 0.25, 0.5)
@@ -36,17 +36,60 @@ namespace Server.Mobiles.Swiateczne
 			SetResistance(ResistanceType.Poison, 75);
 			SetResistance(ResistanceType.Energy, 75);
 
-			SetSkill(SkillName.MagicResist, 110.0, 110.0);
-			SetSkill(SkillName.Tactics, 100.0, 100.0);
-			SetSkill(SkillName.Wrestling, 100.0, 100.0);
-			SetSkill(SkillName.EvalInt, 120.0, 120.0);
-			SetSkill(SkillName.Magery, 120.0, 120.0);
+			SetSkill(SkillName.MagicResist, 120.0, 120.0);
+			SetSkill(SkillName.Tactics, 120.0, 120.0);
+			SetSkill(SkillName.Wrestling, 120.0, 120.0);
+			SetSkill(SkillName.Anatomy, 120.0, 120.0);
+			SetSkill(SkillName.EvalInt, 80.0, 100.0);
+			SetSkill(SkillName.Magery, 80.0, 100.0);
 
 			Fame = 22500;
 			Karma = 22500;
 
 			VirtualArmor = 80;
 			AddItem(new LightSource());
+
+			AddItem(new FancyShirt(0x20));
+			AddItem(new Surcoat(0x20));
+			AddItem(new LongPants(0x20));
+			AddItem(new FurCape(0x497));
+			AddItem(new ElvenBoots());
+
+			HairItemID = 0x203C;
+			HairHue = 0x47F;
+
+			FacialHairItemID = 0x204B;
+			FacialHairHue = 0x47F;
+		}
+		// public override Poison HitPoison { get { return Poison.Lethal; } }
+
+		public MikolajBoss(Serial serial) : base(serial)
+		{
+		}
+
+		public override bool BardImmune
+		{
+			get { return true; }
+		}
+
+		public override double AttackMasterChance
+		{
+			get { return 0.15; }
+		}
+
+		public override double SwitchTargetChance
+		{
+			get { return 0.15; }
+		}
+
+		public override double DispelDifficulty
+		{
+			get { return 135.0; }
+		}
+
+		public override Poison PoisonImmune
+		{
+			get { return Poison.Lethal; }
 		}
 
 		public override void OnThink()
@@ -64,22 +107,14 @@ namespace Server.Mobiles.Swiateczne
 
 		private void ThrowSnowball()
 		{
-			HashSet<Mobile> targets = new HashSet<Mobile>();
-			foreach (AggressorInfo aggressorInfo in Aggressors)
-			{
-				targets.Add(aggressorInfo.Attacker);
-			}
-
-			foreach (AggressorInfo aggressorInfo in Aggressed)
-			{
-				targets.Add(aggressorInfo.Defender);
-			}
+			var targets = new HashSet<Mobile>();
+			foreach (var aggressorInfo in Aggressors) targets.Add(aggressorInfo.Attacker);
+			foreach (var aggressorInfo in Aggressed) targets.Add(aggressorInfo.Defender);
 
 			if (targets.Count > 0)
 			{
 				PublicOverheadMessage(MessageType.Regular, 0, 1007149); //Ho ho ho!
-				foreach (Mobile target in targets)
-				{
+				foreach (var target in targets)
 					if (Utility.RandomDouble() > 0.8)
 					{
 						PlaySound(0x145);
@@ -89,33 +124,21 @@ namespace Server.Mobiles.Swiateczne
 						Effects.SendMovingEffect(this, target, 0x36E4, 7, 0, false, true, 0x480, 0);
 						AOS.Damage(target, Utility.RandomMinMax(5, 10), 0, 0, 100, 0, 0);
 					}
-				}
 			}
-		}
-
-		public override bool BardImmune { get { return true; } }
-		public override double AttackMasterChance { get { return 0.15; } }
-		public override double SwitchTargetChance { get { return 0.15; } }
-		public override double DispelDifficulty { get { return 135.0; } }
-		public override Poison PoisonImmune { get { return Poison.Lethal; } }
-		public override Poison HitPoison { get { return Poison.Lethal; } }
-
-		public MikolajBoss(Serial serial) : base(serial)
-		{
 		}
 
 		public override void Serialize(GenericWriter writer)
 		{
 			base.Serialize(writer);
 
-			writer.Write((int)0); // version
+			writer.Write(0); // version
 		}
 
 		public override void Deserialize(GenericReader reader)
 		{
 			base.Deserialize(reader);
 
-			int version = reader.ReadInt();
+			var version = reader.ReadInt();
 		}
 	}
 }
