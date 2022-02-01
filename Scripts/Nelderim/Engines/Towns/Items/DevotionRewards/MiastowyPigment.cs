754bef0489c88c072bd0e61ff3efc494f81601fd
Kaczy <kaczyf93@gmail.com>
2022-02-01 21:06:26 +0100

    PigmentTasandora

---
+++ Scripts/Nelderim/Engines/Towns/Items/DevotionRewards/MiastowyPigment.cs
@@ -0,0 +1,31 @@
+﻿namespace Server.Items
+{
+	public class PigmentTasandora : BasePigment
+	{
+		[Constructable]
+		public PigmentTasandora()
+			: base((int)PigmentTarget.Cloth, 5, 2894)
+		{
+		}
+
+		[Constructable]
+		public PigmentTasandora(int uses)
+			: base((int)PigmentTarget.Cloth, uses, 2894)
+		{
+		}
+
+		public override void Serialize(GenericWriter writer)
+		{
+			base.Serialize(writer);
+
+			writer.Write(0);
+		}
+
+		public override void Deserialize(GenericReader reader)
+		{
+			base.Deserialize(reader);
+
+			int version = reader.ReadInt();
+		}
+	}
+}
