63cc0af453e8deb66a22b5540429389e10a1a956
Kaczy <kaczyf93@gmail.com>
2022-02-01 20:34:48 +0100

    Dodanie szansy na bycie kobieta do populacji w NelderimRegions

--- Scripts/Nelderim/Engines/RegionsEngine/Population.cs
+++ Scripts/Nelderim/Engines/RegionsEngine/Population.cs
@@ -1,48 +1,47 @@
 // 05.05.11 :: troyan
 
-using System;
-using System.Collections;
-using System.Xml;
-using Server;
-using Server.Mobiles;
-using Server.Items;
-
 namespace Server.Nelderim
 {
-	
 	public class Population
 	{
 		private double[] m_Proportions;
-		
-		public Population ( double[] proportions )
+		private double m_Female;
+
+		public Population(double[] proportions, double female)
 		{
 			m_Proportions = proportions;
+			m_Female = female;
 		}
-		
+
 		public Race GetRace
 		{
 			get
 			{
-				double rand = Server.Utility.Random( 0, 99 );
-                double cumsum = 0;
-                int index = 0;
-				
-				for( int i = 0; i < Race.AllRaces.Count; i++)
+				double rand = Server.Utility.Random(0, 99);
+				double cumsum = 0;
+				int index = 0;
+
+				for (int i = 0; i < Race.AllRaces.Count; i++)
 				{
-					if (  ( cumsum += m_Proportions[i] ) > rand )
+					if ((cumsum += m_Proportions[i]) > rand)
 					{
 						index = i;
 						break;
 					}
 				}
 
-                return Race.AllRaces[ index ];
+				return Race.AllRaces[index];
 			}
 		}
-		
+
 		public double[] Proportions
 		{
 			get { return m_Proportions; }
+		}
+
+		public double Female
+		{
+			get { return m_Female; }
 		}
 	}
 }
