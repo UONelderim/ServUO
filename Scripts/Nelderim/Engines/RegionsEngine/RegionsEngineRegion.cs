f6eeb8493e919224846d049f287fcc4ad56df779
Kaczy <kaczyf93@gmail.com>
2022-02-01 20:34:48 +0100

    Dodanie szansy na bycie kobieta do populacji w NelderimRegions

--- Scripts/Nelderim/Engines/RegionsEngine/RegionsEngineRegion.cs
+++ Scripts/Nelderim/Engines/RegionsEngine/RegionsEngineRegion.cs
@@ -10,9 +10,9 @@ namespace Server.Nelderim
 		private bool[] m_Followers;
 		private int m_TameLimit;
 		private int[] m_Intolerance;
-        private double[] m_ResourceVeins;
-		
-		public RegionsEngineRegion( string name, string parent )
+		private double[] m_ResourceVeins;
+
+		public RegionsEngineRegion(string name, string parent)
 		{
 			m_Name = name;
 			m_Parent = parent;
@@ -22,95 +22,102 @@ namespace Server.Nelderim
 			m_Followers = null;
 			m_TameLimit = 0;
 			m_Intolerance = null;
-            m_ResourceVeins = null;
+			m_ResourceVeins = null;
 		}
-		
+
 		public string Name
 		{
 			get { return m_Name; }
 		}
-		
+
 		public string Parent
 		{
 			get { return m_Parent; }
 		}
-		
+
 		public Population RegionPopulation
 		{
 			get { return m_Population; }
 			set { m_Population = value; }
 		}
-		
+
 		public Race GetRace
 		{
 			get { return m_Population.GetRace; }
 		}
-		
+
+
+		public double GetFemaleChance
+		{
+			get { return m_Population.Female; }
+		}
+
 		public GuardEngine[] Guards
 		{
 			get { return m_Guards; }
 			set { m_Guards = value; }
 		}
-		
+
 		public bool[] Schools
 		{
 			get { return m_Schools; }
 			set { m_Schools = value; }
 		}
-		
+
 		public bool MageryIsBanned
 		{
 			get { return m_Schools[0]; }
 			set { m_Schools[0] = value; }
 		}
-		
+
 		public bool ChivalryIsBanned
 		{
 			get { return m_Schools[1]; }
 			set { m_Schools[1] = value; }
 		}
-		
+
 		public bool NecromantionIsBanned
 		{
 			get { return m_Schools[2]; }
 			set { m_Schools[2] = value; }
 		}
+
 		public bool DruidismIsBanned
 		{
 			get { return m_Schools[3]; }
 			set { m_Schools[3] = value; }
 		}
-		
+
 		public bool[] BannedFollowers
 		{
 			get { return m_Followers; }
 			set { m_Followers = value; }
 		}
-		
+
 		public bool SummonsAreBanned
 		{
 			get { return m_Followers[0]; }
 			set { m_Followers[0] = value; }
 		}
-		
+
 		public bool FamiliarsAreBanned
 		{
 			get { return m_Followers[1]; }
 			set { m_Followers[1] = value; }
 		}
-		
+
 		public bool PetsAreBanned
 		{
 			get { return m_Followers[2]; }
 			set { m_Followers[2] = value; }
 		}
-		
+
 		public int TameLimit
 		{
 			get { return m_TameLimit; }
 			set { m_TameLimit = value; }
 		}
-		
+
 		public int[] Intolerance
 		{
 			get { return m_Intolerance; }
@@ -122,6 +129,5 @@ namespace Server.Nelderim
 			get { return m_ResourceVeins; }
 			set { m_ResourceVeins = value; }
 		}
-
- 	}
+	}
 }
