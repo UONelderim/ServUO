f644522e0384bd96b9b5a3e55467fffdcc75eeba
Kaczy <kaczyf93@gmail.com>
2022-02-01 16:40:32 +0100

    Kolejna porcja porzadkow w regionach
    (cherry picked from commit 4ec5c9d44590d66659c5b947a9f54f6b5f30525c)

---
+++ Scripts/Nelderim/Regions/GeographicRegion.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml;
+
+namespace Server.Regions
+{
+	public class GeographicRegion : NelderimRegion
+	{
+		public GeographicRegion(XmlElement xml, Map map, Region parent) : base(xml, map, parent)
+		{
+		}
+
+		public override bool AllowHousing(Mobile from, Point3D p)
+		{
+			return false;
+		}
+	}
+}
---
+++ Scripts/Nelderim/Regions/SpawnRegion.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml;
+
+namespace Server.Regions
+{
+	public class SpawnRegion : NelderimRegion
+	{
+		public SpawnRegion(XmlElement xml, Map map, Region parent) : base(xml, map, parent)
+		{
+		}
+	}
+}
