0867da28eaac0cd0068eede856d74b4ae1154567
Kaczy <kaczyf93@gmail.com>
2022-02-02 21:36:56 +0100

    AllowGhosts dla teleporterow

---
+++ Scripts/Nelderim/Items/Internal/TeleporterExt.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using Nelderim;
+
+namespace Server.Items
+{
+	public partial class Teleporter
+	{
+		[CommandProperty(AccessLevel.GameMaster)]
+		public bool AllowGhosts
+		{
+			get => TeleporterExt.Get(this).AllowGhosts;
+			set => TeleporterExt.Get(this).AllowGhosts = value;
+		}
+	}
+
+	class TeleporterExt : NExtension<TeleporterExtInfo>
+	{
+		public static string ModuleName = "Teleporter";
+
+		public static void Initialize()
+		{
+			EventSink.WorldSave += Save;
+			Load(ModuleName);
+		}
+
+		public static void Save(WorldSaveEventArgs args)
+		{
+			Cleanup();
+			Save(args, ModuleName);
+		}
+
+		private static void Cleanup()
+		{
+			List<Serial> toRemove = new List<Serial>();
+			foreach (KeyValuePair<Serial, TeleporterExtInfo> kvp in m_ExtensionInfo)
+			{
+				if (World.FindItem(kvp.Key) == null)
+					toRemove.Add(kvp.Key);
+			}
+
+			foreach (Serial serial in toRemove)
+			{
+				m_ExtensionInfo.Remove(serial);
+			}
+		}
+	}
+
+	class TeleporterExtInfo : NExtensionInfo
+	{
+		public bool AllowGhosts { get; set; }
+
+		public TeleporterExtInfo()
+		{
+			AllowGhosts = true;
+		}
+
+		public override void Deserialize(GenericReader reader)
+		{
+			AllowGhosts = reader.ReadBool();
+		}
+
+		public override void Serialize(GenericWriter writer)
+		{
+			writer.Write(AllowGhosts);
+		}
+	}
+}
