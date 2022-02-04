796e6a3ca7ef8f5562356a95f5bcb542dbd3695b
Kaczy <kaczyf93@gmail.com>
2022-02-04 21:58:05 +0100

    Prog taktyki dla worga 110

--- Scripts/Nelderim/Mobiles/NewPets/Worg.cs
+++ Scripts/Nelderim/Mobiles/NewPets/Worg.cs
@@ -6,7 +6,7 @@ namespace Server.Mobiles
 	public class Worg : BaseMount
 	{
 		[Constructable]
-		public Worg() : this("worg")
+		public Worg() : this("Worg")
 		{
 		}
 
@@ -15,11 +15,11 @@ namespace Server.Mobiles
 		{
 			double chance = Utility.RandomDouble() * 23301;
 
-			if ( chance <= 1 )
+			if (chance <= 1)
 				Hue = 0x489;
-			else if ( chance < 50 )
+			else if (chance < 50)
 				Hue = Utility.RandomList(0x657, 0x515, 0x4B1, 0x481, 0x482, 0x455);
-			else if ( chance < 500 )
+			else if (chance < 500)
 				Hue = Utility.RandomList(0x97A, 0x978, 0x901, 0x8AC, 0x5A7, 0x527);
 
 			SetStr(600, 630);
@@ -44,19 +44,19 @@ namespace Server.Mobiles
 			SetSkill(SkillName.Anatomy, 65.5, 69.4);
 			//SetSkill( SkillName.Healing, 72.2, 98.9 );
 
-			Fame = 5000;  //Guessing here
-			Karma = 5000;  //Guessing here
+			Fame = 5000; //Guessing here
+			Karma = 5000; //Guessing here
 
 			Tamable = true;
 			ControlSlots = 4;
 			MinTameSkill = 101.1;
 
-			if ( Utility.RandomDouble() < 0.2 )
+			if (Utility.RandomDouble() < 0.2)
 				PackItem(new TreasureMap(5, Map.Felucca));
 
 			// TODO 0-2 spellweaving scroll
 
-			if ( Utility.RandomDouble() < 0.2 )
+			if (Utility.RandomDouble() < 0.2)
 				PackItem(new ObsidianStone());
 
 			SetWeaponAbility(WeaponAbility.BleedAttack);
@@ -67,30 +67,11 @@ namespace Server.Mobiles
 			AddLoot(LootPack.FilthyRich, 5);
 		}
 
-		public override void OnDoubleClick(Mobile from)
-		{
-			/*
-            //if ( from.Race != Race.Elf && from == ControlMaster && from.AccessLevel == AccessLevel.Player )
-			if ( from.Race != Race.None && from == ControlMaster && from.AccessLevel == AccessLevel.Player )
-			{
-				Item pads = from.FindItemOnLayer( Layer.Shoes );
-				
-				if ( pads is PadsOfTheCuSidhe )
-					from.SendLocalizedMessage( 1071981 ); // Your boots allow you to mount the Cu Sidhe.
-				else
-				{
-					from.SendLocalizedMessage( 1072203 ); // Only Elves may use this.
-					return;
-				}
-			}*/
-
-			base.OnDoubleClick(from);
-		}
 		public override double GetControlChance(Mobile m, bool useBaseSkill)
 		{
 			double skill = (useBaseSkill ? m.Skills.Tactics.Base : m.Skills.Tactics.Value);
 
-			if ( skill >= 90.0 )
+			if (skill >= 110.0)
 				return 1.0;
 
 			return base.GetControlChance(m, useBaseSkill);
