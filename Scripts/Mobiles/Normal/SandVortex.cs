31a195fe6816799eedd7d52d22d781729ba8b77d
Kaczy <kaczyf93@gmail.com>
2022-02-05 12:43:04 +0100

    Sansza na Piasek i VolcanicAsh z SandVortexa

--- Scripts/Mobiles/Normal/SandVortex.cs
+++ Scripts/Mobiles/Normal/SandVortex.cs
@@ -1,108 +1,118 @@
-using Server.Items;
 using System;
+using Server.Items;
 
 namespace Server.Mobiles
 {
-    [CorpseName("zwloki piaskowego wiru")]
-    public class SandVortex : BaseCreature
-    {
-        private DateTime m_NextAttack;
-        [Constructable]
-        public SandVortex()
-            : base(AIType.AI_Melee, FightMode.Closest, 10, 1, 0.2, 0.4)
-        {
-            Name = "piaskowy wir";
-            Body = 790;
-            BaseSoundID = 263;
+	[CorpseName("zwloki piaskowego wiru")]
+	public class SandVortex : BaseCreature
+	{
+		private DateTime m_NextAttack;
 
-            SetStr(96, 120);
-            SetDex(171, 195);
-            SetInt(76, 100);
+		[Constructable]
+		public SandVortex()
+			: base(AIType.AI_Melee, FightMode.Closest, 10, 1, 0.2, 0.4)
+		{
+			Name = "piaskowy wir";
+			Body = 790;
+			BaseSoundID = 263;
 
-            SetHits(51, 62);
+			SetStr(96, 120);
+			SetDex(171, 195);
+			SetInt(76, 100);
 
-            SetDamage(3, 16);
+			SetHits(51, 62);
 
-            SetDamageType(ResistanceType.Physical, 90);
-            SetDamageType(ResistanceType.Fire, 10);
+			SetDamage(3, 16);
 
-            SetResistance(ResistanceType.Physical, 80, 90);
-            SetResistance(ResistanceType.Fire, 60, 70);
-            SetResistance(ResistanceType.Cold, 60, 70);
-            SetResistance(ResistanceType.Poison, 60, 70);
-            SetResistance(ResistanceType.Energy, 60, 70);
+			SetDamageType(ResistanceType.Physical, 90);
+			SetDamageType(ResistanceType.Fire, 10);
 
-            SetSkill(SkillName.MagicResist, 150.0);
-            SetSkill(SkillName.Tactics, 70.0);
-            SetSkill(SkillName.Wrestling, 80.0);
+			SetResistance(ResistanceType.Physical, 80, 90);
+			SetResistance(ResistanceType.Fire, 60, 70);
+			SetResistance(ResistanceType.Cold, 60, 70);
+			SetResistance(ResistanceType.Poison, 60, 70);
+			SetResistance(ResistanceType.Energy, 60, 70);
 
-            Fame = 4500;
-            Karma = -4500;
-        }
+			SetSkill(SkillName.MagicResist, 150.0);
+			SetSkill(SkillName.Tactics, 70.0);
+			SetSkill(SkillName.Wrestling, 80.0);
 
-        public SandVortex(Serial serial)
-            : base(serial)
-        {
-        }
+			Fame = 4500;
+			Karma = -4500;
+		}
 
-        public override void GenerateLoot()
-        {
-            AddLoot(LootPack.Meager, 2);
-            AddLoot(LootPack.LootItem<Bone>());
-        }
+		public SandVortex(Serial serial)
+			: base(serial)
+		{
+		}
 
-        public override void OnActionCombat()
-        {
-            Mobile combatant = Combatant as Mobile;
+		public override void GenerateLoot()
+		{
+			AddLoot(LootPack.Meager, 2);
+			AddLoot(LootPack.LootItem<Bone>());
+			if (!IsBonded && !IsChampionSpawn)
+			{
+				if (Utility.RandomDouble() < 0.10)
+					AddItem(new Sand());
+				if (Utility.RandomDouble() < 0.01)
+					AddItem(new VolcanicAsh());
+			}
+		}
 
-            if (combatant == null || combatant.Deleted || combatant.Map != Map || !InRange(combatant, 12) || !CanBeHarmful(combatant) || !InLOS(combatant))
-                return;
+		public override void OnActionCombat()
+		{
+			Mobile combatant = Combatant as Mobile;
 
-            if (DateTime.UtcNow >= m_NextAttack)
-            {
-                SandAttack(combatant);
-                m_NextAttack = DateTime.UtcNow + TimeSpan.FromSeconds(10.0 + (10.0 * Utility.RandomDouble()));
-            }
-        }
+			if (combatant == null || combatant.Deleted || combatant.Map != Map || !InRange(combatant, 12) ||
+			    !CanBeHarmful(combatant) || !InLOS(combatant))
+				return;
 
-        public void SandAttack(Mobile m)
-        {
-            DoHarmful(m);
+			if (DateTime.UtcNow >= m_NextAttack)
+			{
+				SandAttack(combatant);
+				m_NextAttack = DateTime.UtcNow + TimeSpan.FromSeconds(10.0 + (10.0 * Utility.RandomDouble()));
+			}
+		}
 
-            m.FixedParticles(0x36B0, 10, 25, 9540, 2413, 0, EffectLayer.Waist);
+		public void SandAttack(Mobile m)
+		{
+			DoHarmful(m);
 
-            new InternalTimer(m, this).Start();
-        }
+			m.FixedParticles(0x36B0, 10, 25, 9540, 2413, 0, EffectLayer.Waist);
 
-        public override void Serialize(GenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0); // version
-        }
+			new InternalTimer(m, this).Start();
+		}
 
-        public override void Deserialize(GenericReader reader)
-        {
-            base.Deserialize(reader);
-            int version = reader.ReadInt();
-        }
+		public override void Serialize(GenericWriter writer)
+		{
+			base.Serialize(writer);
+			writer.Write(0); // version
+		}
 
-        private class InternalTimer : Timer
-        {
-            private readonly Mobile m_Mobile;
-            private readonly Mobile m_From;
-            public InternalTimer(Mobile m, Mobile from)
-                : base(TimeSpan.FromSeconds(1.0))
-            {
-                m_Mobile = m;
-                m_From = from;
-                Priority = TimerPriority.TwoFiftyMS;
-            }
+		public override void Deserialize(GenericReader reader)
+		{
+			base.Deserialize(reader);
+			int version = reader.ReadInt();
+		}
 
-            protected override void OnTick()
-            {
-                m_Mobile.PlaySound(0x4CF);
-                AOS.Damage(m_Mobile, m_From, Utility.RandomMinMax(1, 40), 90, 10, 0, 0, 0);
-            }
-        }
-    }
+		private class InternalTimer : Timer
+		{
+			private readonly Mobile m_Mobile;
+			private readonly Mobile m_From;
+
+			public InternalTimer(Mobile m, Mobile from)
+				: base(TimeSpan.FromSeconds(1.0))
+			{
+				m_Mobile = m;
+				m_From = from;
+				Priority = TimerPriority.TwoFiftyMS;
+			}
+
+			protected override void OnTick()
+			{
+				m_Mobile.PlaySound(0x4CF);
+				AOS.Damage(m_Mobile, m_From, Utility.RandomMinMax(1, 40), 90, 10, 0, 0, 0);
+			}
+		}
+	}
 }
