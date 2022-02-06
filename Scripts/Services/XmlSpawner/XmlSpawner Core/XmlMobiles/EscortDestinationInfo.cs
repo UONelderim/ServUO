946fba95121fe165eee897326a6623d9ff8f128e
Kaczy <kaczyf93@gmail.com>
2022-02-06 12:17:44 +0100

    New version of xmlSpawner from https://github.com/ruaduck/xmlspawner

---
+++ Scripts/Services/XmlSpawner/XmlSpawner Core/XmlMobiles/EscortDestinationInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Server.Mobiles
+{
+	public class EscortDestinationInfo
+	{
+		public EscortDestinationInfo(string name, Region region)
+		{
+			Name = name;
+			Region = region;
+		}
+
+		public string Name { get; set; }
+		public Region Region { get; set; }
+
+		public static EscortDestinationInfo Find(string name)
+		{
+			if (String.IsNullOrEmpty(name))
+				return null;
+			Region reg = null;
+			foreach (var r in Region.Regions.Where(r => r.Name == name))
+				reg = r;
+
+			return new EscortDestinationInfo(name, reg);
+		}
+
+		public bool Contains(Point3D location)
+		{
+			return Region.Regions.Where(r => r.Name == Name).Any(r => r.Contains(location));
+		}
+	}
+}
---
+++ Scripts/Services/XmlSpawner/XmlSpawner Extras/XmlLevelItem/ILevelable.cs
@@ -0,0 +1,13 @@
+namespace Server.Items
+{
+	/// <summary>
+	/// Summary description for ILevelable.
+	/// </summary>
+	public interface ILevelable
+	{
+		int Experience { get; set; }
+		int Level { get; set; }
+		int Points { get; set; }
+		int MaxLevel { get; set; }
+	}
+}
