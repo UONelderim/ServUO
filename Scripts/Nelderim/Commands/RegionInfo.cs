b7efb8baa9f0693d0c8d6143b07d9001d635d51e
Kaczy <kaczyf93@gmail.com>
2022-02-06 16:21:09 +0100

    Komenda [regioninfo

---
+++ Scripts/Nelderim/Commands/RegionInfo.cs
@@ -0,0 +1,23 @@
+namespace Server.Commands
+{
+	public class RegionInfo
+	{
+		public static void Initialize()
+		{
+			CommandSystem.Register("regionInfo", AccessLevel.Administrator,
+				new CommandEventHandler(RegionInfo_OnCommand));
+		}
+
+		public static void RegionInfo_OnCommand(CommandEventArgs e)
+		{
+			Mobile from = e.Mobile;
+
+			from.SendMessage("Aktywny region: " + from.Region);
+			from.SendMessage("Wszystkie regiony w tym miejscu:");
+			foreach (var region in from.Map.Regions.Values)
+			{
+				if (region.Contains(from.Location)) from.SendMessage(region.Name);
+			}
+		}
+	}
+}
