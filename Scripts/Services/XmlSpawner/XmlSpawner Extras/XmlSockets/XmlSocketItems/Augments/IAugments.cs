3122a14bd4cd26fbb84ad5a4f76a903d6bd9658f
Kaczy <kaczyf93@gmail.com>
2022-02-06 12:17:44 +0100

    New version of xmlSpawner from https://github.com/ruaduck/xmlspawner

---
+++ Scripts/Services/XmlSpawner/XmlSpawner Extras/XmlSockets/XmlSocketItems/Augments/IAugments.cs
@@ -0,0 +1,18 @@
+namespace Server.Engines.XmlSpawner2
+{
+	public interface IAncientAugment
+	{
+	}
+
+	public interface ILegendaryAugment
+	{
+	}
+
+	public interface IMythicAugment
+	{
+	}
+
+	public interface ICrystalAugment
+	{
+	}
+}
