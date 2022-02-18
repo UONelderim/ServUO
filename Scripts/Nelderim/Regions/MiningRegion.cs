4d05e24d4bfe85943dec2a627003d670028f5b87
Kaczy <kaczyf93@gmail.com>
2022-02-18 22:12:17 +0100

    RegionalModifier dla gain chance

--- Scripts/Nelderim/Regions/MiningRegion.cs
+++ Scripts/Nelderim/Regions/MiningRegion.cs
@@ -1,10 +1,8 @@
-using System;
 using System.Xml;
-using Server;
 
 namespace Server.Regions
 {
-    public class MiningRegion : CityRegion
+    public class MiningRegion : NelderimRegion
     {
         public MiningRegion( XmlElement xml, Map map, Region parent ) : base( xml, map, parent )
         {
