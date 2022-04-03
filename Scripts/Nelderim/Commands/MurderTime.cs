ccd630f39997282ffc669813b84553134002ddc4
ev0lveDD <102914983+ev0lveDD@users.noreply.github.com>
2022-04-03 23:17:31 +0200

    Komenda [murdertime

---
+++ Scripts/Nelderim/Commands/MurderTime.cs
@@ -0,0 +1,25 @@
+﻿#region References
+
+using Server.Mobiles;
+
+#endregion
+
+namespace Server.Commands
+{
+	public class MurderTimeCommand
+	{
+		public static void Initialize()
+		{
+			CommandSystem.Register("murdertime", AccessLevel.Player, Status_OnCommand);
+		}
+
+		[Usage("MurderTime")]
+		[Description("Wyswietla informacje o czasie pozostałym do przedawnienia ostatniego morderstwa")]
+		public static void Status_OnCommand(CommandEventArgs e)
+		{
+			PlayerMobile pm = (PlayerMobile)e.Mobile;
+
+			pm.SendMessage("MurderTime: {0}", pm.LongTermElapse - pm.GameTime);
+		}
+	}
+}
