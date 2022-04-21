191f0689e7180048e466fa4622ea67baa6dadaea
Maciej Zarzycki <rapfamilly@gmail.com>
2022-04-21 12:12:04 +0200

    Achievement System - dodano; do przerobienia

---
+++ Scripts/Nelderim/Engines/MythikAchievmentSystem-master/Achievement.cs
@@ -0,0 +1,38 @@
+﻿using Server;
+using Server.Mobiles;
+using System;
+
+
+namespace Scripts.Mythik.Systems.Achievements
+{
+   
+    class AchieveData
+    {
+        //public int ID { get; set; }
+        public int Progress { get; set; }
+        public DateTime CompletedOn { get; set; }
+
+        public AchieveData()
+        {
+
+        }
+        public AchieveData(GenericReader reader)
+        {
+            Deserialize(reader);
+        }
+        public void Serialize(GenericWriter writer)
+        {
+            writer.Write(1); // version
+            writer.Write(Progress);
+            writer.Write(CompletedOn);
+
+        }
+        public void Deserialize(GenericReader reader)
+        {
+            int version = reader.ReadInt();
+            Progress = reader.ReadInt();
+            CompletedOn = reader.ReadDateTime();
+        }
+
+    }
+}
