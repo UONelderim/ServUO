65286b31f5c1752df1e4e12405003c7eb92996c3
Kaczy <kaczyf93@gmail.com>
2022-06-09 16:04:25 +0200

    SkillEstimates for all skills except combat

--- Scripts/Nelderim/Misc/SkillEstimate.cs
+++ Scripts/Nelderim/Misc/SkillEstimate.cs
@@ -5,6 +5,7 @@ using Server;
 using Server.Commands;
 using Server.Engines.Craft;
 using Server.Engines.Harvest;
+using Server.Items;
 using Server.Misc;
 using Server.Spells;
 using Server.Spells.Bushido;
@@ -71,65 +72,70 @@ namespace Nelderim
 			switch (skill.SkillName)
 			{
 				case SkillName.Alchemy: return OptimalCraftSuccessChance(fakeMob, DefAlchemy.CraftSystem);
-				case SkillName.Anatomy: return 0;
-				case SkillName.AnimalLore: return 0;
-				case SkillName.ItemID: return 0;
-				case SkillName.ArmsLore: return MinMaxSuccessChance(skill, 0, 100);
-				case SkillName.Parry: return 0;
-				case SkillName.Begging: return 0;
+				case SkillName.Anatomy: return MinMaxSuccessChance(skill);
+				case SkillName.AnimalLore: return MinMaxSuccessChance(skill);
+				case SkillName.ItemID: return 0; // Not relevant
+				case SkillName.ArmsLore: return MinMaxSuccessChance(skill);
+				case SkillName.Parry: return 0.5; ///At average should be around 50% if we train correctly
+				case SkillName.Begging: return 0; // Not relevant
 				case SkillName.Blacksmith: return OptimalCraftSuccessChance(fakeMob, DefBlacksmithy.CraftSystem);
 				case SkillName.Fletching: return OptimalCraftSuccessChance(fakeMob, DefBowFletching.CraftSystem);
-				case SkillName.Peacemaking: return 0.5; //optimal
-				case SkillName.Camping: return 0;
+				case SkillName.Peacemaking: return 0.5; //At average should be around 50% if we train correctly
+				case SkillName.Camping: return MinMaxSuccessChance(skill);
 				case SkillName.Carpentry: return OptimalCraftSuccessChance(fakeMob, DefCarpentry.CraftSystem);
 				case SkillName.Cartography: return OptimalCraftSuccessChance(fakeMob, DefCartography.CraftSystem);
 				case SkillName.Cooking: return OptimalCraftSuccessChance(fakeMob, DefCooking.CraftSystem);
-				case SkillName.DetectHidden: return 0;
-				case SkillName.Discordance: return 0.5; //optimal
-				case SkillName.EvalInt: return MinMaxSuccessChance(skill, 0, skill.Cap); //EvalInt assumes only direct skill use, without casting spells
-				case SkillName.Healing: return 0;
+				case SkillName.DetectHidden: return MinMaxSuccessChance(skill);
+				case SkillName.Discordance: return 0.5; //At average should be around 50% if we train correctly
+				case SkillName.EvalInt: return MinMaxSuccessChance(skill); //EvalInt assumes only direct skill use, without casting spells
+				case SkillName.Healing: return MinMaxSuccessChance(skill, 0, 120);
 				case SkillName.Fishing: return HarvestSuccessChance(fakeMob, skill, Fishing.System, Fishing.System.Definition);
-				case SkillName.Herbalism: return 0;
-				case SkillName.Herding: return 0;
-				case SkillName.Hiding: return 0;
-				case SkillName.Provocation: return 0.5; //optimal
+				case SkillName.Herbalism: return skill.Value < 85.0 ? MinMaxSuccessChance(skill, 0, 90) : MinMaxSuccessChance(skill, 80, 100);
+				case SkillName.Herding: return 0.5;
+				case SkillName.Hiding: return MinMaxSuccessChance(skill);
+				case SkillName.Provocation: return 0.5; ///At average should be around 50% if we train correctly
 				case SkillName.Inscribe: return OptimalCraftSuccessChance(fakeMob, DefInscription.CraftSystem);
-				case SkillName.Lockpicking: return 0;
+				case SkillName.Lockpicking: return 0.5; ///At average should be around 50% if we train correctly
 				case SkillName.Magery: return MagicalSuccessChance(fakeMob, skill);
-				case SkillName.MagicResist: return 0;
-				case SkillName.Tactics: return 0;
-				case SkillName.Snooping: return 0;
-				case SkillName.Musicianship: return 0;
-				case SkillName.Poisoning: return 0;
-				case SkillName.Archery: return 0;
-				case SkillName.SpiritSpeak: return 0;
-				case SkillName.Stealing: return 0;
+				case SkillName.MagicResist: return MinMaxSuccessChance(skill, 0, 120);
+				case SkillName.Tactics: return MinMaxSuccessChance(skill);
+				case SkillName.Snooping: return MinMaxSuccessChance(skill);
+				case SkillName.Musicianship: return MinMaxSuccessChance(skill, 0, 120);
+				case SkillName.Poisoning: return 0.5; ///At average should be around 50% if we train correctly
+				case SkillName.Archery: return 0.5; ///At average should be around 50% if we train correctly
+				case SkillName.SpiritSpeak: return MinMaxSuccessChance(skill, 0, 120);
+				case SkillName.Stealing: return 0.5; //At average should be around 50% if we train correctly
 				case SkillName.Tailoring: return OptimalCraftSuccessChance(fakeMob, DefTailoring.CraftSystem);
-				case SkillName.AnimalTaming: return 0.5; //optimal
-				case SkillName.TasteID: return 0;
+				case SkillName.AnimalTaming: return 0.5; //At average should be around 50% if we train correctly
+				case SkillName.TasteID: return 0; // Not relevant
 				case SkillName.Tinkering: return OptimalCraftSuccessChance(fakeMob, DefTinkering.CraftSystem);
-				case SkillName.Tracking: return 0;
-				case SkillName.Veterinary: return 0;
-				case SkillName.Swords: return 0;
-				case SkillName.Macing: return 0;
-				case SkillName.Fencing: return 0;
-				case SkillName.Wrestling: return 0;
+				case SkillName.Tracking: return MinMaxSuccessChance(skill); //It's not as simple as that, but it's good enough
+				case SkillName.Veterinary: return MinMaxSuccessChance(skill, 0, 120);
+				case SkillName.Swords: return 0.5; ///At average should be around 50% if we train correctly
+				case SkillName.Macing: return 0.5; ///At average should be around 50% if we train correctly
+				case SkillName.Fencing: return 0.5; ///At average should be around 50% if we train correctly
+				case SkillName.Wrestling: return 0.5; ///At average should be around 50% if we train correctly
 				case SkillName.Lumberjacking: return HarvestSuccessChance(fakeMob, skill, Lumberjacking.System, Lumberjacking.System.Definition);
 				case SkillName.Mining: return HarvestSuccessChance(fakeMob, skill, Mining.System, Mining.System.OreAndStone);
-				case SkillName.Meditation: return 0;
-				case SkillName.Stealth: return 0;
-				case SkillName.RemoveTrap: return 0;
+				case SkillName.Meditation: return 0; // Not relevant, trains itself
+				case SkillName.Stealth: return 0.5; //At average should be around 50% if we train correctly
+				case SkillName.RemoveTrap: return 0.5; //At average should be around 50% if we train correctly
 				case SkillName.Necromancy: return MagicalSuccessChance(fakeMob, skill);
-				case SkillName.Focus: return 0;
+				case SkillName.Focus: return 0; // Not relevant, trains itself
 				case SkillName.Chivalry: return MagicalSuccessChance(fakeMob, skill);
 				case SkillName.Bushido: return MagicalSuccessChance(fakeMob, skill);
 				case SkillName.Ninjitsu: return MagicalSuccessChance(fakeMob, skill);
 				case SkillName.Spellweaving: return MagicalSuccessChance(fakeMob, skill);
 				case SkillName.Mysticism: return MagicalSuccessChance(fakeMob, skill);
-				case SkillName.Imbuing: return 0;
-				case SkillName.Throwing: return 0;
+				case SkillName.Imbuing: return 0.5; //At average should be around 50% if we train correctly
+				case SkillName.Throwing: return 0.5; ///At average should be around 50% if we train correctly
 				default: throw new NotImplementedException();
 			}
+		}
+
+		private static double MinMaxSuccessChance(Skill skill)
+		{
+			return MinMaxSuccessChance(skill, 0, skill.Cap);
 		}
 
 		private static double MinMaxSuccessChance(Skill skill, double minSkill, double maxSkill)
@@ -240,62 +246,62 @@ namespace Nelderim
 			switch (skill.SkillName)
 			{
 				case SkillName.Alchemy: return DefAlchemy.CraftSystem.Delay;
-				case SkillName.Anatomy: return 0;
-				case SkillName.AnimalLore: return 0;
-				case SkillName.ItemID: return 0;
+				case SkillName.Anatomy: return 1;
+				case SkillName.AnimalLore: return 1;
+				case SkillName.ItemID: return 1;
 				case SkillName.ArmsLore: return 1;
 				case SkillName.Parry: return 0;
-				case SkillName.Begging: return 0;
+				case SkillName.Begging: return 10;
 				case SkillName.Blacksmith: return DefBlacksmithy.CraftSystem.Delay;
 				case SkillName.Fletching: return DefBowFletching.CraftSystem.Delay;
 				case SkillName.Peacemaking: return ChanceBasedAverage(GetOptimalSuccessChance(from, fakeMob, skill), 10, 5);
-				case SkillName.Camping: return 0;
+				case SkillName.Camping: return 2;
 				case SkillName.Carpentry: return DefCarpentry.CraftSystem.Delay;
 				case SkillName.Cartography: return DefCartography.CraftSystem.Delay;
 				case SkillName.Cooking: return DefCooking.CraftSystem.Delay;
-				case SkillName.DetectHidden: return 0;
+				case SkillName.DetectHidden: return 10;
 				case SkillName.Discordance: return ChanceBasedAverage(GetOptimalSuccessChance(from, fakeMob, skill), 8, 5);;
 				case SkillName.EvalInt: return 1;
-				case SkillName.Healing: return 0;
-				case SkillName.Fishing: return 0;
-				case SkillName.Herbalism: return 0;
-				case SkillName.Herding: return 0;
-				case SkillName.Hiding: return 0;
+				case SkillName.Healing: return BandageContext.GetDelay(from, fakeMob, false, skill.SkillName).TotalSeconds;
+				case SkillName.Fishing: return Fishing.System.Definition.EffectDelay.TotalSeconds;
+				case SkillName.Herbalism: return 0; 
+				case SkillName.Herding: return 0; // no delay
+				case SkillName.Hiding: return 10;
 				case SkillName.Provocation: return 10;
 				case SkillName.Inscribe: return DefInscription.CraftSystem.Delay;
-				case SkillName.Lockpicking: return 0;
+				case SkillName.Lockpicking: return 1.5;
 				case SkillName.Magery: return MagicalCastDelay(from, skill);
-				case SkillName.MagicResist: return 0;
+				case SkillName.MagicResist: return 0.5; // Lowest delay for magic resist effected spell
 				case SkillName.Tactics: return 0;
-				case SkillName.Snooping: return 0;
-				case SkillName.Musicianship: return 0;
-				case SkillName.Poisoning: return 0;
+				case SkillName.Snooping: return 0; // no delay
+				case SkillName.Musicianship: return 1;
+				case SkillName.Poisoning: return 10;
 				case SkillName.Archery: return 0;
-				case SkillName.SpiritSpeak: return 0;
-				case SkillName.Stealing: return 0;
+				case SkillName.SpiritSpeak: return 5;
+				case SkillName.Stealing: return 10;
 				case SkillName.Tailoring: return DefTailoring.CraftSystem.Delay;
 				case SkillName.AnimalTaming: return 4 * 3; //3-5 ticks, 3 seconds each
-				case SkillName.TasteID: return 0;
+				case SkillName.TasteID: return 1;
 				case SkillName.Tinkering: return DefTinkering.CraftSystem.Delay;
-				case SkillName.Tracking: return 0;
-				case SkillName.Veterinary: return 0;
+				case SkillName.Tracking: return 10;
+				case SkillName.Veterinary: return BandageContext.GetDelay(from, fakeMob, false, skill.SkillName).TotalSeconds;;
 				case SkillName.Swords: return 0;
 				case SkillName.Macing: return 0;
 				case SkillName.Fencing: return 0;
 				case SkillName.Wrestling: return 0;
-				case SkillName.Lumberjacking: return 0;
-				case SkillName.Mining: return 0;
-				case SkillName.Meditation: return 0;
-				case SkillName.Stealth: return 0;
-				case SkillName.RemoveTrap: return 0;
+				case SkillName.Lumberjacking: return Lumberjacking.System.Definition.EffectDelay.TotalSeconds;
+				case SkillName.Mining: return Mining.System.OreAndStone.EffectDelay.TotalSeconds;
+				case SkillName.Meditation: return 0.5; // Higher regen rate is 1/0.5s
+				case SkillName.Stealth: return skill.Value * 0.4 / 5 ; //One step takes 0.4 seconds, and each 5 skill grants 1 extra step
+				case SkillName.RemoveTrap: return 10;
 				case SkillName.Necromancy: return MagicalCastDelay(from, skill);
-				case SkillName.Focus: return 0;
+				case SkillName.Focus: return 0.5; // Higher regen rate is 1/0.5s
 				case SkillName.Chivalry: return MagicalCastDelay(from, skill);
 				case SkillName.Bushido: return MagicalCastDelay(from, skill);
 				case SkillName.Ninjitsu: return MagicalCastDelay(from, skill);
 				case SkillName.Spellweaving: return MagicalCastDelay(from, skill);
 				case SkillName.Mysticism: return MagicalCastDelay(from, skill);
-				case SkillName.Imbuing: return 0;
+				case SkillName.Imbuing: return 1;
 				case SkillName.Throwing: return 0;
 				default: throw new NotImplementedException();
 			}
