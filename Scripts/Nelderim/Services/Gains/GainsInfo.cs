993eb807fb8ed7efca6ce59824a5926226fe31fa
Kaczy <kaczyf93@gmail.com>
2022-08-19 14:20:35 +0200

    Gain Booster, refactor GlobalGainFactor i PowerHour, [kox -> [GainInfo start/stop

--- Scripts/Nelderim/Services/Gains/GainsInfo.cs
+++ Scripts/Nelderim/Services/Gains/GainsInfo.cs
@@ -10,44 +10,52 @@ namespace Nelderim.Gains
 	class GainsInfo : NExtensionInfo
 	{
 		private double _GainFactor = 1.0;
+		private Timer _GainBoostTimer;
+		private DateTime _GainBoostEndTime;
+
+		public bool GainDebug { get; set; }
 
 		public double GainFactor
 		{
 			get => _GainFactor;
-			set => _GainFactor = Math.Min(Math.Round(value, 1), 1.0);
+			set => _GainFactor = Math.Max(Math.Round(value, 1), 1.0);
 		}
 
-		public double StrGain { get; set; }
+		public DateTime GainBoostEndTime
+		{
+			get => _GainBoostEndTime;
+			set
+			{
+				_GainBoostTimer?.Stop();
+				
+				_GainBoostEndTime = value;
+				if(GainBoostEndTime > DateTime.Now)
+					_GainBoostTimer = Timer.DelayCall(GainBoostEndTime - DateTime.Now, () => GainFactor = 1.0);
+			}
+		}
 
-		public double DexGain { get; set; }
+		public bool ActivateGainBoost(double gainFactor, TimeSpan duration)
+		{
+			if (GainBoostEndTime > DateTime.Now) return false;
 
-		public double IntGain { get; set; }
+			GainFactor = gainFactor;
+			GainBoostEndTime = DateTime.Now + duration;
+			return true;
+		}
 
 		public override void Serialize(GenericWriter writer)
 		{
-			writer.Write( (int)1 ); //version
-			writer.Write(StrGain);
-			writer.Write(DexGain);
-			writer.Write(IntGain);
-			
+			writer.Write( (int)0 ); //version
+			writer.Write(GainFactor);
+			writer.Write(GainBoostEndTime - DateTime.Now);
 		}
 
 		public override void Deserialize(GenericReader reader)
 		{
-			int version = 0;
-			if (Fix)
-				version = reader.ReadInt(); //version
-			switch (version)
-			{
-				case 1:
-				case 0:
-					if(version == 0)
-						/*LastPowerHour =*/ reader.ReadDateTime();
-					StrGain = reader.ReadDouble();
-					DexGain = reader.ReadDouble();
-					IntGain = reader.ReadDouble();
-					break;
-			}
+			int version = reader.ReadInt(); //version
+			
+			GainFactor = reader.ReadDouble();
+			GainBoostEndTime = DateTime.Now + reader.ReadTimeSpan();
 		}
 	}
 }
