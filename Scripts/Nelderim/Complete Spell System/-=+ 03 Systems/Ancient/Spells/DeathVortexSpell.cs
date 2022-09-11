1cb56c75d81393588f5a18e9f0b73726d1e93f9b
Kaczy <kaczyf93@gmail.com>
2022-09-11 20:37:38 +0200

    Fix na ancient spelle

--- Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Ancient/Spells/DeathVortexSpell.cs
+++ Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Ancient/Spells/DeathVortexSpell.cs
@@ -1,47 +1,44 @@
 using System;
 using Server.Mobiles;
-using Server.Network;
 using Server.Targeting;
 using Server.Spells;
 
 namespace Server.ACC.CSS.Systems.Ancient
 {
-    public class AncientDeathVortexSpell : AncientSpell
-    {
-        public override double CastDelay { get { return 2.5; } }
-        public override double RequiredSkill { get { return 80.0; } }
-        public override int RequiredMana { get { return 50; } }
-        private static SpellInfo m_Info = new SpellInfo(
-                                                        "Wir Śmierci", "Vas Corp Hur",
-                                                        260,
-                                                        9032,
-                                                        false,
-                                                        Reagent.Bloodmoss,
-                                                        Reagent.SulfurousAsh,
-                                                        Reagent.MandrakeRoot,
-                                                        Reagent.Nightshade
-                                                       );
+	public class AncientDeathVortexSpell : AncientSpell
+	{
+		public override double CastDelay => 2.5;
+		public override double RequiredSkill => 80.0;
+		public override int RequiredMana => 50;
 
-        public override SpellCircle Circle
-        {
-            get { return SpellCircle.Eighth; }
-        }
+		private static SpellInfo m_Info = new SpellInfo(
+			"Wir Śmierci", "Vas Corp Hur",
+			260,
+			9032,
+			false,
+			Reagent.Bloodmoss,
+			Reagent.SulfurousAsh,
+			Reagent.MandrakeRoot,
+			Reagent.Nightshade
+		);
 
-        public AncientDeathVortexSpell(Mobile caster, Item scroll)
-            : base(caster, scroll, m_Info)
-        {
-                                if (this.Scroll != null)
-                        Scroll.Consume();
-        }
+		public override SpellCircle Circle => SpellCircle.Eighth;
 
-      public override bool CheckCast()
+		public AncientDeathVortexSpell(Mobile caster, Item scroll)
+			: base(caster, scroll, m_Info)
 		{
-			if ( !base.CheckCast() )
+			if (this.Scroll != null)
+				Scroll.Consume();
+		}
+
+		public override bool CheckCast()
+		{
+			if (!base.CheckCast())
 				return false;
 
-			if ( (Caster.Followers + (Core.SE ? 2 : 1)) > Caster.FollowersMax )
+			if ((Caster.Followers + (Core.SE ? 2 : 1)) > Caster.FollowersMax)
 			{
-				Caster.SendLocalizedMessage( 1049645 ); // You have too many followers to summon that creature.
+				Caster.SendLocalizedMessage(1049645); // You have too many followers to summon that creature.
 				return false;
 			}
 
@@ -50,64 +47,64 @@ namespace Server.ACC.CSS.Systems.Ancient
 
 		public override void OnCast()
 		{
-			Caster.Target = new InternalTarget( this );
+			Caster.Target = new InternalTarget(this);
 		}
 
 
-public void Target( IPoint3D p )
+		public void Target(IPoint3D p)
 		{
 			Map map = Caster.Map;
 
-			SpellHelper.GetSurfaceTop( ref p );
+			SpellHelper.GetSurfaceTop(ref p);
 
-			if ( map == null || !map.CanSpawnMobile( p.X, p.Y, p.Z ) )
+			if (map == null || !map.CanSpawnMobile(p.X, p.Y, p.Z))
 			{
-				Caster.SendLocalizedMessage( 501942 ); // That location is blocked.
+				Caster.SendLocalizedMessage(501942); // That location is blocked.
 			}
-			else if ( SpellHelper.CheckTown( p, Caster ) && CheckSequence() )
+			else if (SpellHelper.CheckTown(p, Caster) && CheckSequence())
 			{
 				TimeSpan duration;
 
-				if ( Core.AOS )
-					duration = TimeSpan.FromSeconds( 90.0 );
+				if (Core.AOS)
+					duration = TimeSpan.FromSeconds(90.0);
 				else
-					duration = TimeSpan.FromSeconds( Utility.Random( 80, 40 ) );
+					duration = TimeSpan.FromSeconds(Utility.Random(80, 40));
 
-				BaseCreature.Summon( new DeathVortex(), false, Caster, new Point3D( p ), 0x212, duration );
+				BaseCreature.Summon(new DeathVortex(), false, Caster, new Point3D(p), 0x212, duration);
 			}
 
 			FinishSequence();
 		}
 
-        private class InternalTarget : Target
-        {
-            private AncientDeathVortexSpell m_Owner;
+		private class InternalTarget : Target
+		{
+			private AncientDeathVortexSpell m_Owner;
 
-            public InternalTarget(AncientDeathVortexSpell owner)
-                : base(12, true, TargetFlags.None)
-            {
-                m_Owner = owner;
-            }
+			public InternalTarget(AncientDeathVortexSpell owner)
+				: base(12, true, TargetFlags.None)
+			{
+				m_Owner = owner;
+			}
 
-            protected override void OnTarget(Mobile from, object o)
-            {
-                if (o is IPoint3D)
-                    m_Owner.Target((IPoint3D)o);
-            }
+			protected override void OnTarget(Mobile from, object o)
+			{
+				if (o is IPoint3D)
+					m_Owner.Target((IPoint3D)o);
+			}
 
-            protected override void OnTargetOutOfLOS(Mobile from, object o)
-            {
-                from.SendLocalizedMessage(501943); // Target cannot be seen. Try again.
-                from.Target = new InternalTarget(m_Owner);
-                from.Target.BeginTimeout(from, TimeoutTime - DateTime.Now);
-                m_Owner = null;
-            }
+			protected override void OnTargetOutOfLOS(Mobile from, object o)
+			{
+				from.SendLocalizedMessage(501943); // Target cannot be seen. Try again.
+				from.Target = new InternalTarget(m_Owner);
+				from.Target.BeginTimeout(from, TimeoutTime - DateTime.Now);
+				m_Owner = null;
+			}
 
-            protected override void OnTargetFinish(Mobile from)
-            {
-                if (m_Owner != null)
-                    m_Owner.FinishSequence();
-            }
-        }
-    }
+			protected override void OnTargetFinish(Mobile from)
+			{
+				if (m_Owner != null)
+					m_Owner.FinishSequence();
+			}
+		}
+	}
 }
