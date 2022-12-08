28f432e053e0c4fbefeacba7516f20d36b1a636f
Maupishon <RapFamilly@gmail.com>
2022-12-08 12:07:14 +0100

    Będzie można naprawić BlazenskieSzczescie.cs

--- Scripts/Nelderim/Items/Artifacts/Old/BlazenskieSzczescie.cs
+++ Scripts/Nelderim/Items/Artifacts/Old/BlazenskieSzczescie.cs
@@ -1,7 +1,7 @@
 namespace Server.Items
 {
 	[FlipableAttribute(0x171C, 0x171C)]
-	public class BlazenskieSzczescie : BaseArmor
+	public class BlazenskieSzczescie : JesterHat
 	{
 		public override int LabelNumber { get { return 1065805; } } // Blazenskie Szczescie
 		public override int InitMinHits { get { return 60; } }
@@ -14,11 +14,7 @@ namespace Server.Items
 		public override int BaseEnergyResistance { get { return 5; } }
 
 		public override int StrReq { get { return 20; } }
-
-		public override ArmorMaterialType MaterialType { get { return ArmorMaterialType.Leather; } }
-		public override CraftResource DefaultResource { get { return CraftResource.RegularLeather; } }
-
-		public override ArmorMeditationAllowance DefMedAllowance { get { return ArmorMeditationAllowance.All; } }
+		
 
 		[Constructable]
 		public BlazenskieSzczescie() : base(0x171C)
