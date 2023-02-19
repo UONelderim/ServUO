8b9ca14eb7efb4d35116ca43b062a7091a96399d
Maupishon <RapFamilly@gmail.com>
2023-02-19 10:26:22 +0100

    Zmniejszenie zbyt dużego DMG od czaru SwarmofInsectsSpell.cs

--- Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Undead/Spells/SwarmofInsectsSpell.cs
+++ Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Undead/Spells/SwarmofInsectsSpell.cs
@@ -52,7 +52,7 @@ namespace Server.ACC.CSS.Systems.Undead
 				// m.FixedParticles( 0x91B, 1, 240, 9916, 0, 3, EffectLayer.Head );
 				m.PlaySound(0x1E5);
 
-				double damage = ((Caster.Skills[CastSkill].Value - m.Skills[SkillName.SpiritSpeak].Value) / 10) + 30;
+				double damage = ((Caster.Skills[CastSkill].Value - m.Skills[SkillName.SpiritSpeak].Value) / 10) + 3;
 
 				if (damage < 1)
 					damage = 1;
