c531bee01576bd3eec2427cbb21a794e81b6562d
kaczy93 <kaczy93@users.noreply.github.com>
2023-03-03 08:35:22 +0100

    Recycle Spellbooks wiith scissors

--- Scripts/Items/Equipment/Spellbooks/Spellbook.cs
+++ Scripts/Items/Equipment/Spellbooks/Spellbook.cs
@@ -32,7 +32,7 @@ namespace Server.Items
         Exceptional
     }
 
-    public class Spellbook : Item, ISpellbook, ICraftable, ISlayer, IEngravable, IVvVItem, IOwnerRestricted, IWearableDurability
+    public partial class Spellbook : Item, ISpellbook, ICraftable, ISlayer, IEngravable, IVvVItem, IOwnerRestricted, IWearableDurability
     {
         private static readonly Dictionary<Mobile, List<Spellbook>> m_Table = new Dictionary<Mobile, List<Spellbook>>();
 
