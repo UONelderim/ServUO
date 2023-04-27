6a31cc86ca9c74fd99857fead606b2517a7a0077
Maupishon <RapFamilly@gmail.com>
2023-04-27 15:27:15 +0200

    Add/Remove Decor

--- Scripts/Items/Decorative/Static.cs
+++ Scripts/Items/Decorative/Static.cs
@@ -3,26 +3,79 @@ namespace Server.Items
     public class Static : Item
     {
         public Static()
-            : base(0x80)
+	        : base(0x80)
+        {
+	        Movable = false;
+        }
+
+        [Constructable]
+        public Static(int itemID) : base(itemID)
         {
             Movable = false;
         }
 
-        [Constructable]
-        public Static(int itemID)
-            : base(itemID)
-        {
-            Movable = false;
-        }
-
-        [Constructable]
-        public Static(int itemID, int count)
-            : this(Utility.Random(itemID, count))
+       [Constructable]
+        public Static(int itemID, int count) : this(Utility.Random(itemID, count))
         {
         }
 
-        public Static(Serial serial)
-            : base(serial)
+        [CommandProperty(AccessLevel.Counselor)]
+        public string DecorName
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        [CommandProperty(AccessLevel.Counselor)]
+        public string DecorLabel
+        {
+            get { return Label1; }
+            set { Label1 = value; }
+        }
+
+        [CommandProperty(AccessLevel.Counselor)]
+        public LootType DecorLootType
+        {
+            get { return LootType; }
+            set { LootType = value; }
+        }
+
+        [CommandProperty(AccessLevel.Counselor)]
+        public int DecorItemID
+        {
+            get { return ItemID; }
+            set { ItemID = value; }
+        }
+
+        [CommandProperty(AccessLevel.Counselor)]
+        public int DecorHue
+        {
+            get { return Hue; }
+            set { Hue = value; }
+        }
+
+        [CommandProperty(AccessLevel.Counselor)]
+        public bool DecorMovable
+        {
+            get { return Movable; }
+            set { Movable = value; }
+        }
+
+        [CommandProperty(AccessLevel.Counselor)]
+        public Point3D DecorLocation
+        {
+            get { return Location; }
+            set { Location = value; }
+        }
+
+        [CommandProperty(AccessLevel.Counselor)]
+        public double DecorWeight
+        {
+            get { return Weight; }
+            set { Weight = value; }
+        }
+
+        public Static(Serial serial) : base(serial)
         {
         }
 
@@ -30,7 +83,7 @@ namespace Server.Items
         {
             base.Serialize(writer);
 
-            writer.Write(1); // version
+            writer.Write((int)1); // version
         }
 
         public override void Deserialize(GenericReader reader)
@@ -47,44 +100,44 @@ namespace Server.Items
     public class LocalizedStatic : Static
     {
         private int m_LabelNumber;
-        [Constructable]
-        public LocalizedStatic(int itemID)
-            : this(itemID, itemID < 0x4000 ? 1020000 + itemID : 1078872 + itemID)
-        {
-        }
-
-        [Constructable]
-        public LocalizedStatic(int itemID, int labelNumber)
-            : base(itemID)
-        {
-            m_LabelNumber = labelNumber;
-        }
-
-        public LocalizedStatic(Serial serial)
-            : base(serial)
-        {
-        }
 
         [CommandProperty(AccessLevel.GameMaster)]
         public int Number
         {
-            get
-            {
-                return m_LabelNumber;
-            }
+            get { return m_LabelNumber; }
             set
             {
                 m_LabelNumber = value;
                 InvalidateProperties();
             }
         }
-        public override int LabelNumber => m_LabelNumber;
+
+        public override int LabelNumber
+        {
+            get { return m_LabelNumber; }
+        }
+
+        [Constructable]
+        public LocalizedStatic(int itemID) : this(itemID, 1020000 + itemID)
+        {
+        }
+
+        [Constructable]
+        public LocalizedStatic(int itemID, int labelNumber) : base(itemID)
+        {
+            m_LabelNumber = labelNumber;
+        }
+
+        public LocalizedStatic(Serial serial) : base(serial)
+        {
+        }
+
         public override void Serialize(GenericWriter writer)
         {
             base.Serialize(writer);
 
             writer.Write((byte)0); // version
-            writer.WriteEncodedInt(m_LabelNumber);
+            writer.WriteEncodedInt((int)m_LabelNumber);
         }
 
         public override void Deserialize(GenericReader reader)
@@ -96,10 +149,10 @@ namespace Server.Items
             switch (version)
             {
                 case 0:
-                    {
-                        m_LabelNumber = reader.ReadEncodedInt();
-                        break;
-                    }
+                {
+                    m_LabelNumber = reader.ReadEncodedInt();
+                    break;
+                }
             }
         }
     }
