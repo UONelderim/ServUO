7b15eaf9c0a2e9ed1034623883de3c353dbb9583
kaczy93 <kaczy93@users.noreply.github.com>
2023-05-09 20:06:25 +0200

    Moved Achievement system, cleaned up the surface

--- Scripts/Nelderim/Services/Achievements/AchieveTypes/ItemCraftedAchievement.cs
+++ Scripts/Nelderim/Services/Achievements/AchieveTypes/ItemCraftedAchievement.cs
@@ -1,10 +1,6 @@
 ﻿using Server;
 using Server.Mobiles;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Scripts.Mythik.Systems.Achievements
 {
