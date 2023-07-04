8b0057e570c66e31b8c9bb0e899412063a5d2c65
Maupishon <RapFamilly@gmail.com>
2023-07-04 12:18:35 +0200

    Doruczic SBKeeperOfBushido do KeeperOfChivalry

--- Scripts/VendorInfo/SBKeeperOfChivalry.cs
+++ Scripts/VendorInfo/SBKeeperOfChivalry.cs
@@ -1,5 +1,7 @@
-using Server.Items;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using Server.Items;
 
 namespace Server.Mobiles
 {
@@ -8,6 +10,10 @@ namespace Server.Mobiles
 		private readonly List<IBuyItemInfo> m_BuyInfo = new InternalBuyInfo();
 		private readonly IShopSellInfo m_SellInfo = new InternalSellInfo();
 
+		public SBKeeperOfChivalry()
+		{
+		}
+
 		public override IShopSellInfo SellInfo => m_SellInfo;
 		public override List<IBuyItemInfo> BuyInfo => m_BuyInfo;
 
@@ -15,22 +21,16 @@ namespace Server.Mobiles
 		{
 			public InternalBuyInfo()
 			{
-				Add(new GenericBuyInfo(typeof(BookOfChivalry), 500, 20, 0x2252, 0));
-				AddRange(ChivalryScrolls);
+                
+                Add( new GenericBuyInfo( typeof( BookOfChivalry ), 500, 20, 0x2252, 0 ) );
 			}
 		}
-		
-		public static List<IBuyItemInfo> ChivalryScrolls = new List<IBuyItemInfo>(
-			new []{
-				new GenericBuyInfo(typeof(CloseWoundsScroll), 20, 10, 0x1F6E, 1150),
-				new GenericBuyInfo(typeof(RemoveCurseScroll), 20, 10, 0x1F6E, 1150),
-				new GenericBuyInfo(typeof(CleanseByFireScroll), 20, 10, 0x1F6D, 1150),
-				new GenericBuyInfo(typeof(ConsecrateWeaponScroll), 20, 10, 0x1F6D, 1150),
-				new GenericBuyInfo(typeof(DivineFuryScroll), 20, 10, 0x1F6D, 1150)
-			});
 
 		public class InternalSellInfo : GenericSellInfo
 		{
+			public InternalSellInfo()
+			{
+			}
 		}
 	}
 }
