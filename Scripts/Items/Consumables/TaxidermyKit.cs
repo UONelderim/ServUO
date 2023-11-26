2eb93bf230c4039a45e1114d89c6eec6fd545c78
kaczy <kaczyf93@gmail.com>
2023-11-26 16:32:01 +0100

    Reverted TaxidermyKit changes + few translations

--- Scripts/Items/Consumables/TaxidermyKit.cs
+++ Scripts/Items/Consumables/TaxidermyKit.cs
@@ -103,195 +103,40 @@ namespace Server.Items
             
             // Nelderimowe
             
-            			new TrophyInfo( typeof( GreyWolf ),		0x20EA,	-1, 0x03E5 ),	// (szary)
-			new TrophyInfo( typeof( TimberWolf ),	0x20EA,	-1, 0     ),	// (lesny)
-			new TrophyInfo( typeof( DireWolf ),		0x20EA,	-1, 0x0455 ),	// (wsciekly)
-			new TrophyInfo( typeof( WhiteWolf ),	0x20EA,	-1, 0x0385 ),	// (bialy)
-			new TrophyInfo( typeof( GreatHart ),	0x1E68,	0x1E61, 0 ),
-			//new TrophyInfo( typeof( Pig ),			0x1E8F,	0x1E8E, 0 ),	// za nisko sciany wisi
-			new TrophyInfo( typeof( Hind ),			0x20D4,	-1, -99 ),
-			new TrophyInfo( typeof( Gorilla ),		0x20C9,	-1, -99 ),
-			new TrophyInfo( typeof( GiantRat ),		0x20D0,	-1, -99 ),
-			new TrophyInfo( typeof( Rabbit ),		0x2125,	-1,	-99 ),
-			new TrophyInfo( typeof( Dog ),			0x20D5,	-1, -99 ),
-			new TrophyInfo( typeof( Bird ),			0x211A,	-1, -99 ),
-			new TrophyInfo( typeof( Bird ),			0x20EE,	-1, -99 ),
-			new TrophyInfo( typeof( Eagle ),		0x211D,	-1, -99 ),
-			new TrophyInfo( typeof( Eagle ),		0x20F2,	-1, -99 ),
-			new TrophyInfo( typeof( Walrus ),		0x20FF,	-1, -99 ),
-			new TrophyInfo( typeof( Wisp ),			0x2100,	-1, -99 ),
-			new TrophyInfo( typeof( Panther ),		0x2102,	-1, 0x901 ), 	// 0x2119-mniejsza (big cat,kot)
-			new TrophyInfo( typeof( Cougar ),		0x2102,	-1, -99 ),
-			new TrophyInfo( typeof( Cat ),			0x211B,	-1, -99 ),
-			new TrophyInfo( typeof( Gazer ),		0x20F4, -1, -99 ),
-			new TrophyInfo( typeof( ElderGazer ),	0x20F4,	-1, -99 ),
-			new TrophyInfo( typeof( Snake ),		0x20FE,	-1, -99 ),
-			new TrophyInfo( typeof( Crane ),		0x2764,	-1, -99 ),
-			new TrophyInfo( typeof( SummonedDaemon ),0x2104, -1, -99 ),			
-			new TrophyInfo( typeof( Dragon ),		0x2235,	0x2234, -99 ),
-			new TrophyInfo( typeof( Pixie ),		0x2A72,	0x2A71, 0 ),
-			new TrophyInfo( typeof( Pixie ),		0x2A74,	0x2A73, 0 ),
-			new TrophyInfo( typeof( Pixie ),		0x2A76,	0x2A75, 0 ),
-			new TrophyInfo( typeof( Pixie ),		0x2A78,	0x2A77, 0 ),
-			new TrophyInfo( typeof( Pixie ),		0x2A7A,	0x2A79, 0 ),
-			new TrophyInfo( typeof( Pixie ),		0x2D8A,	-1, -99 ),
-			new TrophyInfo( typeof( Unicorn ),		0x3158,	0x3159, 0 ),
-			new TrophyInfo( typeof( Lizardman ),	0x20CA,	-1, 0 )
-			
-			/*
-			//new TrophyInfo( typeof( PolarBear ),		0x20E1,	-1, 0 ),	
-			//new TrophyInfo( typeof( GrizzlyBear ),	0x20DB,	-1, 0 ), // 0x20DB-za maly 0x211E-za maly
-			//new TrophyInfo( typeof( Rat ),			0x2123,	-1, 0 ),	// troche za maly
-			//new TrophyInfo( typeof( Sheep ),			0x20EB,	-1, 0 ),	// lipne:0x20EB
-			//new TrophyInfo( typeof( Alligator ),		0x20DA,	-1, 0 ),	// za maly
-			//new TrophyInfo( typeof( Chicken ),		0x20D1,	-1, 0 ),	// troche za duzy
-			//new TrophyInfo( typeof( Bull ),			0x20EF,	-1, 0 ),	// ZaMale: 0x20EF-nielaciaty 0x20F0-lacaity						
-			//new TrophyInfo( typeof( Dolphin ),		0x20F1,	-1, 0 ),	// dziwny
-			//new TrophyInfo( typeof( Pig ),			0x2101,	-1, 0 ),	// wieksza	
-			//new TrophyInfo( typeof( Cow ),			0x2103,	-1, 0 ),	// za mala
-			//new TrophyInfo( typeof( MountainGoat ),	0x2108,	-1, 0 ),	// zaMala
-			//new TrophyInfo( typeof( Goat ),			0x2580,	-1, 0 ),	// ladna, za zala, niepodobna	
-			
-			//new TrophyInfo( typeof( Llama ),			0x20F6,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Horse ),			0x211F,	-1, 0 ),	// za male: 0x2120-jasny  0x2121-ciemny  0x211F-bialy  0x2124-jasnyLepszy?
-			//new TrophyInfo( typeof( Ridgeback ),		0x2615,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( PackHorse ),		0x2126,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( PackLlama ),		0x2127,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( DesertOstard ),	0x2135,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( FrenziedOstard ),	0x2136,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( ForestOstard ),	0x2137,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( SilverSteed ),	0x259D,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( FireSteed ),		0x21F1,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Beetle ),			0x260F,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Nightmare ),		0x259C,	-1, 0 ),	// za male
-		
-			//new TrophyInfo( typeof( Cyclops ),	0x212D,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Titan ),		0x25CD,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Troll ),		0x20E9,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( TrollLord ),	0x1E63,	0x1E6A, 0 ),	// za male
-			//new TrophyInfo( typeof( Ettin ),		0x20C8,	-1, 0 ),	// za male		
-			//new TrophyInfo( typeof( Ogre ),		0x20DF,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( OgreLord ),	0x20CB,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Orc ),		0x20E0,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( OrcCaptain ),	0x25AF,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( OrcishLord ),	0x25B0,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( OrcishMage ),	0x25B1,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Ratman ),		0x20E3,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( RatmanArcher ),	0x20E3,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( RatmanMage ),	0x20E3,	-1, 0 ),	// za male			
-			//new TrophyInfo( typeof( Harpy ),		0x20DC,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( StoneHarpy ),	0x2594,	-1, 0 ),	// za male
-			
-			//new TrophyInfo( typeof( Scorpion ),	0x20E4,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Mongbat ),	0x20F9,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Reaper ),		0x20FA,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( SeaSerpent ),	0x20FB,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( GiantSerpent ),	0x20FC,	-1, 0 ),	// za male		
-		
-			//new TrophyInfo( typeof( GiantSpider ),		0x20FD,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( GiantBlackWidow ),	0x25C3,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( DreadSpider ),		0x25C4,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( FrostSpider ),		0x25C5,	-1, 0 ),	// za male			
-		
-			//new TrophyInfo( typeof( TerathanDrone ),		0x212B,	-1, 0 ),	// za male // 0x25C9-inny
-			//new TrophyInfo( typeof( TerathanWarrior ),	0x212A,	-1, 0 ),	// za male // 0x25CC-inny		
-			//new TrophyInfo( typeof( TerathanMatriarch ),	0x212C,	-1, 0 ),	// za male // 0x25CB-queen
-			//new TrophyInfo( typeof( TerathanAvenger ),	0x25CA,	-1, 0 ),	// za male
-			
-			//new TrophyInfo( typeof( OphidianMage ),			0x2132,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( OphidianArchmage ),		0x25AC,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( OphidianWarrior ),		0x2133,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( OphidianKnight ),		0x25AA,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( OphidianMatriarch ),	0x2134,	-1, 0 ),	// za male // 0x25AC-bok
-			
-			//new TrophyInfo( typeof( Gorgoyle ),			0x20D9,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( GargoyleDestroyer ),	0x258D,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( StoneGargoyle ),		0x258E,	-1, 0 ),	// za male
-			
-			//new TrophyInfo( typeof( Zombie ),			0x20EC,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Lich ),			0x20F8,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( LichLord ),		0x25A5,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Ghoul ),			0x2109,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( HeadlessOne ),	0x210A,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Mummy ),			0x25A7,	-1, 0 ),	// za male
-			// //new TrophyInfo( typeof( Skeleton ),	0x20E7,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Skeleton ),		0x25BC,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( SkeletalKnight ),	0x25BD,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( SkeletalMage ),	0x25BE,	-1, 0 ),	// za male			
-			
-			//new TrophyInfo( typeof( Slime ),		0x20E8,	-1, 0 ),	// za duze
-			//new TrophyInfo( typeof( GiantToad ),	0x212F,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( BullFrog ),	0x2130,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( LavaLizard ),	0x2131,	-1, 0 ),	// za male
-			
-			//new TrophyInfo( typeof( Wyvern ),		0x25D4,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Dragon ),		0x20D6,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Kraken ),		0x25A2,	-1, 0 ),	// za male			
-			
-			//new TrophyInfo( typeof( RedSolenQueen ),	0x2602,	-1, 0 ),	// za male //red
-			//new TrophyInfo( typeof( RedSolenWarrior ),	0x2603,	-1, 0 ),	// za male //red
-			//new TrophyInfo( typeof( RedSolenWorker ),	0x2604,	-1, 0 ),	// za male //red			
-			
-			//new TrophyInfo( typeof( bagienny potwor ),	0x2608,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Quagmire ),		0x2614,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Corpser ),			0x20D2,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( WhippingVine ),		0x20D2,	-1, 0 ),	// za male			
-
-			//new TrophyInfo( typeof( FireDaemon ),		0x20D3,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( ChaosDemon ),		0x2609,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( ArcaneDaemon ),	0x2605,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( HordeDaemon ),	0x260E,	-1, 0 ),	// za male //niebieski czteroreki
-			//new TrophyInfo( typeof( HordeMinion ),	0x2611,	-1, 0 ),	// za male		
-			
-			//new TrophyInfo( typeof( EvilMage ),			0x258A,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( MeerMage ),			0x261C,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( MeerWarrior ),		0x261D,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( JukaMage ),			0x261E,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( JukaWarrior ),		0x261F,	-1, 0 ),	// za male			
-			
-			//new TrophyInfo( typeof( YumotsuElder ),	0x2772,	-1, 0 ),	// za male	
-			//new TrophyInfo( typeof( YumotsuPriest ),	0x2773,	-1, 0 ),	// za male	
-			
-			//new TrophyInfo( typeof( AgapiteElemental ),		0x20D7,	-1, 0x1B9 ),	// za male	
-			//new TrophyInfo( typeof( BronzeElemental ),		0x20D7,	-1, 0x156 ),	// za male	
-			//new TrophyInfo( typeof( CopperElemental ),		0x20D7,	-1, 0xF9 ),	// za male	
-			//new TrophyInfo( typeof( DullCopperElemental ),	0x20D7,	-1, 0x3A7 ),	// za male	
-			//new TrophyInfo( typeof( GoldenElemental ),		0x20D7,	-1, 0x35 ),	// za male	
-			//new TrophyInfo( typeof( ShadowIronElemental ),	0x20D7,	-1, 0x395 ),	// za male	
-			//new TrophyInfo( typeof( ValoriteElemental ),	0x20D7,	-1, 0x5 ),	// za male	
-			//new TrophyInfo( typeof( VeriteElemental ),		0x20D7,	-1, 0x41 ),	// za male	
-			//new TrophyInfo( typeof( EarthElemental ),	0x20D7,	-1, 0 ),	// za male	
-			//new TrophyInfo( typeof( AirElemental ),		0x20ED,	-1, 0 ),	// za male	
-			//new TrophyInfo( typeof( FireElemental ),	0x20F3,	-1, 0 ),	// za male	
-			//new TrophyInfo( typeof( WaterElemental ),	0x210B,	-1, 0 ),	// za male	
-			//new TrophyInfo( typeof( CrystalElemental ),	0x2620,	-1, 0 ),	// za male	
-			
-			//new TrophyInfo( typeof( Golem ),			0x2610,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( FleshGolem ),		0x2624,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( GoreFiend ),		0x2625,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Imp ),			0x259F,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Gibberling ),		0x2627,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof(  devourer  ),		0x2623,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Doppleganger ),	0x260D,	-1, 0 ),	// za male
-			
-			//new TrophyInfo( typeof( TreeFellow ),		0x2621,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( RaiJu ),			0x2766,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( SnowLady ),		0x276C,	-1, 0 ), 	// za male
-			//new TrophyInfo( typeof( Centaur ),			0x2581,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( EtherealWarrior ),	0x2589,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Pixie ),			0x25B6,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Kirin ),			0x25A0,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Unicorn ),			0x25CE,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( BakeKitsune ),		0x2763,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Gaman ),			0x2768,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Hiryu ),			0x276A,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Kappa ),			0x276B,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Oni ),				0x276D,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( RevenantLion ),		0x276E,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( RuneBeetle ),		0x276F,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( TsukiWolf ),		0x2770,	-1, 0 ),	// za male
-			//new TrophyInfo( typeof( Yamandon ),			0x2771,	-1, 0 )	// za male
-			*/
+   //          new TrophyInfo( typeof( GreyWolf ),		0x20EA,	-1, 0x03E5 ),	// (szary)
+			// new TrophyInfo( typeof( TimberWolf ),	0x20EA,	-1, 0     ),	// (lesny)
+			// new TrophyInfo( typeof( DireWolf ),		0x20EA,	-1, 0x0455 ),	// (wsciekly)
+			// new TrophyInfo( typeof( WhiteWolf ),	0x20EA,	-1, 0x0385 ),	// (bialy)
+			// new TrophyInfo( typeof( GreatHart ),	0x1E68,	0x1E61, 0 ),
+			// //new TrophyInfo( typeof( Pig ),			0x1E8F,	0x1E8E, 0 ),	// za nisko sciany wisi
+			// new TrophyInfo( typeof( Hind ),			0x20D4,	-1, -99 ),
+			// new TrophyInfo( typeof( Gorilla ),		0x20C9,	-1, -99 ),
+			// new TrophyInfo( typeof( GiantRat ),		0x20D0,	-1, -99 ),
+			// new TrophyInfo( typeof( Rabbit ),		0x2125,	-1,	-99 ),
+			// new TrophyInfo( typeof( Dog ),			0x20D5,	-1, -99 ),
+			// new TrophyInfo( typeof( Bird ),			0x211A,	-1, -99 ),
+			// new TrophyInfo( typeof( Bird ),			0x20EE,	-1, -99 ),
+			// new TrophyInfo( typeof( Eagle ),		0x211D,	-1, -99 ),
+			// new TrophyInfo( typeof( Eagle ),		0x20F2,	-1, -99 ),
+			// new TrophyInfo( typeof( Walrus ),		0x20FF,	-1, -99 ),
+			// new TrophyInfo( typeof( Wisp ),			0x2100,	-1, -99 ),
+			// new TrophyInfo( typeof( Panther ),		0x2102,	-1, 0x901 ), 	// 0x2119-mniejsza (big cat,kot)
+			// new TrophyInfo( typeof( Cougar ),		0x2102,	-1, -99 ),
+			// new TrophyInfo( typeof( Cat ),			0x211B,	-1, -99 ),
+			// new TrophyInfo( typeof( Gazer ),		0x20F4, -1, -99 ),
+			// new TrophyInfo( typeof( ElderGazer ),	0x20F4,	-1, -99 ),
+			// new TrophyInfo( typeof( Snake ),		0x20FE,	-1, -99 ),
+			// new TrophyInfo( typeof( Crane ),		0x2764,	-1, -99 ),
+			// new TrophyInfo( typeof( SummonedDaemon ),0x2104, -1, -99 ),			
+			// new TrophyInfo( typeof( Dragon ),		0x2235,	0x2234, -99 ),
+			// new TrophyInfo( typeof( Pixie ),		0x2A72,	0x2A71, 0 ),
+			// new TrophyInfo( typeof( Pixie ),		0x2A74,	0x2A73, 0 ),
+			// new TrophyInfo( typeof( Pixie ),		0x2A76,	0x2A75, 0 ),
+			// new TrophyInfo( typeof( Pixie ),		0x2A78,	0x2A77, 0 ),
+			// new TrophyInfo( typeof( Pixie ),		0x2A7A,	0x2A79, 0 ),
+			// new TrophyInfo( typeof( Pixie ),		0x2D8A,	-1, -99 ),
+			// new TrophyInfo( typeof( Unicorn ),		0x3158,	0x3159, 0 ),
+			// new TrophyInfo( typeof( Lizardman ),	0x20CA,	-1, 0 )
         };
 
         public class TrophyInfo
