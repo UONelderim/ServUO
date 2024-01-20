9e3b96ea3af06cd02e4662d7a24f87bad877136e
kaczy <kaczyf93@gmail.com>
2024-01-20 13:12:34 +0100

    Fixes in ArmysPaeonSpell

--- Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Bard/Spells/ArmysPaeonSpell.cs
+++ Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Bard/Spells/ArmysPaeonSpell.cs
@@ -1,64 +1,54 @@
 using System;
-using System.Collections;
-using Server;
+using System.Collections.Generic;
 using Server.Mobiles;
-using Server.Network;
-using Server.Items;
 using Server.Spells;
 
 namespace Server.ACC.CSS.Systems.Bard
 {
 	public class BardArmysPaeonSpell : BardSpell
 	{
-
 		private static SpellInfo m_Info = new SpellInfo(
-		                                                "Śpiew Armii", "Paeonus",
-		                                                //SpellCircle.First,
-		                                                212,
-		                                                9041
-		                                               );
+			"Śpiew Armii",
+			"Paeonus",
+			//SpellCircle.First,
+			212,
+			9041
+		);
 
-        public override SpellCircle Circle
-        {
-            get { return SpellCircle.First; }
-        }
+		public override SpellCircle Circle => SpellCircle.First;
+		public override double CastDelay => 3;
+		public override double RequiredSkill => 60.0;
+		public override int RequiredMana => 15;
 
-		public override double CastDelay{ get{ return 3; } }
-		public override double RequiredSkill{ get{ return 60.0; } }
-		public override int RequiredMana{ get{ return 15; } }
-
-		public BardArmysPaeonSpell( Mobile caster, Item scroll) : base( caster, scroll, m_Info )
+		public BardArmysPaeonSpell(Mobile caster, Item scroll) : base(caster, scroll, m_Info)
 		{
-			                    if (this.Scroll != null)
-                        Scroll.Consume();
+			Scroll?.Consume();
 		}
 
 		public override void OnCast()
 		{
-			if( CheckSequence() )
+			if (CheckSequence())
 			{
-				ArrayList targets = new ArrayList();
+				var targets = new List<Mobile>();
 
-				IPooledEnumerable eable = Caster.GetMobilesInRange( 3 );
-				foreach ( Mobile m in eable )
+				IPooledEnumerable eable = Caster.GetMobilesInRange(3);
+				foreach (Mobile m in eable)
 				{
-					if ( Caster.CanBeBeneficial( m, false, true ) && !(m is Golem) )
-						targets.Add( m );
+					if (Caster.CanBeBeneficial(m, false, true) && !(m is Golem))
+						targets.Add(m);
 				}
+
 				eable.Free();
 
-				for ( int i = 0; i < targets.Count; ++i )
+				foreach (var m in targets)
 				{
-					Mobile m = (Mobile)targets[i];
+					int rounds = (int)(Caster.Skills[SkillName.Musicianship].Value * 0.5);
 
-					TimeSpan duration = TimeSpan.FromSeconds( Caster.Skills[CastSkill].Value * 0.6 );
-					int rounds = (int)( Caster.Skills[SkillName.Musicianship].Value * .16 );
+					new ExpireTimer(m, rounds, TimeSpan.FromSeconds(5)).Start();
 
-					new ExpireTimer( m, 0, rounds, TimeSpan.FromSeconds( 2 ) ).Start();
-					
 					if (m.Hidden == false)
 					{
-						m.FixedParticles(0x376A, 9, 32, 5030, 0x21, 3, EffectLayer.Waist);
+						m.FixedParticles(0x376A, 9, 32, 5030, 1153, 3, EffectLayer.Waist);
 					}
 				}
 			}
@@ -72,30 +62,31 @@ namespace Server.ACC.CSS.Systems.Bard
 			private int m_Round;
 			private int m_Totalrounds;
 
-			public ExpireTimer( Mobile m, int round, int totalrounds, TimeSpan delay ) : base( delay )
+			public ExpireTimer(Mobile m, int totalrounds, TimeSpan delay) : base(delay, delay)
 			{
 				m_Mobile = m;
-				m_Round = round;
+				m_Round = 0;
 				m_Totalrounds = totalrounds;
 			}
 
 			protected override void OnTick()
 			{
-				if ( m_Mobile != null )
+				if (m_Mobile == null || m_Mobile.Deleted)
 				{
+					Stop();
+					return;
+				}
 
-					m_Mobile.Hits += 2;
+				m_Mobile.Hits += 5;
 
-					if ( m_Round >= m_Totalrounds )
-					{
-						m_Mobile.SendMessage( "The effect of Army's Paeon wears off." );
-
-					}
-					else
-					{
-						m_Round += 1;
-						new ExpireTimer( m_Mobile, m_Round, m_Totalrounds, TimeSpan.FromSeconds( 2 ) ).Start();
-					}
+				if (m_Round >= m_Totalrounds)
+				{
+					m_Mobile.SendMessage("The effect of Army's Paeon wears off.");
+					Stop();
+				}
+				else
+				{
+					m_Round += 1;
 				}
 			}
 		}
