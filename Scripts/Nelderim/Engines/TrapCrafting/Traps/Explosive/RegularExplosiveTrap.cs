476cde11adeb98443795e0247f9491d656506b71
kaczy <kaczyf93@gmail.com>
2024-01-20 18:46:21 +0100

    TinkerTraps wont require skill to arm

--- Scripts/Nelderim/Engines/TrapCrafting/Traps/Explosive/RegularExplosiveTrap.cs
+++ Scripts/Nelderim/Engines/TrapCrafting/Traps/Explosive/RegularExplosiveTrap.cs
@@ -15,14 +15,13 @@ namespace Server.Items
         private static string m_ArmedName = "uzbrojona wybuchowa pułapkap";
         private static string m_UnarmedName = "nueuzbrojona  wybuchowa pułapka";
         private static double m_ExpiresIn = 900.0;
-        private static int m_ArmingSkill = 25;
         private static int m_DisarmingSkill = 50;
         private static int m_KarmaLoss = 50;
         private static bool m_AllowedInTown = false;
 
         [Constructable]
         public ExplosiveRegularTrap()
-            : base(m_ArmedName, m_UnarmedName, m_ExpiresIn, m_ArmingSkill, m_DisarmingSkill, m_KarmaLoss, m_AllowedInTown)
+            : base(m_ArmedName, m_UnarmedName, m_ExpiresIn, m_DisarmingSkill, m_KarmaLoss, m_AllowedInTown)
         {
         }
 
--- Scripts/Nelderim/Engines/TrapCrafting/Traps/Poison Dart/LesserPoisonDartTrap.cs
+++ Scripts/Nelderim/Engines/TrapCrafting/Traps/Poison Dart/LesserPoisonDartTrap.cs
@@ -15,14 +15,13 @@ namespace Server.Items
         private static string m_ArmedName = "uzbrojona trująca pułapka z lekką trucizną";
         private static string m_UnarmedName = "nieuzbrojona trująca pułapka z lekką trucizną";
         private static double m_ExpiresIn = 900.0;
-        private static int m_ArmingSkill = 25;
         private static int m_DisarmingSkill = 30;
         private static int m_KarmaLoss = 60;
         private static bool m_AllowedInTown = false;
 
         [Constructable]
         public PoisonLesserDartTrap()
-            : base(m_ArmedName, m_UnarmedName, m_ExpiresIn, m_ArmingSkill, m_DisarmingSkill, m_KarmaLoss, m_AllowedInTown)
+            : base(m_ArmedName, m_UnarmedName, m_ExpiresIn, m_DisarmingSkill, m_KarmaLoss, m_AllowedInTown)
         {
         }
 
