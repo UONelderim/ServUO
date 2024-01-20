7f105da4d95e83e6b89969a0626d6c0e0b6a2136
kaczy <kaczyf93@gmail.com>
2024-01-20 17:47:18 +0100

    Fix grapsing roots spell

--- Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Druid/Spells/GraspingRootsSpell.cs
+++ Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Druid/Spells/GraspingRootsSpell.cs
@@ -10,7 +10,7 @@ namespace Server.ACC.CSS.Systems.Druid
 {
 	public class DruidGraspingRootsSpell : DruidSpell
 	{
-		private static readonly SpellInfo m_Info = new SpellInfo(
+		private static readonly SpellInfo m_Info = new(
 			"Szalone Korzenie", "En Ohm Sepa Tia Kes",
 			//SpellCircle.Fifth,
 			218,
@@ -22,20 +22,26 @@ namespace Server.ACC.CSS.Systems.Druid
 		);
 
 		public override SpellCircle Circle => SpellCircle.Fifth;
-
 		public override double CastDelay => 1.5;
 		public override double RequiredSkill => 40.0;
 		public override int RequiredMana => 40;
 
 		public DruidGraspingRootsSpell(Mobile caster, Item scroll) : base(caster, scroll, m_Info)
 		{
-			if (this.Scroll != null)
-				Scroll.Consume();
+			Scroll?.Consume();
 		}
 
 		public override void OnCast()
 		{
-			Caster.Target = new InternalTarget(this);
+			Caster.BeginTarget(12,
+				true,
+				TargetFlags.None,
+				(from, targeted) =>
+				{
+					if (targeted is Mobile m)
+						Target(m);
+				}
+			);
 		}
 
 		public void Target(Mobile m)
@@ -50,125 +56,41 @@ namespace Server.ACC.CSS.Systems.Druid
 
 				double duration;
 
-				// Algorithm: ((20% of AnimalTamin) + 7) seconds [- 50% if resisted] seems to work??
 				duration = 7.0 + (Caster.Skills[DamageSkill].Value * 0.2);
-
-				// Resist if Str + Dex / 2 is greater than CastSkill eg. AnimalLore seems to work??
-				if ((Caster.Skills[CastSkill].Value) < ((m.Str + m.Dex) * 0.5))
+				if ( Caster.Skills[CastSkill].Value < m.Skills[SkillName.MagicResist].Value )
 					duration *= 0.5;
-
-				// no less than 0 seconds no more than 9 seconds
-				if (duration < 0.0)
-					duration = 0.0;
-				if (duration > 4.0)
-					duration = 4.0;
-
+				
+				Utility.Clamp(ref duration, 0.0, 4.0);
+				var delay = TimeSpan.FromSeconds(duration);
+				
 				m.PlaySound(0x2A1);
 
-				m.Paralyze(TimeSpan.FromSeconds(duration));
+				m.Paralyze(delay);
 				m.FixedParticles(0x375A, 2, 10, 5027, 0x3D, 2, EffectLayer.Waist);
 
-				{
-					Point3D loc = new Point3D(m.X, m.Y, m.Z);
-
-					Item item = new InternalItem(loc, Caster.Map, Caster);
-				}
+				var roots = new RootsItem(new Point3D(m.X, m.Y, m.Z), Caster.Map);
+				Timer.DelayCall(delay, () => roots.Delete());
 			}
 
 			FinishSequence();
 		}
 
-		private class InternalItem : Item
+		private class RootsItem : Item
 		{
-			private Timer m_Timer;
-			private DateTime m_End;
-
-			public InternalItem(Point3D loc, Map map, Mobile caster) : base(0xC5F)
+			public RootsItem(Point3D loc, Map map) : base(0xC5F)
 			{
-				Visible = false;
-				Movable = false;
-
 				MoveToWorld(loc, map);
-
-				if (caster.InLOS(this))
-					Visible = true;
-				else
-					Delete();
-
-				if (Deleted)
-					return;
-
-				m_Timer = new InternalTimer(this, TimeSpan.FromSeconds(30.0));
-				m_Timer.Start();
-
-				m_End = DateTime.Now + TimeSpan.FromSeconds(30.0);
+				Movable = false;
 			}
 
-			public InternalItem(Serial serial) : base(serial)
+			public RootsItem(Serial serial) : base(serial)
 			{
-			}
-
-			public override void Serialize(GenericWriter writer)
-			{
-				base.Serialize(writer);
-				writer.Write(1); // version
-				writer.Write(m_End - DateTime.Now);
 			}
 
 			public override void Deserialize(GenericReader reader)
 			{
 				base.Deserialize(reader);
-				int version = reader.ReadInt();
-				TimeSpan duration = reader.ReadTimeSpan();
-
-				m_Timer = new InternalTimer(this, duration);
-				m_Timer.Start();
-
-				m_End = DateTime.Now + duration;
-			}
-
-			public override void OnAfterDelete()
-			{
-				base.OnAfterDelete();
-
-				if (m_Timer != null)
-					m_Timer.Stop();
-			}
-
-			private class InternalTimer : Timer
-			{
-				private readonly InternalItem m_Item;
-
-				public InternalTimer(InternalItem item, TimeSpan duration) : base(duration)
-				{
-					m_Item = item;
-				}
-
-				protected override void OnTick()
-				{
-					m_Item.Delete();
-				}
-			}
-		}
-
-		private class InternalTarget : Target
-		{
-			private readonly DruidGraspingRootsSpell m_Owner;
-
-			public InternalTarget(DruidGraspingRootsSpell owner) : base(12, true, TargetFlags.None)
-			{
-				m_Owner = owner;
-			}
-
-			protected override void OnTarget(Mobile from, object o)
-			{
-				if (o is Mobile)
-					m_Owner.Target((Mobile)o);
-			}
-
-			protected override void OnTargetFinish(Mobile from)
-			{
-				m_Owner.FinishSequence();
+				Delete();
 			}
 		}
 	}
