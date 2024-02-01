547547d3bad8607c3cb7949223a6300956fee717
kaczy <kaczyf93@gmail.com>
2024-02-01 20:55:00 +0100

    Better undead language

--- Server/Nelderim/Languages/LanguagesDictionary.cs
+++ Server/Nelderim/Languages/LanguagesDictionary.cs
@@ -18,7 +18,7 @@ namespace Nelderim
 		public static Dictionary<string, string> Drowi = new();
 		public static Dictionary<string, string> Orkowy = new();
 		public static List<string> Demoniczny = new();
-		public static List<string> Nieumarlych = new();
+		public static Dictionary<string, string> Nieumarlych = new();
 		public static List<string> Belkot = new();
 
 		public static void Initialize()
@@ -29,7 +29,7 @@ namespace Nelderim
 			FillDictionary("Drowi", Drowi);
 			FillDictionary("Orkowy", Orkowy);
 			FillList("Demoniczny", Demoniczny);
-			FillList("Nieumarlych", Nieumarlych);
+			FillDictionary("Nieumarlych", Nieumarlych);
 			FillList("Belkot", Belkot);
 		}
 
