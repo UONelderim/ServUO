e560c78baa034e7634f150e41d203a4956d43fbf
kaczy <kaczyf93@gmail.com>
2024-02-01 21:51:15 +0100

    SmokingPipe and tobacco

---
+++ Scripts/Nelderim/Items/Smoking/BaseTobaccoFlavoured.cs
@@ -0,0 +1,128 @@
+namespace Server.Items
+{
+
+	public enum TobaccoFlavour
+	{
+		None = 0,
+		Apple,
+		Pear,
+		Lemon,
+	}
+
+	public abstract class BaseTobaccoFlavoured : BaseTobacco
+	{
+		public override void OnSmoke(Mobile m)
+		{
+			m.SendMessage("Wspaniale pachnacy dym tytoniowy napelnia twoje pluca.");
+
+			m.Emote("*wypuszcza z ust kleby aromatycznego fajkowego dymu");
+
+			m.PlaySound(0x226);
+			SmokeTimer a = new SmokeTimer(m);
+			a.Start();
+
+			m.RevealingAction();
+		}
+
+		private TobaccoFlavour m_Flavour;
+		public TobaccoFlavour Flavour
+		{
+			get
+			{
+				return m_Flavour;
+			}
+			set
+			{
+				m_Flavour = value;
+				InvalidateProperties();
+			}
+		}
+
+		protected int SmokeHue
+		{
+			get
+			{
+				switch (Flavour)
+				{
+					case TobaccoFlavour.None: return 0;
+					case TobaccoFlavour.Apple: return 41;
+					case TobaccoFlavour.Pear: return 51;
+					case TobaccoFlavour.Lemon: return 55;
+					default: return 0;
+				}
+			}
+		}
+
+		public override void GetProperties(ObjectPropertyList list)
+		{
+			base.GetProperties(list);
+			switch (Flavour)
+			{
+				case TobaccoFlavour.None:
+					break;
+				case TobaccoFlavour.Apple:
+					list.Add(1061201, "jablkiem"); // Aromatyzowany ~1_val~
+					break;
+				case TobaccoFlavour.Pear:
+					list.Add(1061201, "gruszka"); // Aromatyzowany ~1_val~
+					break;
+				case TobaccoFlavour.Lemon:
+					list.Add(1061201, "cytryna"); // Aromatyzowany ~1_val~
+					break;
+				default:
+					list.Add(1061201, "czyms dziwnym"); // Aromatyzowany ~1_val~
+					break;
+			}
+		}
+
+		//public int OnCraft(int quality, bool makersMark, Mobile from, CraftSystem craftSystem, Type typeRes, Type typeRes2, BaseTool tool, CraftItem craftItem, int resHue)
+		//{
+		//    TobaccoFlavour tf = TobaccoFlavour.None;
+		//    foreach (CraftRes res in craftItem.Ressources)
+		//    {
+		//        if (res.ItemType == typeof(Apple))
+		//            tf = TobaccoFlavour.Apple;
+		//        else if (res.ItemType == typeof(Pear))
+		//            tf = TobaccoFlavour.Pear;
+		//        else if (res.ItemType == typeof(Lemon))
+		//            tf = TobaccoFlavour.Lemon;
+
+		//        if (tf != TobaccoFlavour.None)
+		//            break;
+		//    }
+
+		//    Flavour = tf;
+
+		//    return 1; // regular quality
+		//}
+
+		public BaseTobaccoFlavoured() : base()
+		{
+		}
+
+		public BaseTobaccoFlavoured(int amount) : base(amount)
+		{
+		}
+
+		public BaseTobaccoFlavoured(Serial serial) : base(serial)
+		{
+		}
+
+		public override void Serialize(GenericWriter writer)
+		{
+			base.Serialize(writer);
+
+			writer.Write((int)0); // version
+			writer.Write((int)Flavour);
+		}
+
+		public override void Deserialize(GenericReader reader)
+		{
+			base.Deserialize(reader);
+
+			int version = reader.ReadInt();
+			Flavour = (TobaccoFlavour)reader.ReadInt();
+		}
+	}
+
+}
---
+++ Scripts/Nelderim/Items/Smoking/SmokingPipe.cs
@@ -0,0 +1,138 @@
+using System;
+
+
+namespace Server.Items
+{
+    public class SmokingPipe : BaseEarrings
+    {
+        //private Type TobbacoType { get; set; }
+        //private int TobbacoQuantity { get; set; }
+        public int UsesRemaining { get; set; }
+        private bool ShowUsesRemaining { get; set; }
+
+        private class SmokeTimer : Timer
+        {
+            private readonly Mobile m_Mobile;
+            private readonly SmokingPipe m_Fajka;
+
+            private int TobaccoRequired => 4;
+
+            public SmokeTimer(SmokingPipe fajka, Mobile mobile) : base(TimeSpan.FromSeconds(1), TimeSpan.Zero)
+            {
+                m_Mobile = mobile;
+                m_Fajka = fajka;
+                Priority = TimerPriority.OneSecond;
+
+                Start();
+            }
+
+            private Item GetTobaccoFromBackpack()
+            {
+                if (m_Mobile == null || m_Mobile.Backpack == null)
+                    return null;
+
+                // Use the deprecated tobacco first
+                Item tobacco = m_Mobile.Backpack.FindItemByType(typeof(Tyton));
+                if (tobacco != null)
+                    return tobacco;
+
+                Item[] tobaccos = m_Mobile.Backpack.FindItemsByType(typeof(ISmokable));
+                foreach (Item tob in tobaccos)
+                {
+                    if (tob != null && tob.Amount >= TobaccoRequired)
+                        return tobacco;
+                }
+
+                return null;
+            }
+
+            protected override void OnTick()
+            {
+                Item tobacco = GetTobaccoFromBackpack();
+
+                if (tobacco == null || (tobacco.Amount < TobaccoRequired && !(tobacco is Tyton))) // Get rid of the deprecated 'Tyton' instances
+                {
+                    m_Mobile.SendMessage("Za malo tytoniu w plecaku.");
+                    m_Mobile.Emote("*z pustej fajki nie unosi sie ani troche dymu*");
+                    Stop();
+                    return;
+                }
+
+                if (tobacco is ISmokable)
+                {
+                    ((ISmokable)tobacco).OnSmoke(m_Mobile);
+                }
+                else
+                {
+                    m_Mobile.SendMessage("Dym tytoniowy napelnia twoje pluca.");
+
+                    m_Mobile.Emote("*wypuszcza z ust odrobine fajkowego dymu*");
+                    Effects.SendLocationParticles(EffectItem.Create(m_Mobile.Location, m_Mobile.Map, EffectItem.DefaultDuration), 0x3728, 1, 13, 9965);
+                    m_Mobile.PlaySound(0x15F);
+                    m_Mobile.RevealingAction();
+                }
+
+                tobacco.Consume(TobaccoRequired);
+                
+                m_Fajka.UsesRemaining--;
+                m_Fajka.InvalidateProperties();
+
+                Stop();
+                if (m_Fajka.UsesRemaining <= 0)
+                {
+                    m_Mobile.SendMessage("Fajka jest zbyt zuzyta, by z niej palic");
+                    m_Fajka.Delete(); // Optionally remove the item when uses are exhausted
+                }
+            }
+        }
+
+        [Constructable]
+        public SmokingPipe() : base(0x17B3)
+        {
+            Weight = 0.1;
+            Name = "Fajka do palenia";
+            Light = LightType.Circle150;
+            UsesRemaining = 100;
+        }
+
+        public override void GetProperties(ObjectPropertyList list)
+        {
+            base.GetProperties(list);
+            list.Add(1060584, UsesRemaining.ToString()); // uses remaining: ~1_val~
+
+        }
+
+        public override void OnDoubleClick(Mobile from)
+        {
+            if (UsesRemaining > 0)
+            {
+                new SmokeTimer(this, from);
+                from.SendMessage("Zaczynasz zaciagac sie dymem z fajki.");
+                from.Emote("*wciaga powietrze przez fajke*");
+            }
+            else
+            {
+                from.SendMessage("Fajka jest pusta.");
+                from.Emote("*spoglada na zatkana fajke*");
+            }
+        }
+
+        public SmokingPipe(Serial serial) : base(serial)
+        {
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+            writer.Write((int)0); // version
+            writer.Write((int)UsesRemaining);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+            int version = reader.ReadInt();
+            UsesRemaining = reader.ReadInt();
+        }
+    }
+}
