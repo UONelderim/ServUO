95db26cb6069cd14f3e0c4c171d961eab48e0fe4
kaczy <kaczyf93@gmail.com>
2024-03-15 15:33:45 +0100

    Improved loot

--- Scripts/Services/LootGeneration/RandomItemGenerator.cs
+++ Scripts/Services/LootGeneration/RandomItemGenerator.cs
@@ -80,7 +80,7 @@ namespace Server.Items
         public static int GetBaseBudget(BaseCreature bc)
         {
 	        if (NConfig.Loot.Enabled && NConfig.Loot.LootBudget)
-		        return (int)(Math.Pow(Math.Log(bc.Difficulty + 1, 50), 5) * 1.3);
+		        return (int)(Math.Pow(Math.Log(bc.Difficulty + 1, 40), 4) * 3.6);
             if (bc is BaseRenowned)
                 return MaxBaseBudget;
 
