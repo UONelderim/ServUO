2d17ef1562460f02d7e065320c5d128d2c72d556
kaczy <kaczyf93@gmail.com>
2024-06-05 14:28:19 +0200

    Align town cryier system

--- Scripts/Services/Town Cryer/Gumps/TownCryerGreetingsGump.cs
+++ Scripts/Services/Town Cryer/Gumps/TownCryerGreetingsGump.cs
@@ -22,100 +22,129 @@ namespace Server.Services.TownCryer
 		public override void AddGumpLayout()
 		{
 			base.AddGumpLayout();
+			User.CloseGump<TownCryerGreetingsGump>();
 
 			var list = new List<TownCryerGreetingEntry>(TownCryerSystem.GreetingsEntries);
 			list.Sort();
 
-			Entry = list[0];
-
-			if (Page >= 0 && Page < list.Count)
+			if (list.Count > 0)
 			{
-				Entry = list[Page];
-			}
+				Entry = list[0];
 
-			var y = 150;
-
-			if (Entry?.Title.IsEmpty == false)
-			{
-				if (Entry.Title.Number > 0)
+				if (Page >= 0 && Page < list.Count)
 				{
-					AddHtmlLocalized(78, y, 700, 400, CenterLoc, Entry.Title.ToString(), C32216(0x080808), false, false);
-				}
-				else
-				{
-					AddHtml(78, y, 700, 400, Entry.Title.ToString(), false, false);
+					Entry = list[Page];
 				}
 
-				y += 40;
-			}
+				var y = 150;
 
-			// For now, we're only supporting a cliloc (hard coded greetings per EA) or string (Custom) entries. Not both.
-			// Html tags will needed to be added when creating the entry, this will not auto format it for you.
-			if (Entry?.Body1.Number > 0)
-			{
-				AddHtmlLocalized(78, y, 700, 400, Entry.Body1.Number, false, false);
-			}
-			else if (!String.IsNullOrEmpty(Entry?.Body1.String))
-			{
-				var str = Entry.Body1.String;
-
-				if (!String.IsNullOrEmpty(Entry.Body2))
+				if (Entry?.Title.IsEmpty == false)
 				{
-					if (!str.EndsWith("<br>"))
+					if (Entry.Title.Number > 0)
 					{
-						str += " ";
+						AddHtmlLocalized(78,
+							y,
+							700,
+							400,
+							CenterLoc,
+							Entry.Title.ToString(),
+							C32216(0x080808),
+							false,
+							false);
+					}
+					else
+					{
+						AddHtml(78, y, 700, 400, Entry.Title.ToString(), false, false);
 					}
 
-					str += Entry.Body2;
+					y += 40;
 				}
 
-				if (!String.IsNullOrEmpty(Entry.Body3))
+				// For now, we're only supporting a cliloc (hard coded greetings per EA) or string (Custom) entries. Not both.
+				// Html tags will needed to be added when creating the entry, this will not auto format it for you.
+				if (Entry?.Body1.Number > 0)
 				{
-					if (!str.EndsWith("<br>"))
+					AddHtmlLocalized(78, y, 700, 400, Entry.Body1.Number, false, false);
+				}
+				else if (!String.IsNullOrEmpty(Entry?.Body1.String))
+				{
+					var str = Entry.Body1.String;
+
+					if (!String.IsNullOrEmpty(Entry.Body2))
 					{
-						str += " ";
+						if (!str.EndsWith("<br>"))
+						{
+							str += " ";
+						}
+
+						str += Entry.Body2;
 					}
 
-					str += Entry.Body3;
+					if (!String.IsNullOrEmpty(Entry.Body3))
+					{
+						if (!str.EndsWith("<br>"))
+						{
+							str += " ";
+						}
+
+						str += Entry.Body3;
+					}
+
+					AddHtml(78, y, 700, 400, str, false, false);
 				}
 
-				AddHtml(78, y, 700, 400, str, false, false);
-			}
-
-			if (Entry?.Expires != DateTime.MinValue)
-			{
-				AddHtmlLocalized(50, 550, 200, 20, 1060658, $"Created\t{Entry.Created.ToShortDateString()}", 0, false, false);
-				AddHtmlLocalized(50, 570, 200, 20, 1060659, $"Expires\t{Entry.Expires.ToShortDateString()}", 0, false, false);
-			}
-
-			AddButton(350, 570, 0x605, 0x606, 1, GumpButtonType.Reply, 0);
-			AddButton(380, 570, 0x609, 0x60A, 2, GumpButtonType.Reply, 0);
-			AddButton(430, 570, 0x607, 0x608, 3, GumpButtonType.Reply, 0);
-			AddButton(455, 570, 0x603, 0x604, 4, GumpButtonType.Reply, 0);
-
-			AddHtml(395, 570, 35, 20, Center($"{Page + 1}/{Pages}"), false, false);
-
-			AddButton(525, 625, 0x5FF, 0x600, 5, GumpButtonType.Reply, 0);
-			AddHtmlLocalized(550, 625, 300, 20, 1158386, false, false); // Close and do not show this version again
-
-			if (Entry?.Link != null)
-			{
-				if (!String.IsNullOrEmpty(Entry.LinkText))
+				if (Entry?.Expires != DateTime.MinValue)
 				{
-					AddHtml(50, 490, 745, 40, $"<a href=\"{Entry.Link}\">{Entry.LinkText}</a>", false, false);
+					AddHtmlLocalized(50,
+						550,
+						200,
+						20,
+						1060658,
+						$"Created\t{Entry.Created.ToShortDateString()}",
+						0,
+						false,
+						false);
+					AddHtmlLocalized(50,
+						570,
+						200,
+						20,
+						1060659,
+						$"Expires\t{Entry.Expires.ToShortDateString()}",
+						0,
+						false,
+						false);
 				}
-				else
+
+				AddButton(350, 570, 0x605, 0x606, 1, GumpButtonType.Reply, 0);
+				AddButton(380, 570, 0x609, 0x60A, 2, GumpButtonType.Reply, 0);
+				AddButton(430, 570, 0x607, 0x608, 3, GumpButtonType.Reply, 0);
+				AddButton(455, 570, 0x603, 0x604, 4, GumpButtonType.Reply, 0);
+
+				AddHtml(395, 570, 35, 20, Center($"{Page + 1}/{Pages}"), false, false);
+
+				AddButton(525, 625, 0x5FF, 0x600, 5, GumpButtonType.Reply, 0);
+				AddHtmlLocalized(550, 625, 300, 20, 1158386, false, false); // Close and do not show this version again
+
+				if (Entry?.Link != null)
 				{
-					AddHtml(50, 490, 745, 40, $"<a href=\"{Entry.Link}\">{Entry.Link}</a>", false, false);
+					if (!String.IsNullOrEmpty(Entry.LinkText))
+					{
+						AddHtml(50, 490, 745, 40, $"<a href=\"{Entry.Link}\">{Entry.LinkText}</a>", false, false);
+					}
+					else
+					{
+						AddHtml(50, 490, 745, 40, $"<a href=\"{Entry.Link}\">{Entry.Link}</a>", false, false);
+					}
 				}
+				/*
+				if (TownCryerSystem.HasCustomEntries())
+	            {
+	                AddButton(40, 615, 0x603, 0x604, 6, GumpButtonType.Reply, 0);
+	                AddHtmlLocalized(68, 615, 300, 20, 1060660, $"Sort By\t{Sort}", 0, false, false);
+	            }
+				*/
 			}
-			/*
-			if (TownCryerSystem.HasCustomEntries())
-            {
-                AddButton(40, 615, 0x603, 0x604, 6, GumpButtonType.Reply, 0);
-                AddHtmlLocalized(68, 615, 300, 20, 1060660, $"Sort By\t{Sort}", 0, false, false);
-            }
-			*/
+
 			if (User.AccessLevel >= AccessLevel.Administrator)
 			{
 				if (Entry?.CanEdit == true)
