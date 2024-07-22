b68e5dbf92b7e802221e583c5f574d66f4038a51
Maupishon <rapfamilly@gmail.com>
2024-07-22 15:11:47 +0200

    Stuck menu ma dawac mozliwosc powrotu do jednego z frakcyjnych miast

--- Scripts/Services/Help/StuckMenu.cs
+++ Scripts/Services/Help/StuckMenu.cs
@@ -5,259 +5,263 @@ using Server.Network;
 using Server.Spells;
 using System;
 using System.Linq;
+using System.Collections.Generic;
+using Nelderim.Factions;
 
 namespace Server.Menus.Questions
 {
-	public class StuckMenuEntry
-	{
-		private readonly int m_Name;
-		private readonly Point3D[] m_Locations;
+    public class StuckMenuEntry
+    {
+        private readonly int m_Name;
+        private readonly Point3D[] m_Locations;
 
-		public StuckMenuEntry(int name, Point3D[] locations)
-		{
-			m_Name = name;
-			m_Locations = locations;
-		}
+        public StuckMenuEntry(int name, Point3D[] locations)
+        {
+            m_Name = name;
+            m_Locations = locations;
+        }
 
-		public int Name => m_Name;
-		public Point3D[] Locations => m_Locations;
-	}
+        public int Name => m_Name;
+        public Point3D[] Locations => m_Locations;
+    }
 
-	public class StuckMenu : Gump
-	{
-		private static readonly StuckMenuEntry[] m_Entries = {
-			new StuckMenuEntry(1098164, new[] { Map.Felucca.Regions["Orod"].GoLocation }),
-			new StuckMenuEntry(1098165, new[] { Map.Felucca.Regions["Garlan"].GoLocation }),
-			new StuckMenuEntry(1098167, new[] { Map.Felucca.Regions["Lotharn"].GoLocation }),
-			new StuckMenuEntry(1098166, new[] { Map.Felucca.Regions["L'Delmah"].GoLocation }),
-			new StuckMenuEntry(1098169, new[] { Map.Felucca.Regions["Twierdza"].GoLocation }),
-			new StuckMenuEntry(1098168, new[] { Map.Felucca.Regions["Tirassa"].GoLocation })
-		};
+    public class StuckMenu : Gump
+    {
+        private static readonly Dictionary<Faction, List<StuckMenuEntry>> FactionCityEntries = new Dictionary<Faction, List<StuckMenuEntry>>
+        {
+            { Faction.East, new List<StuckMenuEntry>
+                {
 
-		private static readonly StuckMenuEntry[] m_T2AEntries = m_Entries; //Fill me if needed
+                    
+                    new StuckMenuEntry(1098166, new[] { Map.Felucca.Regions["L'Delmah"].GoLocation }),
+                    new StuckMenuEntry(1098169, new[] { Map.Felucca.Regions["Twierdza"].GoLocation }),
+                    new StuckMenuEntry(1098168, new[] { Map.Felucca.Regions["Tirassa"].GoLocation })
+                }
+            },
+            { Faction.West, new List<StuckMenuEntry>
+                {
+	                new StuckMenuEntry(1098164, new[] { Map.Felucca.Regions["Orod"].GoLocation }),
+	                new StuckMenuEntry(1098167, new[] { Map.Felucca.Regions["Lotharn"].GoLocation }),
+	                new StuckMenuEntry(1098165, new[] { Map.Felucca.Regions["Garlan"].GoLocation })
+                }
+            },
 
-		private static readonly StuckMenuEntry[] m_TerMurEntries = m_Entries; //Fill me if needed
+        };
 
-		private readonly Mobile m_Mobile;
-		private readonly Mobile m_Sender;
-		private readonly bool m_MarkUse;
-		private Timer m_Timer;
+        private static List<StuckMenuEntry> GetCitiesForFaction(Faction faction)
+        {
+            if (FactionCityEntries.TryGetValue(faction, out var entries))
+            {
+                return entries;
+            }
+            return new List<StuckMenuEntry>();
+        }
 
-		public StuckMenu(Mobile beholder, Mobile beheld, bool markUse)
-			: base(150, 50)
-		{
-			m_Sender = beholder;
-			m_Mobile = beheld;
-			m_MarkUse = markUse;
+        public static List<StuckMenuEntry> GetStuckMenuEntriesForMobile(Mobile mobile)
+        {
+            Faction mobileFaction = mobile.Faction;
+            
+            return GetCitiesForFaction(mobileFaction);
+        }
 
-			Closable = false;
-			Dragable = false;
-			Disposable = false;
+        private readonly Mobile m_Mobile;
+        private readonly Mobile m_Sender;
+        private readonly bool m_MarkUse;
+        private Timer m_Timer;
 
-			AddBackground(0, 0, 270, 320, 2600);
+        public StuckMenu(Mobile beholder, Mobile beheld, bool markUse)
+            : base(150, 50)
+        {
+            m_Sender = beholder;
+            m_Mobile = beheld;
+            m_MarkUse = markUse;
 
-			AddHtmlLocalized(50, 20, 250, 35, 1011027, false, false); // Chose a town:
+            Closable = false;
+            Dragable = false;
+            Disposable = false;
 
-			StuckMenuEntry[] entries = IsTerMur(beheld) ? m_TerMurEntries :
-				IsInSecondAgeArea(beheld) ? m_T2AEntries : m_Entries;
+            AddBackground(0, 0, 270, 320, 2600);
 
-			for (int i = 0; i < entries.Length; i++)
-			{
-				StuckMenuEntry entry = entries[i];
+            AddHtmlLocalized(50, 20, 250, 35, 1011027, false, false); // Choose a town:
 
-				AddButton(50, 55 + 35 * i, 208, 209, i + 1, GumpButtonType.Reply, 0);
-				AddHtmlLocalized(75, 55 + 35 * i, 335, 40, entry.Name, false, false);
-			}
+            List<StuckMenuEntry> entries = GetStuckMenuEntriesForMobile(beheld);
 
-			AddButton(55, 263, 4005, 4007, 0, GumpButtonType.Reply, 0);
-			AddHtmlLocalized(90, 265, 200, 35, 1011012, false, false); // CANCEL
-		}
+            for (int i = 0; i < entries.Count; i++)
+            {
+                StuckMenuEntry entry = entries[i];
 
-		public void BeginClose()
-		{
-			StopClose();
+                AddButton(50, 55 + 35 * i, 208, 209, i + 1, GumpButtonType.Reply, 0);
+                AddHtmlLocalized(75, 55 + 35 * i, 335, 40, entry.Name, false, false);
+            }
 
-			m_Timer = new CloseTimer(m_Mobile);
-			m_Timer.Start();
+            AddButton(55, 263, 4005, 4007, 0, GumpButtonType.Reply, 0);
+            AddHtmlLocalized(90, 265, 200, 35, 1011012, false, false); // CANCEL
+        }
 
-			m_Mobile.Frozen = true;
-		}
+        public void BeginClose()
+        {
+            StopClose();
 
-		public void StopClose()
-		{
-			if (m_Timer != null)
-				m_Timer.Stop();
+            m_Timer = new CloseTimer(m_Mobile);
+            m_Timer.Start();
 
-			m_Mobile.Frozen = false;
-		}
+            m_Mobile.Frozen = true;
+        }
 
-		public override void OnResponse(NetState state, RelayInfo info)
-		{
-			StopClose();
+        public void StopClose()
+        {
+            if (m_Timer != null)
+                m_Timer.Stop();
 
-			if (info.ButtonID == 0)
-			{
-				if (m_Mobile == m_Sender)
-					m_Mobile.SendLocalizedMessage(1010588); // You choose not to go to any city.
-			}
-			else if (CityTradeSystem.HasTrade(m_Mobile))
-			{
-				m_Mobile.SendLocalizedMessage(1151733); // You cannot do that while carrying a Trade Order.
-			}
-			else
-			{
-				int index = info.ButtonID - 1;
-				StuckMenuEntry[] entries = IsTerMur(m_Mobile) ? m_TerMurEntries :
-					IsInSecondAgeArea(m_Mobile) ? m_T2AEntries : m_Entries;
+            m_Mobile.Frozen = false;
+        }
 
+        public override void OnResponse(NetState state, RelayInfo info)
+        {
+            StopClose();
 
-				if (index >= 0 && index < entries.Length)
-					Teleport(entries[index]);
-			}
-		}
+            if (info.ButtonID == 0)
+            {
+                if (m_Mobile == m_Sender)
+                    m_Mobile.SendLocalizedMessage(1010588); // You choose not to go to any city.
+            }
+            else if (CityTradeSystem.HasTrade(m_Mobile))
+            {
+                m_Mobile.SendLocalizedMessage(1151733); // You cannot do that while carrying a Trade Order.
+            }
+            else
+            {
+                int index = info.ButtonID - 1;
+                List<StuckMenuEntry> entries = GetStuckMenuEntriesForMobile(m_Mobile);
 
-		private static bool IsInSecondAgeArea(Mobile m)
-		{
-			if (m.Map != Map.Trammel && m.Map != Map.Felucca)
-				return false;
+                if (index >= 0 && index < entries.Count)
+                    Teleport(entries[index]);
+            }
+        }
 
-			if (m.X >= 5120 && m.Y >= 2304)
-				return true;
+        private void Teleport(StuckMenuEntry entry)
+        {
+            if (m_MarkUse)
+            {
+                m_Mobile.SendLocalizedMessage(1010589); // You will be teleported within the next two minutes.
 
-			if (m.Region.IsPartOf("Terathan Keep"))
-				return true;
+                new TeleportTimer(m_Mobile, entry, TimeSpan.FromSeconds(10.0 + (Utility.RandomDouble() * 110.0)))
+                    .Start();
+            }
+            else
+            {
+                new TeleportTimer(m_Mobile, entry, TimeSpan.Zero).Start();
+            }
+        }
 
-			return false;
-		}
+        private class CloseTimer : Timer
+        {
+            private readonly Mobile m_Mobile;
+            private readonly DateTime m_End;
 
-		private static bool IsTerMur(Mobile m)
-		{
-			return m.Map == Map.TerMur && !SpellHelper.IsEodon(m.Map, m.Location);
-		}
+            public CloseTimer(Mobile m)
+                : base(TimeSpan.Zero, TimeSpan.FromSeconds(1.0))
+            {
+                m_Mobile = m;
+                m_End = DateTime.UtcNow + TimeSpan.FromMinutes(3.0);
+            }
 
-		private void Teleport(StuckMenuEntry entry)
-		{
-			if (m_MarkUse)
-			{
-				m_Mobile.SendLocalizedMessage(1010589); // You will be teleported within the next two minutes.
+            protected override void OnTick()
+            {
+                if (m_Mobile.NetState == null || DateTime.UtcNow > m_End)
+                {
+                    m_Mobile.Frozen = false;
+                    m_Mobile.CloseGump(typeof(StuckMenu));
 
-				new TeleportTimer(m_Mobile, entry, TimeSpan.FromSeconds(10.0 + (Utility.RandomDouble() * 110.0)))
-					.Start();
-			}
-			else
-			{
-				new TeleportTimer(m_Mobile, entry, TimeSpan.Zero).Start();
-			}
-		}
+                    Stop();
+                }
+                else
+                {
+                    m_Mobile.Frozen = true;
+                }
+            }
+        }
 
-		private class CloseTimer : Timer
-		{
-			private readonly Mobile m_Mobile;
-			private readonly DateTime m_End;
+        private class TeleportTimer : Timer
+        {
+            private readonly Mobile m_Mobile;
+            private readonly StuckMenuEntry m_Destination;
+            private readonly DateTime m_End;
 
-			public CloseTimer(Mobile m)
-				: base(TimeSpan.Zero, TimeSpan.FromSeconds(1.0))
-			{
-				m_Mobile = m;
-				m_End = DateTime.UtcNow + TimeSpan.FromMinutes(3.0);
-			}
+            public TeleportTimer(Mobile mobile, StuckMenuEntry destination, TimeSpan delay)
+                : base(TimeSpan.Zero, TimeSpan.FromSeconds(1.0))
+            {
+                Priority = TimerPriority.TwoFiftyMS;
 
-			protected override void OnTick()
-			{
-				if (m_Mobile.NetState == null || DateTime.UtcNow > m_End)
-				{
-					m_Mobile.Frozen = false;
-					m_Mobile.CloseGump(typeof(StuckMenu));
+                m_Mobile = mobile;
+                m_Destination = destination;
+                m_End = DateTime.UtcNow + delay;
+            }
 
-					Stop();
-				}
-				else
-				{
-					m_Mobile.Frozen = true;
-				}
-			}
-		}
+            private void MovePetsOfLoggedCharacter(Point3D dest, Map destMap)
+            {
+                Map fromMap = m_Mobile.LogoutMap;
+                Point3D fromLoc = m_Mobile.LogoutLocation;
 
-		private class TeleportTimer : Timer
-		{
-			private readonly Mobile m_Mobile;
-			private readonly StuckMenuEntry m_Destination;
-			private readonly DateTime m_End;
+                System.Collections.Generic.List<BaseCreature> move = fromMap.GetMobilesInRange(fromLoc, 3)
+                    .Where(m => m is BaseCreature).Cast<BaseCreature>()
+                    .Where(pet =>
+                        pet.Controlled && pet.ControlMaster == m_Mobile && pet.ControlOrder == OrderType.Guard ||
+                        pet.ControlOrder == OrderType.Follow || pet.ControlOrder == OrderType.Come).ToList();
 
-			public  TeleportTimer(Mobile mobile, StuckMenuEntry destination, TimeSpan delay)
-				: base(TimeSpan.Zero, TimeSpan.FromSeconds(1.0))
-			{
-				Priority = TimerPriority.TwoFiftyMS;
+                move.ForEach(x => x.MoveToWorld(dest, destMap));
+            }
 
-				m_Mobile = mobile;
-				m_Destination = destination;
-				m_End = DateTime.UtcNow + delay;
-			}
+            protected override void OnTick()
+            {
+                if (DateTime.UtcNow < m_End)
+                {
+                    m_Mobile.Frozen = true;
+                }
+                else if (CityTradeSystem.HasTrade(m_Mobile))
+                {
+                    m_Mobile.Frozen = false;
+                    Stop();
+                    m_Mobile.SendLocalizedMessage(1151733); // You cannot do that while carrying a Trade Order.
+                }
+                else
+                {
+                    m_Mobile.Frozen = false;
+                    Stop();
 
-			private void MovePetsOfLoggedCharacter(Point3D dest, Map destMap)
-			{
-				Map fromMap = m_Mobile.LogoutMap;
-				Point3D fromLoc = m_Mobile.LogoutLocation;
+                    int idx = Utility.Random(m_Destination.Locations.Length);
+                    Point3D dest = m_Destination.Locations[idx];
 
-				System.Collections.Generic.List<BaseCreature> move = fromMap.GetMobilesInRange(fromLoc, 3)
-					.Where(m => m is BaseCreature).Cast<BaseCreature>()
-					.Where(pet =>
-						pet.Controlled && pet.ControlMaster == m_Mobile && pet.ControlOrder == OrderType.Guard ||
-						pet.ControlOrder == OrderType.Follow || pet.ControlOrder == OrderType.Come).ToList();
+                    Map destMap = Map.Felucca;
+                    // if (m_Mobile.Map == Map.Trammel || SpellHelper.IsEodon(m_Mobile.Map, m_Mobile.Location))
+                    //  destMap = Map.Trammel;
+                    // else if (m_Mobile.Map == Map.Felucca)
+                    //  destMap = Map.Felucca;
+                    // else if (m_Mobile.Map == Map.TerMur && !SpellHelper.IsEodon(m_Mobile.Map, m_Mobile.Location))
+                    //  destMap = Map.TerMur;
+                    // else if (m_Mobile.Map == Map.Internal)
+                    //  destMap = m_Mobile.LogoutMap == Map.Felucca ? Map.Felucca : Map.Trammel;
+                    // else
+                    //  destMap = m_Mobile.Murderer ? Map.Felucca : Map.Trammel;
+                    //
+                    // if (destMap == Map.Trammel && Siege.SiegeShard)
+                    //  destMap = Map.Felucca;
 
-				move.ForEach(x => x.MoveToWorld(dest, destMap));
-			}
+                    if (m_Mobile.Map != Map.Internal)
+                    {
+                        BaseCreature.TeleportPets(m_Mobile, dest, destMap);
+                        m_Mobile.MoveToWorld(dest, destMap);
+                    }
+                    else
+                    {
+                        // for shards without auto stabling
+                        MovePetsOfLoggedCharacter(dest, destMap);
 
-			protected override void OnTick()
-			{
-				if (DateTime.UtcNow < m_End)
-				{
-					m_Mobile.Frozen = true;
-				}
-				else if (CityTradeSystem.HasTrade(m_Mobile))
-				{
-					m_Mobile.Frozen = false;
-					Stop();
-					m_Mobile.SendLocalizedMessage(1151733); // You cannot do that while carrying a Trade Order.
-				}
-				else
-				{
-					m_Mobile.Frozen = false;
-					Stop();
-
-					int idx = Utility.Random(m_Destination.Locations.Length);
-					Point3D dest = m_Destination.Locations[idx];
-
-					Map destMap = Map.Felucca;
-					// if (m_Mobile.Map == Map.Trammel || SpellHelper.IsEodon(m_Mobile.Map, m_Mobile.Location))
-					// 	destMap = Map.Trammel;
-					// else if (m_Mobile.Map == Map.Felucca)
-					// 	destMap = Map.Felucca;
-					// else if (m_Mobile.Map == Map.TerMur && !SpellHelper.IsEodon(m_Mobile.Map, m_Mobile.Location))
-					// 	destMap = Map.TerMur;
-					// else if (m_Mobile.Map == Map.Internal)
-					// 	destMap = m_Mobile.LogoutMap == Map.Felucca ? Map.Felucca : Map.Trammel;
-					// else
-					// 	destMap = m_Mobile.Murderer ? Map.Felucca : Map.Trammel;
-					//
-					// if (destMap == Map.Trammel && Siege.SiegeShard)
-					// 	destMap = Map.Felucca;
-
-					if (m_Mobile.Map != Map.Internal)
-					{
-						BaseCreature.TeleportPets(m_Mobile, dest, destMap);
-						m_Mobile.MoveToWorld(dest, destMap);
-					}
-					else
-					{
-						// for shards without auto stabling
-						MovePetsOfLoggedCharacter(dest, destMap);
-
-						m_Mobile.LogoutLocation = dest;
-						m_Mobile.LogoutMap = destMap;
-					}
-				}
-			}
-		}
-	}
+                        m_Mobile.LogoutLocation = dest;
+                        m_Mobile.LogoutMap = destMap;
+                    }
+                }
+            }
+        }
+    }
 }
