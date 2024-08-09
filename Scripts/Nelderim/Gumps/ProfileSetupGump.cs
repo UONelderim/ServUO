8b67f9f7a1759b0c844385900cf8a496cff17c44
kaczy <kaczyf93@gmail.com>
2024-08-09 12:00:01 +0200

    ProfileSetupGump improvements

--- Scripts/Nelderim/Gumps/ProfileSetupGump.cs
+++ Scripts/Nelderim/Gumps/ProfileSetupGump.cs
@@ -1,6 +1,5 @@
 using Server.Gumps;
 using Server.Misc;
-using Server.Network;
 using Server.Regions;
 
 namespace Server.Nelderim.Gumps
@@ -9,21 +8,20 @@ namespace Server.Nelderim.Gumps
 	{
 		public static void Initialize()
 		{
-			EventSink.ChangeProfileRequest += e => Apply(e.Beheld);
+			EventSink.ChangeProfileRequest += e => Check(e.Beholder, true);
+			EventSink.Login += e => Check(e.Mobile);
 		}
 		
-		public static void Apply(Mobile m)
+		public static void Check(Mobile m, bool showErrorMessage = false)
 		{
 			m.CloseGump<ProfileSetupGump>();
-			if (m.IsStaff() || m.Region is not RaceRoomRegion)
-				return;
 			
-			if (m.Profile == null || m.Profile.Trim().Length < 50 )
+			if (m.IsPlayer() && (m.Region is RaceRoomRegion || m.Region is JailRegion) && (m.Profile == null || m.Profile.Trim().Length < 50 ))
 			{
 				m.Frozen = true;
 				if(m.Profile == null || m.Profile.Trim().Length == 0)
 					m.Profile = "...";
-				m.SendGump(new ProfileSetupGump(m));
+				m.SendGump(new ProfileSetupGump(m, showErrorMessage));
 			}
 			else
 			{
@@ -33,7 +31,7 @@ namespace Server.Nelderim.Gumps
 		
 		private Mobile _from;
 		
-		public ProfileSetupGump(Mobile from): base(320, 25)
+		public ProfileSetupGump(Mobile from, bool showErrorMessage = false): base(320, 25)
 		{
 			_from = from;
 			
@@ -44,7 +42,7 @@ namespace Server.Nelderim.Gumps
 			Dragable = false;
 			Resizable = false;
 			AddPage(0);
-			AddBackground(0, 0, 550, 450, 302);
+			AddBackground(0, 0, 550, 480, 302);
 			AddImage(15,10, 4506);
 			AddImage(15,100, 4506);
 			AddImage(15,190, 4506);
@@ -54,24 +52,37 @@ namespace Server.Nelderim.Gumps
 			                          "Powinien to być ogólny opis wyglądu i cech charakterystycznych widocznych gołym okiem, " +
 			                          "pozbawiony informacji na temat charakteru, usposobienia czy pochodzenia postaci " +
 			                          "– tylko to, co widać, słychać i czuć.", false, false);
-			AddLabel(80, 150, 0, "Co może zawierać opis postaci:");
-			AddLabel(90, 170, 66, "- opis twarzy: oczy, włosy, zarost, ewentualne blizny, nos");
-			AddLabel(90, 190, 66, "- przybliżony wiek (np: pełen wigoru młodzian lub kulawy starzec)");
-			AddLabel(90, 210, 66, "- opis postury, przybliżony wzrost (np: średniego wzrostu kobieta)");
-			AddLabel(90, 230, 66, "- charakterystyczne znaki na ciele (blizny, tatuaże)");
-			AddLabel(90, 250, 66, "- dodatkowe: zapach, rodzaj spojrzenia, styl chodu");
-			AddLabel(80, 290, 0, "Czego nie powinien zawierać opis postaci:");
-			AddLabel(90, 310, 37, "- historii postaci");
-			AddLabel(90, 330, 37, "- celów i dążeń postaci");
-			AddLabel(90, 350, 37, "- wykonywanego zawodu");
-			AddLabel(90, 390, 42, "Zamknij opis postaci aby zatwierdzić zmianę.");
+			
+			if (showErrorMessage)
+			{
+				AddHtml(80, 135, 300, 20, B(FONT("!!! OPIS JEST ZA KRÓTKI !!!",0xCC0000, 0)), false, false);
+			}
+			var y = 160;
+			AddLabel(80, PostInc(ref y), 0, "Co może zawierać opis postaci:");
+			AddLabel(90, PostInc(ref y), 66, "- opis twarzy: oczy, włosy, zarost, ewentualne blizny, nos");
+			AddLabel(90, PostInc(ref y), 66, "- przybliżony wiek (np: pełen wigoru młodzian lub kulawy starzec)");
+			AddLabel(90, PostInc(ref y), 66, "- opis postury, przybliżony wzrost (np: średniego wzrostu kobieta)");
+			AddLabel(90, PostInc(ref y), 66, "- charakterystyczne znaki na ciele (blizny, tatuaże)");
+			AddLabel(90, PostInc(ref y, 40), 66, "- dodatkowe: zapach, rodzaj spojrzenia, styl chodu");
+			AddLabel(80, PostInc(ref y), 0, "Czego nie powinien zawierać opis postaci:");
+			AddLabel(90, PostInc(ref y), 37, "- historii postaci");
+			AddLabel(90, PostInc(ref y), 37, "- celów i dążeń postaci");
+			AddLabel(90, PostInc(ref y, 40), 37, "- wykonywanego zawodu");
+			AddLabel(72, y, 66, "!");
+			AddLabel(90, PostInc(ref y, 30), 43, "Zamknij opis postaci aby zatwierdzić zmianę.");
+			AddLabel(70, y, 66, "?");
+			// AddHtml(80, y, 20, 20, B(FONT("?", 0x00ff00, 0)), false, false);
+			AddLabel(90, PostInc(ref y, -35), 43, "Opis postaci można edytować otwierajac lewy zwój w paperdoll.");
+			AddImage(485, y - 18, 2437);
+			AddImage(480, y, 2002);
+			AddImage(499, y, 2002);
 		}
-		
 
-		public override void OnResponse(NetState sender, RelayInfo info)
+		private int PostInc(ref int val, int inc = 20)
 		{
-			if (info.ButtonID == 0)
-				Apply(_from);
+			var oldVal = val;
+			val += inc;
+			return oldVal;
 		}
 	}
 }
