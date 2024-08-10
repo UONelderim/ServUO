5a5f4dabc8745a96c7bce9178d90753def1193ad
kaczy <kaczyf93@gmail.com>
2024-08-10 22:27:57 +0200

    CreateFood gump

--- Scripts/Spells/First/CreateFood.cs
+++ Scripts/Spells/First/CreateFood.cs
@@ -1,5 +1,7 @@
 using Server.Items;
 using System;
+using Server.Gumps;
+using Server.Network;
 
 namespace Server.Spells.First
 {
@@ -12,7 +14,7 @@ namespace Server.Spells.First
             Reagent.Garlic,
             Reagent.Ginseng,
             Reagent.MandrakeRoot);
-        private static readonly FoodInfo[] m_Food = new FoodInfo[]
+        internal static readonly FoodInfo[] m_Food = new FoodInfo[]
         {
             new FoodInfo(typeof(Grapes), "a grape bunch"),
             new FoodInfo(typeof(Ham), "a ham"),
@@ -31,23 +33,13 @@ namespace Server.Spells.First
         }
 
         public override SpellCircle Circle => SpellCircle.First;
+
+        
         public override void OnCast()
         {
             if (CheckSequence())
             {
-                FoodInfo foodInfo = m_Food[Utility.Random(m_Food.Length)];
-                Item food = foodInfo.Create();
-
-                if (food != null)
-                {
-                    Caster.AddToBackpack(food);
-
-                    // You magically create food in your backpack:
-                    Caster.SendLocalizedMessage(1042695, true, " " + foodInfo.Name);
-
-                    Caster.FixedParticles(0, 10, 5, 2003, EffectLayer.RightHand);
-                    Caster.PlaySound(0x1E2);
-                }
+	            Caster.SendGump(new CreateFoodGump(Caster));
             }
 
             FinishSequence();
@@ -62,6 +54,10 @@ namespace Server.Spells.First
         {
             m_Type = type;
             m_Name = name;
+            //Hacky but whatever
+            var temp = Create();
+            ItemId = temp.ItemID;
+            temp.Delete();
         }
 
         public Type Type
@@ -86,9 +82,77 @@ namespace Server.Spells.First
                 m_Name = value;
             }
         }
+        public int ItemId { get;  }
         public Item Create()
         {
             return Loot.Construct(m_Type);
         }
+    }
+
+    public class CreateFoodGump : Gump
+    {
+	    const int Margin = 5;
+	    const int ButtonWidth = 80;
+	    const int ButtonHeight = 60;
+	    const int ButtonArt = 0x919;
+	    const int ColumnsCount = 3;
+
+	    private long _CastTime;
+	    private Mobile _Caster;
+	    private FoodInfo[] _FoodInfo;
+	    public CreateFoodGump(Mobile caster) : base(50, 50)
+	    {
+		    _CastTime = Core.TickCount;
+		    caster.CloseGump<CreateFoodGump>();
+		    _Caster = caster;
+		    var foodInfo = CreateFoodSpell.m_Food;
+
+		    var rowsCount = (int)Math.Ceiling(foodInfo.Length / (double)ColumnsCount);
+
+		    var totalWidth = ButtonWidth * ColumnsCount + Margin * 2;
+		    var totalHeight = ButtonHeight * rowsCount + Margin * 2;
+			    
+		    AddPage(0);
+		    AddBackground(0, 0, totalWidth, totalHeight, 3000);
+
+		    for (int i = 0; i < foodInfo.Length; i++)
+		    {
+			    var entry = foodInfo[i];
+			    var x = i % ColumnsCount;
+			    var y = i / ColumnsCount;
+			    AddImageTiledButton(Margin + x * ButtonWidth, Margin + y * ButtonHeight, 
+				    ButtonArt, ButtonArt, i + 1, GumpButtonType.Reply, 0, 
+				    entry.ItemId, 0, 17, 23); 
+		    }
+	    }
+
+	    public override void OnResponse(NetState sender, RelayInfo info)
+	    {
+		    if(_CastTime + 10000 < Core.TickCount) //10s
+		    {
+			    _Caster.SendLocalizedMessage(500641); //Straciles koncentracje, a zaklecie rozproszylo sie. 
+			    return;
+		    }
+
+		    var index = info.ButtonID - 1;
+		    if(index < 0 || index >= CreateFoodSpell.m_Food.Length)
+		    {
+			    _Caster.SendMessage("OSZUST!");
+			    return;
+		    }
+		    var foodInfo = CreateFoodSpell.m_Food[index];
+		    var food = foodInfo.Create();
+		    
+		    if (food != null)
+		    {
+		        _Caster.AddToBackpack(food);
+		    
+		        // You magically create food in your backpack:
+		        _Caster.SendLocalizedMessage(1042695, true, " " + foodInfo.Name);
+		    
+		        _Caster.FixedParticles(0, 10, 5, 2003, EffectLayer.RightHand);
+		        _Caster.PlaySound(0x1E2);
+		    }
+	    }
     }
 }
