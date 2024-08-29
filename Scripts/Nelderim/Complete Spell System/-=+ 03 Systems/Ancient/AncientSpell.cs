aa439bb3354faee26ced7eb4202a03b7e4d5a961
Maupishon <rapfamilly@gmail.com>
2024-08-29 12:55:39 +0200

    Fix

--- Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Ancient/AncientSpell.cs
+++ Scripts/Nelderim/Complete Spell System/-=+ 03 Systems/Ancient/AncientSpell.cs
@@ -33,7 +33,7 @@ namespace Server.ACC.CSS.Systems.Ancient
 			if (!base.CheckFizzle())
 				return false;
 			
-			if (Caster is PlayerMobile && !((PlayerMobile)Caster).Ancient)
+			if (Caster is PlayerMobile && !((PlayerMobile)Caster).Nature)
 			{
 				Caster.SendLocalizedMessage(3060182); // Aby korzystac z tych zaklec, musisz wykonac odpowiednie zadanie..
 				return false;
@@ -41,6 +41,11 @@ namespace Server.ACC.CSS.Systems.Ancient
 
 
 			return true;
+		}
+		
+		public override int GetMana()
+		{
+			return RequiredMana;
 		}
 
 		public override TimeSpan GetCastDelay()
