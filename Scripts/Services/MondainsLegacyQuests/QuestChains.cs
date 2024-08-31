399ea395845cbfaff3fa7ec1d8448940c301a0b0
Maupishon <rapfamilly@gmail.com>
2024-08-31 12:48:22 +0200

    Ancient Quest

--- Scripts/Services/MondainsLegacyQuests/QuestChains.cs
+++ Scripts/Services/MondainsLegacyQuests/QuestChains.cs
@@ -43,7 +43,7 @@ namespace Server.Engines.Quests
         DeathKnight = 32,
         Nature = 33,
         Ancient = 34,
-        
+        Avatar = 35,
     }
 
     public class BaseChain
@@ -85,6 +85,13 @@ namespace Server.Engines.Quests
             Chains[(int)QuestChain.PaladinsOfTrinsic] = new Type[] { typeof(PaladinsOfTrinsic), typeof(PaladinsOfTrinsic2) };
             Chains[(int)QuestChain.RightingWrong] = new Type[] { typeof(RightingWrongQuest2), typeof(RightingWrongQuest3), typeof(RightingWrongQuest4) };
             Chains[(int)QuestChain.Ritual] = new Type[] { typeof(RitualQuest.ScalesOfADreamSerpentQuest), typeof(RitualQuest.TearsOfASoulbinderQuest), typeof(RitualQuest.PristineCrystalLotusQuest) };
+            //SpecialSkills
+            Chains[(int)QuestChain.Mystic] = new Type[] { typeof(MysticPhase2Quest), typeof(MysticPhase3Quest), typeof(MysticPhase4Quest), typeof(MysticPhase5Quest) };
+            Chains[(int)QuestChain.Cleric] = new Type[] { typeof(ClericPhase2Quest), typeof(ClericPhase3Quest), typeof(ClericPhase4Quest), typeof(ClericPhase5Quest) };
+            Chains[(int)QuestChain.DeathKnight] = new Type[] { typeof(DeathKnightPhase2Quest), typeof(DeathKnightPhase3Quest), typeof(DeathKnightPhase4Quest), typeof(DeathKnightPhase5Quest) };
+            Chains[(int)QuestChain.Nature] = new Type[] { typeof(NaturePhase2Quest), typeof(NaturePhase3Quest), typeof(NaturePhase4Quest), typeof(NaturePhase5Quest) };
+            Chains[(int)QuestChain.Ancient] = new Type[] { typeof(AncientPhase2Quest), typeof(AncientPhase3Quest), typeof(AncientPhase4Quest), typeof(AncientPhase5Quest) };
+            Chains[(int)QuestChain.Avatar] = new Type[] { typeof(AvatarPhase2Quest), typeof(AvatarPhase3Quest), typeof(AvatarPhase4Quest), typeof(AvatarPhase5Quest) };
         }
 
         public Type CurrentQuest { get; set; }
