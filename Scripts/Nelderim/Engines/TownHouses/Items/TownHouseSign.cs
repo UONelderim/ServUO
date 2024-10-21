cc2fda64379909f4ef030577a8528082523de858
kaczy <kaczyf93@gmail.com>
2024-10-21 12:11:03 +0200

    Fix town houses

--- Scripts/Nelderim/Engines/TownHouses/Items/TownHouseSign.cs
+++ Scripts/Nelderim/Engines/TownHouses/Items/TownHouseSign.cs
@@ -636,7 +636,10 @@ namespace Knives.TownHouses
 				House = new TownHouse(m, this, c_Locks, c_Secures);
 				int signId = (int)(northSouth ? SignIDs.HouseSignNS : SignIDs.HouseSignWE);
 				House.ChangeSignType(signId);
-
+				
+				House.Components.Resize(maxX - minX, maxY - minY);
+				House.Components.Add(0x520, House.Components.Width - 1, House.Components.Height - 1, -5);
+				
 				House.Location = new Point3D(minX, minY, c_MinZ);
 				House.Map = Map;
 				House.Region.GoLocation = c_BanLoc;
