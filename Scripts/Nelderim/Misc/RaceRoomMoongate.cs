01ae7cccf0e3f7e77a53a6fa1fc12c45a1c81290
kaczy <kaczyf93@gmail.com>
2024-11-22 11:41:28 +0100

    Fix raceroom moongate serialization

--- Scripts/Nelderim/Misc/RaceRoomMoongate.cs
+++ Scripts/Nelderim/Misc/RaceRoomMoongate.cs
@@ -47,5 +47,17 @@ namespace Server.Nelderim.Misc
 		public RaceRoomMoongate(Serial serial) : base(serial)
 		{
 		}
+
+		public override void Serialize(GenericWriter writer)
+		{
+			base.Serialize(writer);
+			writer.Write((int)0);//Version
+		}
+
+		public override void Deserialize(GenericReader reader)
+		{
+			base.Deserialize(reader);
+			var version = reader.ReadInt();
+		}
 	}
 }
