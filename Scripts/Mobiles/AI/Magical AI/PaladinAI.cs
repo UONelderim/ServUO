cb5a8af804670653ffb01839670617a119ee2232
Maupishon <64133911+Maupishon@users.noreply.github.com>
2024-11-29 10:37:34 +0100

    PaladinAI.cs healing

--- Scripts/Mobiles/AI/Magical AI/PaladinAI.cs
+++ Scripts/Mobiles/AI/Magical AI/PaladinAI.cs
@@ -67,10 +67,16 @@ namespace Server.Mobiles
 
         public override Spell GetHealSpell()
         {
-            if (m_Mobile.Mana > 10)
-                return new CloseWoundsSpell(m_Mobile, null);
+	        if (!(m_Mobile.Hits <= m_Mobile.HitsMax * 0.5)) return null;
+	        if (m_Mobile.Mana > 10)
+	        {
+		        if (m_Mobile.Combatant != null && m_Mobile.Combatant.Hits > m_Mobile.Hits)
+		        {
+			        return new CloseWoundsSpell(m_Mobile, null);
+		        }
+	        }
 
-            return null;
+	        return null;
         }
 
         public override Spell GetCureSpell()
