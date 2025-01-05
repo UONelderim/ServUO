cbdb157594d72a18ae5bfb62e9b2b928d6b4e160
kaczy <kaczyf93@gmail.com>
2025-01-05 19:27:20 +0100

    Reworked AzysBracelet

--- Scripts/Nelderim/Items/Artifacts/ZleceniaBossy/AzysBracelet.cs
+++ Scripts/Nelderim/Items/Artifacts/ZleceniaBossy/AzysBracelet.cs
@@ -2,8 +2,6 @@ namespace Server.Items
 {
     public class AzysBracelet : BaseBracelet
     {
-        private bool isSecondClick = false; 
-
         public override int InitMinHits => 50;
         public override int InitMaxHits => 50;
 
@@ -11,15 +9,14 @@ namespace Server.Items
         public AzysBracelet()
             : base(0x1F06)
         {
-            this.Weight = 0.1;
-            this.Name = "Branzoleta Rzemieslnika z Ferion";
-            this.Hue = 0x78A;
-            this.Attributes.Luck = -100;
-            this.Attributes.BonusStr = 15;
-            this.SkillBonuses.Skill_2_Name = SkillName.Mining;
-            this.SkillBonuses.Skill_2_Value = 5;
-            this.SkillBonuses.Skill_1_Name = SkillName.Inscribe;
-            this.SkillBonuses.Skill_1_Value = 0;
+            Weight = 0.1;
+            Name = "Branzoleta Rzemieslnika z Ferion";
+            Hue = 0x78A;
+            
+            Attributes.Luck = -100;
+            Attributes.BonusStr = 15;
+            SkillBonuses.Skill_1_Name = SkillName.Mining;
+            SkillBonuses.Skill_1_Value = 5;
             Label1 = "*branzoleta ma miejsce na 3 palce - czy odwazysz sie jej dotknac?*";
         }
 
@@ -44,26 +41,19 @@ namespace Server.Items
 
         public override void OnDoubleClick(Mobile from)
         {
-            if (!isSecondClick)
-            {
-                this.Attributes.BonusInt = 10;
-                this.Attributes.BonusStr = -30;
-                this.Weight = 5.0;
-                this.SkillBonuses.Skill_1_Name = SkillName.Inscribe;
-                this.SkillBonuses.Skill_1_Value = 5;
-
-                isSecondClick = true;
-            }
+	        if (SkillBonuses.Skill_1_Name == SkillName.Mining)
+	        {
+		        Weight = 5.0;
+		        Attributes.BonusInt = 10;
+		        Attributes.BonusStr = -30;
+		        SkillBonuses.Skill_1_Name = SkillName.Inscribe;
+	        }
             else
             {
-                // On the second double-click, revert attributes and weight to original values
-                this.Attributes.BonusInt = 0; // Reset BonusDex
-                this.Attributes.BonusStr = 15; // Reset BonusInt to the original value
-                this.Weight = 0.1; // Reset weight to the original value
-                this.SkillBonuses.Skill_1_Name = SkillName.Inscribe;
-                this.SkillBonuses.Skill_1_Value = 0;
-
-                isSecondClick = false;
+	            Weight = 0.1;
+	            Attributes.BonusInt = 0;
+	            Attributes.BonusStr = 15;
+	            SkillBonuses.Skill_1_Name = SkillName.Mining;
             }
 
             base.OnDoubleClick(from);
