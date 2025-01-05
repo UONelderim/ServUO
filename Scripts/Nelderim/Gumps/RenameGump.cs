054e87affa8db08fc54d2c869210c6f0bacba1e3
kaczy <kaczyf93@gmail.com>
2025-01-05 10:19:58 +0100

    Gender choice in rename gump

--- Scripts/Nelderim/Gumps/RenameGump.cs
+++ Scripts/Nelderim/Gumps/RenameGump.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using Knives.TownHouses;
 using Nelderim;
 using Server.Commands;
 using Server.Misc;
@@ -35,25 +36,36 @@ namespace Server.Gumps
 				});
 		}
 
-		public RenameGump(string reason = "") : base(50, 50)
+		public RenameGump(bool female = false, string reason = "") : base(50, 50)
 		{
 			Closable = true;
 			Disposable = true;
 			Dragable = true;
 			Resizable = false;
 			AddPage(0);
-			AddBackground(50, 50, 250, 150, 2620);
-			AddLabel(100, 67, 1160, "Nadaj imię twojej postaci");
-			AddLabel(110, 87, 0x20, reason);
-			AddImageTiled(70, 115, 210, 20, 0xBBC);
-			AddTextEntry(72, 115, 206, 20, 1359, 0, "", 30);
-			AddButton(145, 160, 238, 239, 2, GumpButtonType.Reply, 0);
+			AddBackground(0, 0, 250, 160, 2620);
+			
+			AddHtml(0, 15, 250, 20, FONT(CENTER("Nadaj tożsamość twojej postaci"), 0xFFFF00), false, false);
+			
+			AddLabel(17, 45, 1160, "Imie:");
+			AddImageTiled(50, 45, 185, 20, 0xBBC);
+			AddTextEntry(52, 45, 183, 20, 1359, 0, "", 30);
+			
+			AddRadio(17, 70, 9721, 0x86A, !female, 1);
+			AddLabel(52, 75, 1160, "Mężczyzna");
+			AddRadio(137, 70, 9721, 0x86A, female, 2); 
+			AddLabel(172, 75, 1160, "Kobieta");
+			
+			AddHtml(0, 102, 250, 20,FONT(CENTER(reason), 0xFF0000), false, false);
+			
+			AddButton(95, 125, 238, 239, 2, GumpButtonType.Reply, 0);
 		}
 
 		public override void OnResponse(NetState sender, RelayInfo info)
 		{
 			var m = sender.Mobile;
 			var newName = info.GetTextEntry(0).Text;
+			var female = info.Switches.Length > 0 && info.Switches[0] == 2;
 			if (info.ButtonID == 0)
 			{
 				if (m.IsPlayer())
@@ -61,19 +73,22 @@ namespace Server.Gumps
 			}
 			else if (newName.Length <= 3)
 			{
-				m.SendGump(new RenameGump("Imie jest za krótkie"));
+				m.SendGump(new RenameGump(female, "Imie jest za krótkie"));
 			}
 			else if (newName.StartsWith(DEFAULT_PREFIX))
 			{
-				m.SendGump(new RenameGump("Imie jest niedozwolone"));
+				m.SendGump(new RenameGump(female, "Imie jest niedozwolone"));
 			}
 			else
 			{
 				var sanitisedName = String.Join(' ', newName.Split(' ').Select(Translate.CapitalizeFirstLetter));
 				m.Name = sanitisedName;
+				m.Female = female;
 				m.Frozen = false;
+				m.BodyValue = m.Female ? 0x191 : 0x190;
 				Paperdoll.Send(m, m);
 				NewCharSetupGump.Check(m);
+				m.SendMessage(0x40, "Nadales postaci nowa tozsamosc");
 			}
 		}
 	}
--- Server/Nelderim/GumpHtmlExt.cs
+++ Server/Nelderim/GumpHtmlExt.cs
@@ -5,7 +5,7 @@ namespace Server.Gumps
 		public string B(string text) => $"<B>{text}</B>";
 		public string CENTER(string text) => $"<CENTER>{text}</CENTER>";
 
-		public string FONT(string text, int color = 0x00000, int fontId = 0) =>
+		public string FONT(string text, int color = 0x000000, int fontId = 0) =>
 			$"<BASEFONT SIZE={fontId} COLOR=#{color:X6}>{text}</BASEFONT>";
 	}
 }
