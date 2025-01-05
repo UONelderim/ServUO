f2cc54a47f090c9e190c0262e65c82c71284d1b2
kaczy <kaczyf93@gmail.com>
2025-01-05 11:27:27 +0100

    Reworked StaffOfSnakes, removed BaseMagicObject

--- Scripts/Nelderim/Items/Artifacts/ZleceniaBossy/StaffofSnakes.cs
+++ Scripts/Nelderim/Items/Artifacts/ZleceniaBossy/StaffofSnakes.cs
@@ -1,13 +1,30 @@
+using System;
+using System.Collections.Generic;
+using Server.Network;
 using Server.Spells.Eighth;
-
 
 namespace Server.Items
 {
 	public class StaffofSnakes : GnarledStaff
 	{
-		public override int InitMinHits => 60;
-		public override int InitMaxHits => 60;
+		private static readonly TimeSpan _UseDelay = TimeSpan.FromSeconds(4.0);
 
+		private int m_Charges;
+
+		[CommandProperty(AccessLevel.GameMaster)]
+		public int Charges
+		{
+			get => m_Charges;
+			set
+			{
+				m_Charges = value;
+				InvalidateProperties();
+			}
+		}
+
+		public override int InitMinHits => 60;
+
+		public override int InitMaxHits => 60;
 
 		[Constructable]
 		public StaffofSnakes()
@@ -15,7 +32,7 @@ namespace Server.Items
 			Weight = 5.0;
 			Hue = 0x304;
 			Name = "Kij Przywolywacza Demonow";
-			
+
 			AosElementDamages.Poison = 100;
 			Attributes.SpellChanneling = 1;
 			Attributes.WeaponDamage = 35;
@@ -25,36 +42,57 @@ namespace Server.Items
 			WeaponAttributes.HitLeechMana = 50;
 		}
 
-
-        public override void AddNameProperties(ObjectPropertyList list)
+		public override void GetProperties(ObjectPropertyList list)
 		{
-            base.AddNameProperties(list);
-			list.Add( 1049644, "Przyzywa Stworzenia" );
+			base.GetProperties(list);
+			list.Add(1060741, m_Charges.ToString());
+		}
+
+		public override void AddNameProperties(ObjectPropertyList list)
+		{
+			base.AddNameProperties(list);
+			list.Add(1049644, "Przyzywa Stworzenia");
+		}
+
+		public override void OnDoubleClick(Mobile from)
+		{
+			if (Parent != from)
+			{
+				from.SendLocalizedMessage(502641); // You must equip this item to use it.
+				return;
+			}
+
+			if (Charges <= 0)
+			{
+				from.SendLocalizedMessage(502412); // There are no charges left on that item.
+				return;
+			}
+
+			if (from.BeginAction(typeof(StaffofSnakes)))
+			{
+				Charges--;
+				new SummonDaemonSpell(from, this).Cast();
+				Timer.DelayCall(_UseDelay, from.EndAction, typeof(StaffofSnakes));
+			}
+		}
+		
+		public StaffofSnakes(Serial serial) : base(serial)
+		{
+		}
+
+		public override void Serialize(GenericWriter writer)
+		{
+			base.Serialize(writer);
+			writer.Write((int)0); // version
+			writer.Write(m_Charges);
 		}
 
 
-		public StaffofSnakes( Serial serial ) : base( serial )
+		public override void Deserialize(GenericReader reader)
 		{
-		}
-
-
-		public override void Serialize( GenericWriter writer )
-		{
-			base.Serialize( writer );
-			writer.Write( (int) 0 ); // version
-		}
-
-
-		public override void Deserialize( GenericReader reader )
-		{
-			base.Deserialize( reader );
+			base.Deserialize(reader);
 			int version = reader.ReadInt();
-	    }
-
-		//
-		// public override void OnMagicObjectUse( Mobile from )
-		// {
-		// 	Cast( new SummonDaemonSpell( from, this ) );
-		// }
+			m_Charges = reader.ReadInt();
+		}
 	}
 }
