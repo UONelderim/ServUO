32db83c319985e20dc6501985e80c29b287b16c9
kaczy <kaczyf93@gmail.com>
2025-01-05 21:57:10 +0100

    Reworked RodOfResurrection

--- Scripts/Nelderim/Items/Artifacts/ZleceniaBossy/RodOfResurrection.cs
+++ Scripts/Nelderim/Items/Artifacts/ZleceniaBossy/RodOfResurrection.cs
@@ -4,23 +4,20 @@ using Server.Gumps;
 
 namespace Server.Items
 {
-	public enum ResurrectEffect
-	{
-		Charges
-	}
-
 	public class RodOfResurrection : Scepter
 	{
-		private ResurrectEffect m_ResurrectEffect;
-		private int m_Charges;
+		private int _Charges;
 
-		[CommandProperty( AccessLevel.GameMaster )]
-		public ResurrectEffect Effect { get => m_ResurrectEffect;
-			set{ m_ResurrectEffect = value; InvalidateProperties(); } }
-
-		[CommandProperty( AccessLevel.GameMaster )]
-		public int Charges { get => m_Charges;
-			set{ m_Charges = value; InvalidateProperties(); } }
+		[CommandProperty(AccessLevel.GameMaster)]
+		public int Charges
+		{
+			get => _Charges;
+			set
+			{
+				_Charges = value;
+				InvalidateProperties();
+			}
+		}
 
 		[Constructable]
 		public RodOfResurrection()
@@ -28,168 +25,135 @@ namespace Server.Items
 			Name = "Berlo Wskrzeszenia";
 			Hue = 200;
 			Charges = 100;
-			WeaponAttributes.UseBestSkill = 1;
+			Attributes.AttackChance = 12;
 			Attributes.WeaponDamage = 35;
 			WeaponAttributes.HitHarm = 30;
 			WeaponAttributes.HitLeechHits = 25;
-			Attributes.AttackChance = 12;
 			WeaponAttributes.LowerStatReq = 70;
-			
-			
+			WeaponAttributes.UseBestSkill = 1;
+
 			Slayer = SlayerName.Exorcism;
 		}
-		
-		public void GetDamageTypes( Mobile wielder, out int phys, out int fire, out int cold, out int pois, out int nrgy )
+
+		public override void GetDamageTypes(Mobile wielder,
+			out int phys,
+			out int fire,
+			out int cold,
+			out int pois,
+			out int nrgy,
+			out int chaos,
+			out int direct)
 		{
 			cold = 100;
-
-			 fire = pois = phys = nrgy = 0;
+			fire = pois = phys = nrgy = chaos = direct = 0;
 		}
 
-		public override void OnDoubleClick( Mobile from )
+		public override void OnDoubleClick(Mobile from)
 		{
-			if ( Parent != from )
+			if (Parent != from)
 			{
-				from.SendMessage( "You must be holding the rod to resurrect." );
+				from.SendLocalizedMessage(502641); // You must equip this item to use it.
 			}
-			else if ( Charges < 1 )
+			else if (Charges <= 0)
 			{
-				from.SendMessage( "All of the magic has been drained from the rod." );
-				this.Delete();
+				from.SendLocalizedMessage(502412); // There are no charges left on that item.
 			}
 			else
 			{
-				from.Target = new InternalTarget( from, this );
-				from.SendMessage( "Who would you like to resurrect!" );
-			}
-			return;
-		}
-
-        public void Target( Mobile m, Mobile from, RodOfResurrection rod )
-        {
-            if ( !from.CanSee( m ) )
-            {
-                from.SendLocalizedMessage( 500237 ); // Target can not be seen.
-            }
-            else if ( !from.Alive )
-            {
-                from.SendLocalizedMessage( 501040 ); // The resurrecter must be alive.
-            }
-            else if (m is PlayerMobile && m.Alive)
-            {
-                from.SendLocalizedMessage( 501041 ); // Target is not dead.
-            }
-            else if ( !from.InRange( m, 2 ) )
-            {
-                from.SendLocalizedMessage( 501042 ); // Target is not close enough.
-            }
-            else if ( m.Map == null || !m.Map.CanFit( m.Location, 16, false, false ) )
-            {
-                from.SendLocalizedMessage( 501042 ); // Target can not be resurrected at that location.
-                m.SendLocalizedMessage( 502391 ); // Thou can not be resurrected there!
-            }
-            else if ( m is PlayerMobile )
-            {
-                m.PlaySound( 0x214 );
-                m.FixedEffect( 0x376A, 10, 16 );
-
-                m.CloseGump( typeof( ResurrectGump ) );
-				m.SendGump( new ResurrectGump( m, 1 ) );
-
-				ConsumeCharge( rod, from );
-            }
-            else if (m is BaseCreature )
-			{
-				BaseCreature pet = (BaseCreature)m;
-				Mobile master = pet.GetMaster();
- 
-                m.PlaySound( 0x214 );
-                m.FixedEffect( 0x376A, 10, 16 );
- 
-                master.CloseGump(typeof(PetResurrectGump));
-                master.SendGump(new PetResurrectGump(master, pet));
-
-				ConsumeCharge( rod, from );
-            }
-        }
-
-        public void ItemTarget( Item hench, Mobile from, RodOfResurrection rod )
-        {
-			
-		}
- 
-        private class InternalTarget : Target
-        {
-            private Mobile m_Owner;
-            private RodOfResurrection m_Rod;
- 
-            public InternalTarget( Mobile owner, RodOfResurrection rod ) : base( 1, false, TargetFlags.Beneficial )
-            {
-                m_Owner = owner;
-				m_Rod = rod;
-            }
- 
-            protected override void OnTarget( Mobile from, object o )
-            {
-                if ( o is Mobile )
-                {
-                    m_Rod.Target( (Mobile)o, from, m_Rod );
-                }
-                else if ( o is Item )
-                {
-                    m_Rod.ItemTarget( (Item)o, from, m_Rod );
-                }
-            }
-        }
-
-        public override void AddNameProperties(ObjectPropertyList list)
-		{
-            base.AddNameProperties(list);
-			list.Add( 1070722, "*dotkniecie symbolu na rekojesci powoduje wskrzeszenie czlowieka lub zwierzecia*");
-        }
-
-		public void ConsumeCharge( RodOfResurrection rod, Mobile from )
-		{
-			--rod.Charges;
-
-			if ( rod.Charges == 0 )
-			{
-				from.SendMessage( "All of the magic has been drained from the rod." );
-				rod.Delete();
+				from.SendMessage("Who would you like to resurrect!");
+				from.BeginTarget(1, false, TargetFlags.Beneficial, OnTarget);
 			}
 		}
 
-		public override void GetProperties( ObjectPropertyList list )
+		public void OnTarget(Mobile from, object targeted)
 		{
-			base.GetProperties( list );
-			list.Add( 1060584, m_Charges.ToString() );
+			if (targeted is not Mobile m)
+			{
+				from.SendLocalizedMessage(500489); //Nie mozesz tego zrobic
+			}
+			else if (!from.CanSee(m))
+			{
+				from.SendLocalizedMessage(500237); // Target can not be seen.
+			}
+			else if (!from.Alive)
+			{
+				from.SendLocalizedMessage(501040); // The resurrecter must be alive.
+			}
+			else if (m is PlayerMobile && m.Alive)
+			{
+				from.SendLocalizedMessage(501041); // Target is not dead.
+			}
+			else if (!from.InRange(m, 2))
+			{
+				from.SendLocalizedMessage(501042); // Target is not close enough.
+			}
+			else if (m.Map == null || !m.Map.CanFit(m.Location, 16, false, false))
+			{
+				from.SendLocalizedMessage(501042); // Target can not be resurrected at that location.
+				m.SendLocalizedMessage(502391); // Thou can not be resurrected there!
+			}
+			else if (targeted is PlayerMobile)
+			{
+				m.PlaySound(0x214);
+				m.FixedEffect(0x376A, 10, 16);
+				m.CloseGump<ResurrectGump>();
+				m.SendGump(new ResurrectGump(m, 1));
+
+				ConsumeCharge(from);
+			}
+			else if (targeted is BaseCreature pet)
+			{
+				var master = pet.GetMaster();
+
+				m.PlaySound(0x214);
+				m.FixedEffect(0x376A, 10, 16);
+
+				master.CloseGump<PetResurrectGump>();
+				master.SendGump(new PetResurrectGump(master, pet));
+
+				ConsumeCharge(from);
+			}
 		}
 
-		public RodOfResurrection( Serial serial ) : base( serial )
+		public override void AddNameProperties(ObjectPropertyList list)
+		{
+			base.AddNameProperties(list);
+			list.Add("*dotkniecie symbolu na rekojesci powoduje wskrzeszenie czlowieka lub zwierzecia*");
+		}
+
+		public void ConsumeCharge(Mobile from)
+		{
+			Charges--;
+
+			if (Charges == 0)
+			{
+				from.SendMessage("All of the magic has been drained from the rod.");
+				Delete();
+			}
+		}
+
+		public override void GetProperties(ObjectPropertyList list)
+		{
+			base.GetProperties(list);
+			list.Add(1060584, _Charges.ToString());
+		}
+
+		public RodOfResurrection(Serial serial) : base(serial)
 		{
 		}
 
-		public override void Serialize( GenericWriter writer )
+		public override void Serialize(GenericWriter writer)
 		{
-			base.Serialize( writer );
-			writer.Write( (int) 0 ); // version
-			writer.Write( (int) m_ResurrectEffect );
-			writer.Write( (int) m_Charges );
+			base.Serialize(writer);
+			writer.Write((int)0); // version
+			writer.Write(_Charges);
 		}
 
-		public override void Deserialize( GenericReader reader )
+		public override void Deserialize(GenericReader reader)
 		{
-			base.Deserialize( reader );
+			base.Deserialize(reader);
 			int version = reader.ReadInt();
-			switch ( version )
-			{
-				case 0:
-				{
-					m_ResurrectEffect = (ResurrectEffect)reader.ReadInt();
-					m_Charges = (int)reader.ReadInt();
-					break;
-				}
-			}
+			_Charges = reader.ReadInt();
 		}
 	}
 }
