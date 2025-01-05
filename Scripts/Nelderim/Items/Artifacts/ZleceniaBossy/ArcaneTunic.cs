2b0b45870b63437b20623ed4e84181a1932d2f45
kaczy <kaczyf93@gmail.com>
2025-01-05 11:42:56 +0100

    Reworked ArcaneTunic

--- Scripts/Nelderim/Items/Artifacts/ZleceniaBossy/ArcaneTunic.cs
+++ Scripts/Nelderim/Items/Artifacts/ZleceniaBossy/ArcaneTunic.cs
@@ -16,13 +16,10 @@ namespace Server.Items
         {
             Name = "Tunika Arkanisty z Thila";
             Hue = 0x556;
-            Attributes.DefendChance = 0;
             Attributes.CastSpeed = 1;
             Attributes.LowerManaCost = 10;
             Attributes.LowerRegCost = 5;
             Attributes.SpellDamage = 4;
-
-            originalDefenseChance = Attributes.DefendChance;
         }
 
         public override void AddNameProperties(ObjectPropertyList list)
@@ -31,65 +28,36 @@ namespace Server.Items
             list.Add(1049644, "Dotkniecie symbolu wyrytego na piersi tuniki powoduje zwiekszenie umiejetnosci unikania ciosow");
         }
 
-        public void OnRemoved(IEntity parent)
-        {
-            base.OnRemoved(parent);
-
-            Mobile character = parent as Mobile;
-
-            if (character != null && character.Backpack != null && this.Layer == Layer.InnerTorso)
-            {
-                // Decrease Attributes.DefendChance by 10 when the item is removed from the chest to the backpack
-                Attributes.DefendChance = Math.Max(0, Attributes.DefendChance - 10);
-            }
-
-            ResetTimer(); // Reset the timer when the item is removed
-        }
-
         public override void OnDoubleClick(Mobile from)
         {
-            if (!IsChildOf(from.FindItemOnLayer(Layer.InnerTorso)))
-            {
-                if (DateTime.UtcNow < nextUseTime)
-                {
-                    from.SendMessage("Musisz odczekac jeszcze troche czasu przed uzyciem tego ponownie.");
-                    return;
-                }
+	        if (Parent != from)
+	        {
+		        from.SendLocalizedMessage(502641); // You must equip this item to use it.
+		        return;
+	        }
 
-                if (Attributes.DefendChance < 10) // Nie wyjdzie poza 10 DCI
-                {
-                    Attributes.DefendChance = Math.Min(10, Attributes.DefendChance + 10);
-                    from.SendMessage("Twoja umiejetnosc unikania ciosow wzrasta.");
-                    
-                    // Start or restart the timer
-                    ResetTimer();
-                }
-                else if (Attributes.DefendChance >= 10)
-                {
-                    from.SendMessage("Twoja umiejetnosc unikania ciosow jest juz na maksymalnym poziomie.");
-                }
-
-                nextUseTime = DateTime.UtcNow + TimeSpan.FromMinutes(5); // Cooldown 5 min
-            }
+	        if (!from.BeginAction(typeof(ArcaneTunic)))
+	        {
+		        from.SendMessage("Musisz odczekac jeszcze troche czasu przed uzyciem tego ponownie.");
+		        return;
+	        }
+	        BeginEffect(from);
         }
 
-        private void ResetTimer()
+        private void BeginEffect(Mobile from)
         {
-            if (resetTimer != null)
-            {
-                resetTimer.Stop();
-            }
-
-            resetTimer = Timer.DelayCall(TimeSpan.FromMinutes(5), () =>
-            {
-                ResetDefenseChance();
-                resetTimer = null;
-            });
+	        Attributes.DefendChance = 10;
+	        Movable = false;
+	        Timer.DelayCall(TimeSpan.FromMinutes(5), () => EndEffect(from));
+	        from.SendMessage("Twoja umiejetnosc unikania ciosow wzrasta.");
         }
 
-        private void ResetDefenseChance()
+        private void EndEffect(Mobile from)
         {
-            Attributes.DefendChance = originalDefenseChance;
+	        Movable = true;
+	        Attributes.DefendChance = 0;
+	        from.EndAction(typeof(ArcaneTunic));
+	        from.SendMessage("Efekt tuniki przestaje dzialac");
         }
 
         public ArcaneTunic(Serial serial) : base(serial)
@@ -98,28 +66,15 @@ namespace Server.Items
 
         public override void Serialize(GenericWriter writer)
         {
+	        Attributes.DefendChance = 0;
             base.Serialize(writer);
-            writer.Write((int)1); // Version
-            writer.Write(originalDefenseChance);
-            writer.Write(nextUseTime);
-            writer.Write(Attributes.DefendChance);
+            writer.Write((int)0); // Version
         }
 
         public override void Deserialize(GenericReader reader)
         {
             base.Deserialize(reader);
             int version = reader.ReadInt();
-            originalDefenseChance = reader.ReadInt();
-            nextUseTime = reader.ReadDateTime();
-            
-            if (version >= 1)
-            {
-                Attributes.DefendChance = Math.Max(0, reader.ReadInt());
-            }
-            else
-            {
-                Attributes.DefendChance = 0;
-            }
         }
     }
 }
