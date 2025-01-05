427ad4f1c7d933319661a2f24aa532bee24e7354
kaczy <kaczyf93@gmail.com>
2025-01-05 20:13:55 +0100

    Reworked everlasting bottle

--- Scripts/Nelderim/Items/Artifacts/ZleceniaBossy/EverlastingBottle.cs
+++ Scripts/Nelderim/Items/Artifacts/ZleceniaBossy/EverlastingBottle.cs
@@ -1,58 +1,53 @@
 using System;
 
-
 namespace Server.Items
 {
 	public class EverlastingBottle : Item
 	{
-		private DateTime timer;
-
 		public override double DefaultWeight => 1.0;
 
-
 		[Constructable]
-		public EverlastingBottle() : base( 3854 )
+		public EverlastingBottle() : base(0xF0E)
 		{
-			Hue = 0x849;
+			Hue = 2121;
 			Name = "Wieczna Butelka";
-			timer = DateTime.UtcNow;
 		}
 
 
-		public override void OnDoubleClick( Mobile from )
+		public override void OnDoubleClick(Mobile from)
 		{
-			if (DateTime.UtcNow > timer && from.Thirst < 20)
+			if (from.Thirst > 20)
 			{
-				timer = DateTime.Now + TimeSpan.FromHours(4);
-				from.Thirst = 20;
-				from.SendMessage( "You take a healthy drink from the bottle...and it magically begins to refill." );
-				from.PlaySound( Utility.RandomList( 0x30, 0x2D6 ) );
+				from.SendMessage("Nie jestes spragniony");
+				return;
 			}
-			else if (from.Thirst > 20)
-				from.SendMessage("You are not thirsty!");
+			if (from.BeginAction(typeof(EverlastingBottle)))
+			{
+				from.Thirst = 20;
+				from.SendMessage("Wypijasz zawartosc butelki, a ta magicznie zaczyna sie napelniac.");
+				from.PlaySound(Utility.RandomList(0x30, 0x2D6));
+				Timer.DelayCall(TimeSpan.FromMinutes(1), () => from.EndAction(typeof(EverlastingBottle)));
+			}
 			else
-				from.SendMessage("The bottle isn't full yet.");
+				from.SendMessage("Musisz odczekac chwile przed ponownym uzyciem.");
 		}
 
-
-		public EverlastingBottle( Serial serial ) : base( serial )
+		public EverlastingBottle(Serial serial) : base(serial)
 		{
 		}
 
 
-		public override void Serialize( GenericWriter writer )
+		public override void Serialize(GenericWriter writer)
 		{
-			base.Serialize( writer );
-			writer.Write( (int) 0 ); // version
+			base.Serialize(writer);
+			writer.Write((int)0); // version
 		}
 
 
-		public override void Deserialize( GenericReader reader )
+		public override void Deserialize(GenericReader reader)
 		{
-			base.Deserialize( reader );
+			base.Deserialize(reader);
 			int version = reader.ReadInt();
-			ItemID = 3854;
-			Hue = 0x849;
 		}
 	}
 }
