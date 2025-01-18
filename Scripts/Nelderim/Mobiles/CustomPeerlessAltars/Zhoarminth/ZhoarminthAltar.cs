73f6a65273f937659b0bb70a73a9414b389f3e92
Maupishon <64133911+Maupishon@users.noreply.github.com>
2025-01-18 15:41:07 +0100

    Zhoarminth

---
+++ Scripts/Nelderim/Mobiles/CustomPeerlessAltars/Zhoarminth/ZhoarminthAltar.cs
@@ -0,0 +1,55 @@
+using Server.Mobiles;
+using System;
+
+namespace Server.Items
+{
+    public class ZhoarminthAltar : PeerlessAltar
+    {
+        public override int KeyCount => 3;
+        public override MasterKey MasterKey => new ZhoarminthKey();
+
+        public override Type[] Keys => new Type[]
+        {
+	        typeof( OdnogaStaregoGazera ), typeof( PowiekaGazera ), typeof( KielBiesa ) 
+};
+
+        public override BasePeerless Boss => new Zhoaminth();
+
+        [Constructable]
+        public ZhoarminthAltar() : base(0x207C)
+        {
+	        
+            BossLocation = new Point3D(5461, 3796, -25);
+            TeleportDest = new Point3D(5473, 3823, -25);
+            ExitDest = new Point3D(5473, 3830, -25);
+
+            Name = "Oltarz Zhoarmintha";
+            Hue = 1161;
+        }
+
+        public override Rectangle2D[] BossBounds => m_Bounds;
+
+        private readonly Rectangle2D[] m_Bounds = new Rectangle2D[]
+        {
+            new Rectangle2D(5800, 1762, 19, 20),
+        };
+
+        public ZhoarminthAltar(Serial serial) : base(serial)
+        {
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write(0); // version
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+    }
+}
---
+++ Scripts/Nelderim/Mobiles/CustomPeerlessAltars/Zhoarminth/ZhoarminthKey.cs
@@ -0,0 +1,38 @@
+namespace Server.Items
+{
+    public class ZhoarminthKey : MasterKey
+    {
+        public ZhoarminthKey()
+            : base(0xFF3)
+        {
+        }
+
+        public ZhoarminthKey(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override int Lifespan => 600;
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write(0); // version
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+
+        public override bool CanOfferConfirmation(Mobile from)
+        {
+            if (from.Region != null && from.Region.IsPartOf("Alcala_Boss"))
+                return base.CanOfferConfirmation(from);
+
+            return false;
+        }
+    }
+}
