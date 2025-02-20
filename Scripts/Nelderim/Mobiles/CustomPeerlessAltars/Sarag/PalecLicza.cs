7435e3fc1c5cf3bdf7a9ba836f8637781f0d2858
Maupishon <64133911+Maupishon@users.noreply.github.com>
2025-02-20 11:03:37 +0100

    Sarag is Peerless

---
+++ Scripts/Nelderim/Mobiles/CustomPeerlessAltars/Sarag/PalecLicza.cs
@@ -0,0 +1,34 @@
+namespace Server.Items
+{
+    public class PalecLicza : PeerlessKey
+    {
+        [Constructable]
+        public PalecLicza()
+            : base(4327)
+        {
+            Weight = 1;
+            Hue = 1121; 
+            LootType = LootType.Blessed;
+        }
+
+        public PalecLicza(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override int LabelNumber => 3070009;//rog zhoarmintha
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write(0); // version
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+    }
+}
--- Scripts/Nelderim/Mobiles/Monsters/MigBoss/NSarag.cs
+++ Scripts/Nelderim/Mobiles/Monsters/MigBoss/NSarag.cs
@@ -8,7 +8,7 @@ using Server.Items;
 namespace Server.Mobiles
 {
 	[CorpseName("zgliszcza saraga")]
-	public class NSarag : BaseCreature
+	public class NSarag : BasePeerless
 	{
 		public override bool BardImmune => true;
 		public override double AttackMasterChance => 0.15;
