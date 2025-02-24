ad9d58d7c59011fe23c77d605401f2d4e71db1a4
Maupishon <64133911+Maupishon@users.noreply.github.com>
2025-02-24 08:46:16 +0100

    Travesty spawn changed

--- Scripts/Items/Functional/CitadelAltar.cs
+++ Scripts/Items/Functional/CitadelAltar.cs
@@ -18,16 +18,16 @@ namespace Server.Items
         [Constructable]
         public CitadelAltar() : base(0x207E)
         {
-            BossLocation = new Point3D(165, 1802, 0);
-            TeleportDest = new Point3D(150, 1819, 0);
-            ExitDest = new Point3D(144, 1824, 0);
+            BossLocation = new Point3D(6972, 38, 0);
+            TeleportDest = new Point3D(6972, 50, 0);
+            ExitDest = new Point3D(7061, 26, 0);
         }
 
         public override Rectangle2D[] BossBounds => m_Bounds;
 
         private readonly Rectangle2D[] m_Bounds = new Rectangle2D[]
         {
-            new Rectangle2D(152, 1801, 51, 39),
+            new Rectangle2D(6962, 29, 51, 39),
         };
 
         public CitadelAltar(Serial serial) : base(serial)
