24848a0b0e3af20625869c47499697e9098f5f36
kaczy <kaczyf93@gmail.com>
2025-02-24 15:54:18 +0100

    Revert "Remove CommissionContractOfEmployment from SBBanker"
    This reverts commit ca534612367a9f9ea9095f748f21c902d503e3e5.

--- Scripts/VendorInfo/SBBanker.cs
+++ Scripts/VendorInfo/SBBanker.cs
@@ -17,7 +17,7 @@ namespace Server.Mobiles
 			{
 				Add(new GenericBuyInfo("1041243", typeof(ContractOfEmployment), 5000, 20, 0x14F0, 0));
 				Add(new GenericBuyInfo("1062332", typeof(VendorRentalContract), 20000, 20, 0x14F0, 0x672));
-				// Add(new GenericBuyInfo("1159156", typeof(CommissionContractOfEmployment), 25000, 20, 0x14F0, 0));
+				Add(new GenericBuyInfo("1159156", typeof(CommissionContractOfEmployment), 25000, 20, 0x14F0, 0));
 				Add(new GenericBuyInfo("1047016", typeof(CommodityDeed), 5, 20, 0x14F0, 0x47));
 			}
 		}
