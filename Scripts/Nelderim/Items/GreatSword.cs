e5c09922e32b114f396b178fa1d8c1912463c411
kaczy <kaczyf93@gmail.com>
2025-03-01 10:48:27 +0100

    GreatSword will be TwoHanded

--- Scripts/Nelderim/Items/GreatSword.cs
+++ Scripts/Nelderim/Items/GreatSword.cs
@@ -3,18 +3,21 @@
 	[FlipableAttribute(0x26CE, 0x26CF)]
 	public class GreatSword : BaseSword
 	{
-		public override WeaponAbility PrimaryAbility { get { return WeaponAbility.WhirlwindAttack; } }
-		public override WeaponAbility SecondaryAbility { get { return WeaponAbility.Disarm; } }
+		public override WeaponAbility PrimaryAbility => WeaponAbility.WhirlwindAttack;
+		public override WeaponAbility SecondaryAbility => WeaponAbility.Disarm;
 
-		public override int StrengthReq { get { return 50; } }
-		public override int MinDamage { get { return 20; } }
-		public override int MaxDamage { get { return 24; } }
-		public override float Speed { get { return 5.0f; } }
-		public override int DefHitSound { get { return 0x237; } }
-		public override int DefMissSound { get { return 0x23A; } }
+		public override int StrengthReq => 50;
+		public override int MinDamage => 20;
+		public override int MaxDamage => 24;
+		public override float Speed => 5.0f;
+		public override int DefHitSound => 0x237;
+		public override int DefMissSound => 0x23A;
 
-		public override int InitMinHits { get { return 31; } }
-		public override int InitMaxHits { get { return 71; } }
+		public override int InitMinHits => 31;
+		public override int InitMaxHits => 71;
+
+		[CommandProperty(AccessLevel.GameMaster)]
+		public override Layer Layer => Layer.TwoHanded;
 
 		[Constructable]
 		public GreatSword() : base(0x26CE)
