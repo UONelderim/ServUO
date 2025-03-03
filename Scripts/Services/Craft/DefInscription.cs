4bf2f32a8fef2caef5b9d880275cfdcc9e61441d
Maupishon <64133911+Maupishon@users.noreply.github.com>
2025-03-03 21:58:19 +0100

    DefInscription.cs cliloc fixes

--- Scripts/Services/Craft/DefInscription.cs
+++ Scripts/Services/Craft/DefInscription.cs
@@ -366,13 +366,13 @@ namespace Server.Engines.Craft
             AddCraft(typeof(Spellbook), otherGroup, 1023834, 50.0, 126, typeof(BlankScroll), 1044377, 10, 1044378);
             
             index = AddCraft(typeof(PowerScrollBook), otherGroup, 3070040, 85.0, 120, typeof(BlankScroll), 1044377, 20, 1044378);
-            AddRes(index, typeof(DaemonBone), 1023965, 10, 1044253);
+            AddRes(index, typeof(DaemonBone), 1023968, 10, 1044253);
             AddRes(index, typeof(ParasiticPlant), 1032688, 2, 1044253);
             AddRes(index, typeof(Taint), 1032679, 2, 1044253);
             ForceNonExceptional(index);
             
             index = AddCraft(typeof(ScrollOfTranscendenceBook), otherGroup, 3070039, 85.0, 120, typeof(BlankScroll), 1044377, 20, 1044378);
-            AddRes(index, typeof(DaemonBone), 1044360, 10, 1044253);
+            AddRes(index, typeof(DaemonBone), 1023965, 10, 1044253);
             AddRes(index, typeof(LuminescentFungi), 1032689, 2, 1044253);
             AddRes(index, typeof(Corruption), 1032676, 2, 1044253);
             ForceNonExceptional(index);
