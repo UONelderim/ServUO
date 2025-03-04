314bde089a1fb6608e90ae0c34461ebe1bf7aef9
kaczy <kaczyf93@gmail.com>
2025-03-04 15:18:49 +0100

    Properly modified base gain factor :)

--- Server/Nelderim/Configuration/NConfig.cs
+++ Server/Nelderim/Configuration/NConfig.cs
@@ -14,7 +14,7 @@ namespace Nelderim.Configuration
 		public static bool CustomOnSpeech => Get("Nelderim.CustomOnSpeech", true);
 		public static bool CustomFameKarma => Get("Nelderim.CustomFameKarma", true);
 		public static bool CustomGainChance => Get("Nelderim.CustomGainChance", true);
-		public static double BaseGainFactor => Get("Nelderim.BaseGainFactor", 0.05);
+		public static double BaseGainFactor => Get("Nelderim.BaseGainFactor", 0.1);
 		public static bool TimeSystemEnabled => Get("Nelderim.TimeSystemEnabled", true);
 		public static bool FameTitlesEnabled => Get("Nelderim.FameTitlesEnabled", false);
 		public static bool NameSystemEnabled => Get("Nelderim.NameSystemEnabled", true);
