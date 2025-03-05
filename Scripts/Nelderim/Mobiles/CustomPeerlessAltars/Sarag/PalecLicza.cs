39a69b45714939c270eec07927d530fc53f7fa8f
Maupishon <64133911+Maupishon@users.noreply.github.com>
2025-03-05 20:51:07 +0100

    PalecLicza.cs cliloc fix

--- Scripts/Nelderim/Mobiles/CustomPeerlessAltars/Sarag/PalecLicza.cs
+++ Scripts/Nelderim/Mobiles/CustomPeerlessAltars/Sarag/PalecLicza.cs
@@ -16,7 +16,7 @@ namespace Server.Items
         {
         }
 
-        public override int LabelNumber => 3070009;//rog zhoarmintha
+        public override int LabelNumber => 3070035; //palec licza
         public override void Serialize(GenericWriter writer)
         {
             base.Serialize(writer);
