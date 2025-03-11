06434a062b793d4db89031fb3e623d2409cc2e34
Levy <Levy@Nelderim.pl>
2025-03-11 21:19:49 +0100

    New beetle kind (Frost)

---
+++ Scripts/Nelderim/Mobiles/Animals/FrostBeetle.cs
@@ -0,0 +1,208 @@
+using System.Collections.Generic;
+using Server.Items;
+using Server.ContextMenus;
+
+namespace Server.Mobiles
+{
+    [CorpseName("zwloki ogromnego zuka")]
+    public class FrostBeetle : BaseMount
+    {
+        public virtual double BoostedSpeed => 0.1;
+
+        [Constructable]
+        public FrostBeetle()
+            : this("ogromny zuk")
+        {
+        }
+
+        public override bool SubdueBeforeTame => true;// Must be beaten into submission
+        public override bool ReduceSpeedWithDamage => false;
+
+        [Constructable]
+        public FrostBeetle(string name)
+            : base(name, 0x317, 0x3EBC, AIType.AI_Melee, FightMode.Closest, 10, 1, 0.25, 0.5)
+        {
+	        double chance = Utility.RandomDouble() * 100;
+
+	        if (chance <= 0.5) 
+		        Hue = 2470;
+	        else if (chance <= 2) 
+		        Hue = Utility.RandomList(1073, 1790, 1077);
+	        else if (chance <= 10)
+		        Hue = Utility.RandomList(2498, 2461, 2460);
+	        else if (chance <= 25)
+		        Hue = Utility.RandomList(1150, 1153);
+	        else
+		        Hue = 0;
+	        
+            SetStr(300);
+            SetDex(100);
+            SetInt(500);
+
+            SetHits(200);
+
+            SetDamage(7, 20);
+
+            SetDamageType(ResistanceType.Physical, 100);
+
+            SetResistance(ResistanceType.Physical, 30, 40);
+            SetResistance(ResistanceType.Fire, 20, 30);
+            SetResistance(ResistanceType.Cold, 20, 30);
+            SetResistance(ResistanceType.Poison, 20, 30);
+            SetResistance(ResistanceType.Energy, 20, 30);
+
+            SetSkill(SkillName.MagicResist, 80.0);
+            SetSkill(SkillName.Tactics, 100.0);
+            SetSkill(SkillName.Wrestling, 100.0);
+
+            Fame = 4000;
+            Karma = -4000;
+
+            Tamable = true;
+            ControlSlots = 3;
+            MinTameSkill = 29.1;
+
+            Container pack = Backpack;
+
+            if (pack != null)
+                pack.Delete();
+
+            pack = new StrongBackpack
+            {
+                Movable = false
+            };
+
+            AddItem(pack);
+        }
+
+        public override int GetAngerSound()
+        {
+            return 0x21D;
+        }
+
+        public override int GetIdleSound()
+        {
+            return 0x21D;
+        }
+
+        public override int GetAttackSound()
+        {
+            return 0x162;
+        }
+
+        public override int GetHurtSound()
+        {
+            return 0x163;
+        }
+
+        public override int GetDeathSound()
+        {
+            return 0x21D;
+        }
+
+        public override FoodType FavoriteFood => FoodType.Meat;
+
+        		public override void GetProperties( ObjectPropertyList list ) // bedzie wyswietlac wage w zwierzach z paczka
+        {
+            base.GetProperties( list );
+
+            if (Backpack!= null) {
+                list.Add( 1072241, "{0}\t{1}\t{2}\t{3}", Backpack.TotalItems, Backpack.MaxItems, Backpack.TotalWeight, Backpack.MaxWeight ); // Contents: ~1_COUNT~/~2_MAXCOUNT~ items, ~3_WEIGHT~/~4_MAXWEIGHT~ stones
+            }
+		}
+
+		public override void OnWeightChange( int oldValue )
+		{
+			InvalidateProperties();
+		}
+
+        public override bool CanAutoStable => (Backpack == null || Backpack.Items.Count == 0) && base.CanAutoStable;
+
+        public FrostBeetle(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override void OnHarmfulSpell(Mobile from)
+        {
+            if (!Controlled && ControlMaster == null)
+                CurrentSpeed = BoostedSpeed;
+        }
+
+        public override void OnCombatantChange()
+        {
+            if (Combatant == null && !Controlled && ControlMaster == null)
+                CurrentSpeed = PassiveSpeed;
+        }
+
+        #region Pack Animal Methods
+        public override DeathMoveResult GetInventoryMoveResultFor(Item item)
+        {
+            return DeathMoveResult.MoveToCorpse;
+        }
+
+        public override bool IsSnoop(Mobile from)
+        {
+            if (PackAnimal.CheckAccess(this, from))
+                return false;
+
+            return base.IsSnoop(from);
+        }
+
+        public override bool OnDragDrop(Mobile from, Item item)
+        {
+            if (CheckFeed(from, item))
+                return true;
+
+            if (PackAnimal.CheckAccess(this, from))
+            {
+                AddToBackpack(item);
+                return true;
+            }
+
+            return base.OnDragDrop(from, item);
+        }
+
+        public override bool CheckNonlocalDrop(Mobile from, Item item, Item target)
+        {
+            return PackAnimal.CheckAccess(this, from);
+        }
+
+        public override bool CheckNonlocalLift(Mobile from, Item item)
+        {
+            return PackAnimal.CheckAccess(this, from);
+        }
+
+        public override void GetContextMenuEntries(Mobile from, List<ContextMenuEntry> list)
+        {
+            base.GetContextMenuEntries(from, list);
+
+            PackAnimal.GetContextMenuEntries(this, from, list);
+        }
+        #endregion
+
+        public override void OnAfterTame(Mobile tamer)
+        {
+            base.OnAfterTame(tamer);
+
+            if (Owners.Count == 0)
+            {
+                SetInt(500);
+            }
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write(1); // version
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+    }
+}
