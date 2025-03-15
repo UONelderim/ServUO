8daf42b6f58a730a70f5cb5c156d98b4288dc9b2
Levy <Levy@Nelderim.pl>
2025-03-15 23:06:06 +0100

    FireHorn Rework Cliloc for Horns

--- Scripts/Items/Equipment/Instruments/FireHorn.cs
+++ Scripts/Items/Equipment/Instruments/FireHorn.cs
@@ -8,20 +8,21 @@ namespace Server.Items
 {
     public partial class FireHorn : Item
     {
-		[Constructable]
+        [Constructable]
         public FireHorn()
             : base(0xFC7)
         {
             Hue = 0x466;
             Weight = 1.0;
-		}
+        }
 
         public FireHorn(Serial serial)
             : base(serial)
         {
         }
 
-		public override int LabelNumber => 1060456;// fire horn
+        public override int LabelNumber => 1060456;// Fire Horn
+
         public override void OnDoubleClick(Mobile from)
         {
             if (CheckUse(from))
@@ -31,16 +32,70 @@ namespace Server.Items
             }
         }
 
+        protected bool CheckUse(Mobile from)
+        {
+            if (!IsAccessibleTo(from))
+                return false;
+
+            if (from.Map != Map || !from.InRange(GetWorldLocation(), 2))
+            {
+                from.LocalOverheadMessage(MessageType.Regular, 0x3B2, 1019045); // I can't reach that.
+                return false;
+            }
+
+            if (!from.CanBeginAction(typeof(FireHorn)) || !from.CanBeginAction(typeof(IceHorn)) || 
+                !from.CanBeginAction(typeof(PoisonHorn)) || !from.CanBeginAction(typeof(EnergyHorn)))
+            {
+                from.SendLocalizedMessage(1049615); // You must take a moment to catch your breath.
+                return false;
+            }
+
+            if (from.Backpack == null || from.Backpack.GetAmount(typeof(SulfurousAsh)) < 4)
+            {
+                from.SendLocalizedMessage(1049621); // You do not have enough sulfurous ash.
+                return false;
+            }
+
+            return true;
+        }
+
+        private class InternalTarget : Target
+        {
+            private readonly FireHorn m_Horn;
+            
+            public InternalTarget(FireHorn horn)
+                : base(3, true, TargetFlags.Harmful)
+            {
+                m_Horn = horn;
+            }
+
+            protected override void OnTarget(Mobile from, object targeted)
+            {
+                if (m_Horn.Deleted)
+                    return;
+
+                IPoint3D loc = targeted is Item ? ((Item)targeted).GetWorldLocation() : targeted as IPoint3D;
+                m_Horn.Use(from, loc);
+            }
+        }
+
         public void Use(Mobile from, IPoint3D loc)
         {
             if (!CheckUse(from))
                 return;
 
             from.BeginAction(typeof(FireHorn));
-            Timer.DelayCall(TimeSpan.FromSeconds(6.0), new TimerStateCallback(EndAction), from);
+            from.BeginAction(typeof(IceHorn));
+            from.BeginAction(typeof(PoisonHorn));
+            from.BeginAction(typeof(EnergyHorn));
+            Timer.DelayCall(TimeSpan.FromSeconds(6.0), () => {
+                from.EndAction(typeof(FireHorn));
+                from.EndAction(typeof(IceHorn));
+                from.EndAction(typeof(PoisonHorn));
+                from.EndAction(typeof(EnergyHorn));
+            });
 
             int music = from.Skills[SkillName.Musicianship].Fixed;
-
             int sucChance = 500 + (music - 775) * 2;
             double dSucChance = sucChance / 1000.0;
 
@@ -56,7 +111,7 @@ namespace Server.Items
             from.PlaySound(0x15F);
             Effects.SendPacket(from, from.Map, new HuedEffect(EffectType.Moving, from.Serial, Serial.Zero, 0x36D4, from.Location, loc, 5, 0, false, true, 0, 0));
 
-            System.Collections.Generic.List<Mobile> targets = SpellHelper.AcquireIndirectTargets(from, loc, from.Map, 2).OfType<Mobile>().ToList();
+            var targets = SpellHelper.AcquireIndirectTargets(from, loc, from.Map, 2).OfType<Mobile>().ToList();
             int count = targets.Count;
             bool playerVsPlayer = targets.Any(t => t.Player);
 
@@ -99,85 +154,11 @@ namespace Server.Items
                     Effects.SendTargetEffect(m, 0x3709, 10, 30);
                 }
             }
-
-            ColUtility.Free(targets);
-
-            if ( --UsesRemaining <= 0 )
+            
+            if (--UsesRemaining <= 0)
             {
                 from.SendLocalizedMessage(1049619); // The fire horn crumbles in your hands.
                 Delete();
-            }
-        }
-
-        public override void Serialize(GenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.WriteEncodedInt(0); // version
-		}
-
-        public override void Deserialize(GenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            int version = reader.ReadEncodedInt();
-		}
-
-        private static void EndAction(object state)
-        {
-            Mobile m = (Mobile)state;
-
-            m.EndAction(typeof(FireHorn));
-            m.SendLocalizedMessage(1049621); // You catch your breath.
-        }
-
-        private bool CheckUse(Mobile from)
-        {
-            if (!IsAccessibleTo(from))
-                return false;
-
-            if (from.Map != Map || !from.InRange(GetWorldLocation(), 2))
-            {
-                from.LocalOverheadMessage(MessageType.Regular, 0x3B2, 1019045); // I can't reach that.
-                return false;
-            }
-
-            if (!from.CanBeginAction(typeof(FireHorn)))
-            {
-                from.SendLocalizedMessage(1049615); // You must take a moment to catch your breath.
-                return false;
-            }
-
-            if (from.Backpack == null || from.Backpack.GetAmount(typeof(SulfurousAsh)) < 4)
-            {
-                from.SendLocalizedMessage(1049617); // You do not have enough sulfurous ash.
-                return false;
-            }
-
-            return true;
-        }
-
-        private class InternalTarget : Target
-        {
-            private readonly FireHorn m_Horn;
-            public InternalTarget(FireHorn horn)
-                : base(3, true, TargetFlags.Harmful)
-            {
-                m_Horn = horn;
-            }
-
-            protected override void OnTarget(Mobile from, object targeted)
-            {
-                if (m_Horn.Deleted)
-                    return;
-
-                IPoint3D loc;
-                if (targeted is Item)
-                    loc = ((Item)targeted).GetWorldLocation();
-                else
-                    loc = targeted as IPoint3D;
-
-                m_Horn.Use(from, loc);
             }
         }
     }
