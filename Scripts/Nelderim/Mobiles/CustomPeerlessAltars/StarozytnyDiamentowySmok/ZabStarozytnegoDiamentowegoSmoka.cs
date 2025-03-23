4604028ae3e8998d24d17f7bee4be9c1f5e91eba
Maupishon <64133911+Maupishon@users.noreply.github.com>
2025-03-23 18:52:08 +0100

    Peerless key cliloc fix

--- Scripts/Nelderim/Mobiles/CustomPeerlessAltars/StarozytnyDiamentowySmok/ZabStarozytnegoDiamentowegoSmoka.cs
+++ Scripts/Nelderim/Mobiles/CustomPeerlessAltars/StarozytnyDiamentowySmok/ZabStarozytnegoDiamentowegoSmoka.cs
@@ -16,7 +16,7 @@ namespace Server.Items
         {
         }
 
-        public override int LabelNumber => 3070005;// zab starozytnego lodowego smoka
+        public override int LabelNumber => 33070013; //zab starozytnego diamentowego smoka
         public override void Serialize(GenericWriter writer)
         {
             base.Serialize(writer);
