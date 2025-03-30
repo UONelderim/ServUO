9545222f3dc4bb54cbb89d24b61cf03341489eb5
Maupishon <64133911+Maupishon@users.noreply.github.com>
2025-03-30 19:56:39 +0200

    Travesty.cs name fix

--- Scripts/Mobiles/Bosses/Travesty.cs
+++ Scripts/Mobiles/Bosses/Travesty.cs
@@ -38,7 +38,7 @@ namespace Server.Mobiles
         public Travesty()
             : base(AIType.AI_Mage, FightMode.Closest, 10, 1, 0.2, 0.4)
         {
-			Name = "Zmiennoksztaltny Vox Populi";
+			Name = "Nasladowca Vox Populi";
 			Body = 247;
 
             BaseSoundID = 0x46E;
