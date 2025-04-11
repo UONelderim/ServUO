50f7dad3c9090d68cd6155618ca96f820b67157b
Maupishon <64133911+Maupishon@users.noreply.github.com>
2025-04-11 19:44:54 +0200

    Translations

--- Scripts/Quests/ProfessionalBountyQuest/Mobiles/PirateCrew.cs
+++ Scripts/Quests/ProfessionalBountyQuest/Mobiles/PirateCrew.cs
@@ -32,7 +32,7 @@ namespace Server.Mobiles
         [Constructable]
         public PirateCrew() : base(AIType.AI_Melee, FightMode.Closest, 10, 1, 0.2, 0.4)
         {
-            Name = "Orcish Crew";
+            Name = "Orkowa zaloga";
 
             Body = 0.33 > Utility.RandomDouble() ? 0x8C : Utility.RandomList(0xB5, 0xB6);
 
