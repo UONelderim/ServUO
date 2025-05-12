d33b623db98ac1793d40961c076b6b76ba77e234
Maupishon <64133911+Maupishon@users.noreply.github.com>
2025-05-12 19:40:55 +0200

    SummonReaper.cs slots down

--- Scripts/Spells/Skill Masteries/SummonReaper.cs
+++ Scripts/Spells/Skill Masteries/SummonReaper.cs
@@ -120,7 +120,7 @@ namespace Server.Spells.SkillMasteries
             SetSkill(SkillName.Tactics, Math.Max(100, 75 * scale));
             SetSkill(SkillName.Wrestling, Math.Max(100, 75 * scale));
 
-            ControlSlots = 5;
+            ControlSlots = 4;
 
             Timer.DelayCall(TimeSpan.FromSeconds(1), () =>
                 {
