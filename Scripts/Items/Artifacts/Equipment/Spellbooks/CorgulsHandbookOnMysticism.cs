42fc4220f2a988e4601cf521aa911da106edced9
Maupishon <64133911+Maupishon@users.noreply.github.com>
2025-05-27 16:36:44 +0200

    CorgulsHandbookOnMysticism.cs name and isartifact fix

--- Scripts/Items/Artifacts/Equipment/Spellbooks/CorgulsHandbookOnMysticism.cs
+++ Scripts/Items/Artifacts/Equipment/Spellbooks/CorgulsHandbookOnMysticism.cs
@@ -3,12 +3,14 @@
     public class CorgulsHandbookOnMysticism : MysticBook
     {
         public override int LabelNumber => 1149779;
+        public override bool IsArtifact => true;
 
         [Constructable]
         public CorgulsHandbookOnMysticism()
         {
             Hue = 1159;
             Attributes.RegenMana = 3;
+            Name = "Podrecznik Corgula o mistycyzmie";
             Attributes.DefendChance = 5;
             Attributes.LowerManaCost = 10;
             Attributes.LowerRegCost = 20;
