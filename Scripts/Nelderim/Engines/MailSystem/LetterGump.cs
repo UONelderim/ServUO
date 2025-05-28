8d915b895c622bb63fa8c8532ec8570aa0b779ea
kaczy <kaczyf93@gmail.com>
2025-05-28 07:10:09 +0200

    Better letter gump

--- Scripts/Nelderim/Engines/MailSystem/LetterGump.cs
+++ Scripts/Nelderim/Engines/MailSystem/LetterGump.cs
@@ -8,20 +8,30 @@ namespace Server.Gumps
 	public class LetterGump : Gump
 	{
 		private const int LINE_LENGTH = 32;
+		private const int LINES_COUNT = 15;
+		private const int LINE_HEIGHT = 18;
 		
 		private Letter _Letter;
-
+ 
 		public LetterGump(Letter letter) : base(25, 25)
 		{
 			_Letter = letter;
 
 			AddPage(0);
-			AddBackground(0, 0, 260, 320, 1579);
-			for (var i = 0; i < 15; i++)
+			var y = 0;
+			AddImage(0, y, 0x820);
+			y += 37;
+			for (var i = 0; i < 4; i++)
+			{
+				AddImage(17, y, 0x821 + (i & 0x1)); //Interlaced background
+				y += 70;
+			}
+			AddImage(18, y, 0x823);
+			for (var i = 0; i < LINES_COUNT; i++)
 			{
 				var startIndex = i * LINE_LENGTH;
 				var initialText = (letter.Text != null && letter.Text.Length > (startIndex + LINE_LENGTH)) ? letter.Text.Substring(startIndex, LINE_LENGTH) : "";
-				AddTextEntry(20, 20 + i * 18, 220, 18, 0, i, initialText.Trim(), LINE_LENGTH);
+				AddTextEntry(28, 38 + i * LINE_HEIGHT, 220, 18, 0, i, initialText.Trim(), LINE_LENGTH);
 			}
 		}
 
